3748fe9d623a1cd8e4f6f900bc1c714cb42700be
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-25 09:51:44 -0300

    vite config setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ export default defineConfig(({ mode }: { mode: string }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the
   // `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), ''); // process requires "@types/node"
+  const env = loadEnv(mode, process.cwd(), ''); // "process.cwd()" requires "@types/node"
   return {
     // vite config
-    define: {
-      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    server: {
+      port: parseInt(env.VITE_CLIENT_PORT) || 5173,
     },
   };
 });
