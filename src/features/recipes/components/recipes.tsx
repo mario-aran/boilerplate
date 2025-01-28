f94b5ada96c7056143d21f31f8011001f04e8226
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-28 18:13:16 -0300

    shadcn pagination

--- src/features/recipes/components/recipes.tsx
+++ src/features/recipes/components/recipes.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -7,27 +8,38 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { useRecipes } from '@/features/recipes/hooks';
+import { useState } from 'react';
 
 export const Recipes = () => {
-  const { data } = useRecipes();
+  const [page, setPage] = useState(1);
+  const { data } = useRecipes({ page });
   const recipes = data?.recipes || [];
 
   return (
-    <div className="grid grid-cols-3 gap-8">
-      {recipes.map((recipe) => (
-        <Card key={recipe.id}>
-          <CardHeader>
-            <CardTitle>Card Title</CardTitle>
-            <CardDescription>Card Description</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>Card Content</p>
-          </CardContent>
-          <CardFooter>
-            <p>Card Footer</p>
-          </CardFooter>
-        </Card>
-      ))}
-    </div>
+    <section>
+      <div className="grid grid-cols-3 gap-8 place-items-center">
+        {recipes.map((recipe) => (
+          <Card
+            key={recipe.id}
+            className="flex flex-col h-full w-full max-w-80 items-center justify-center"
+          >
+            <CardHeader>
+              <CardTitle>{recipe.name}</CardTitle>
+              <CardDescription>
+                {recipe.cookTimeMinutes} mins to cook.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="max-w-64">
+              <img src={recipe.image} alt="Recipe Image" />
+            </CardContent>
+            <CardFooter>
+              <Button>View Recipe</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+
+      <div>Pagination</div>
+    </section>
   );
 };
