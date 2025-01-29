249a3e52f2b24756cec1e3f2fc09ffd6e9658dd9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-29 19:18:02 -0300

    recipes hooks

--- src/features/recipes/hooks/use-recipe-query.ts
+++ src/features/recipes/hooks/use-recipe-query.ts
@@ -1,19 +1,10 @@
-import { VITE_API_URL } from '@/config/env';
-import { RecipeApiResponse } from '@/features/recipes/types';
+import { getRecipe } from '@/features/recipes/apis';
 import { useQuery } from '@tanstack/react-query';
 
-// Types
-type GetRecipe = Promise<RecipeApiResponse>;
-
-// Utils
-const getRecipe = async (recipeId: string): GetRecipe => {
-  const url = `${VITE_API_URL}/recipes/${recipeId}`;
-  const response = await fetch(url);
-  return response.json();
-};
-
-export const useRecipeQuery = (recipeId: string) =>
-  useQuery({
+export const useRecipeQuery = (recipeId: string) => {
+  // return "tanstack-query"
+  return useQuery({
     queryKey: ['recipe', recipeId],
     queryFn: () => getRecipe(recipeId),
   });
+};
--- src/features/recipes/hooks/use-recipes-query.ts
+++ src/features/recipes/hooks/use-recipes-query.ts
@@ -1,45 +1,25 @@
-import { VITE_API_URL } from '@/config/env';
-import { RecipesApiResponse, RecipesParams } from '@/features/recipes/types';
+import { getRecipes } from '@/features/recipes/apis';
+import { useRecipesStore } from '@/lib/zustand/stores';
 import { useQuery } from '@tanstack/react-query';
 
-// Types
-interface PageMetadata {
-  lastPage: number | null;
-  prevPage: number | null;
-  nextPage: number | null;
-}
+export const useRecipesQuery = () => {
+  // "zustand"
+  const page = useRecipesStore((state) => state.page);
+  const rowsPerPage = useRecipesStore((state) => state.rowsPerPage);
+  const sortBy = useRecipesStore((state) => state.sortBy);
+  const order = useRecipesStore((state) => state.order);
 
-type GetRecipes = Promise<RecipesApiResponse & PageMetadata>;
-
-// Utils
-const getRecipes = async ({
-  limit = 10,
-  page = 1,
-  sortBy = 'id',
-  order = 'asc',
-}: RecipesParams): GetRecipes => {
-  // Fetch data
-  const skip = limit * (page - 1);
-  const url = `${VITE_API_URL}/recipes?limit=${limit}&skip=${skip}&sortBy=${sortBy}&order=${order}`;
-  const response = await fetch(url);
-  const data: RecipesApiResponse = await response.json();
-
-  // Prepare metadata
-  const total = data.total ?? 0;
-  const lastPage = Math.ceil(total / limit) || null;
-  const prevPage = page > 1 ? page - 1 : null;
-  const nextPage = skip + limit < total ? page + 1 : null;
-
-  return {
-    ...data,
-    lastPage,
-    prevPage,
-    nextPage,
+  // Prepare params
+  const params = {
+    page,
+    limit: rowsPerPage,
+    sortBy,
+    order,
   };
-};
 
-export const useRecipesQuery = (params: RecipesParams = {}) =>
-  useQuery({
+  // return "tanstack-query"
+  return useQuery({
     queryKey: ['recipes', params],
     queryFn: () => getRecipes(params),
   });
+};
