cfd29268df2eed0c73e7855a72d75451084b2aea
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-29 19:32:29 -0300

    recipes-cards

---
+++ src/features/recipes/components/recipes-cards.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { useRecipesQuery } from '@/features/recipes/hooks';
+
+export const RecipesCards = () => {
+  const { data } = useRecipesQuery();
+  const recipes = data?.recipes || [];
+
+  return (
+    <div className="grid grid-cols-3 gap-8 place-items-center">
+      {recipes.map((recipe) => (
+        <Card
+          key={recipe.id}
+          className="flex flex-col h-full w-full max-w-80 items-center justify-center"
+        >
+          <CardHeader>
+            <CardTitle>{recipe.name}</CardTitle>
+            <CardDescription>
+              {recipe.cookTimeMinutes} mins to cook.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="max-w-64">
+            <img src={recipe.image} alt="Recipe Image" />
+          </CardContent>
+          <CardFooter>
+            <Button>View Recipe</Button>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  );
+};
