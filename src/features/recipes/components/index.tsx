56bac1261bdfa1e833fde0fd62d58a00420de2af
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-29 19:28:54 -0300

    recipes component

---
+++ src/features/recipes/components/index.tsx
@@ -0,0 +1,11 @@
+import { RecipesCards } from './recipes-cards';
+import { RecipesPagination } from './recipes-pagination';
+
+export const Recipes = () => {
+  return (
+    <div>
+      <RecipesCards />
+      <RecipesPagination />
+    </div>
+  );
+};
