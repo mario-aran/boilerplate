2c69d7dc0374b51bf1e45210fcd31105eb28da80
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-30 18:43:11 -0300

    get-recipes

--- src/features/recipes/apis/get-recipes.ts
+++ src/features/recipes/apis/get-recipes.ts
@@ -1,33 +1,21 @@
 import { VITE_API_URL } from '@/config/env';
-import { Recipe, RecipesParams } from '@/features/recipes/types';
-
-// Types
-interface RecipesApiResponse {
-  recipes: Recipe[];
-  total: number;
-  skip: number;
-  limit: number;
-}
-
-interface CustomData {
-  numPage: number;
-  prevPage: number | null;
-  nextPage: number | null;
-}
-
-type GetRecipes = Promise<RecipesApiResponse & CustomData>;
+import {
+  GetRecipes,
+  RecipesApiResponse,
+  RecipesParams,
+} from '@/features/recipes/types';
 
 // Constants
 const FIRST_PAGE = 1;
 
 export const getRecipes = async ({
-  page,
   limit,
+  page,
   sortBy,
   order,
 }: RecipesParams): GetRecipes => {
   // Prepare url
-  const skip = (page - 1) * limit;
+  const skip = limit * (page - 1);
   const url = `${VITE_API_URL}/recipes?limit=${limit}&skip=${skip}&sortBy=${sortBy}&order=${order}`;
 
   // Fetch data
@@ -36,8 +24,8 @@ export const getRecipes = async ({
 
   // Prepare results
   const numPage = Math.ceil(data.total / limit) || FIRST_PAGE;
-  const prevPage = page > FIRST_PAGE ? page - 1 : null;
-  const nextPage = page < numPage ? page + 1 : null;
+  const prevPage = page > FIRST_PAGE ? page - 1 : 0;
+  const nextPage = page < numPage ? page + 1 : 0;
 
   return {
     ...data,
