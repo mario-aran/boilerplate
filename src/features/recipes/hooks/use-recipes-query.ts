0b7473d4e640c71094d85a8ded89f576ebc14eca
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-30 18:58:09 -0300

    use-recipes-query

--- src/features/recipes/hooks/use-recipes-query.ts
+++ src/features/recipes/hooks/use-recipes-query.ts
@@ -1,25 +1,28 @@
 import { getRecipes } from '@/features/recipes/apis';
-import { useRecipesStore } from '@/lib/zustand/stores';
+import { useRecipesStore } from '@/features/recipes/stores';
 import { useQuery } from '@tanstack/react-query';
 
 export const useRecipesQuery = () => {
   // "zustand"
   const page = useRecipesStore((state) => state.page);
-  const rowsPerPage = useRecipesStore((state) => state.rowsPerPage);
+  const limit = useRecipesStore((state) => state.limit);
   const sortBy = useRecipesStore((state) => state.sortBy);
   const order = useRecipesStore((state) => state.order);
+  const setPageData = useRecipesStore((state) => state.setPageData);
 
   // Prepare params
-  const params = {
-    page,
-    limit: rowsPerPage,
-    sortBy,
-    order,
-  };
+  const params = { page, limit, sortBy, order };
 
   // return "tanstack-query"
   return useQuery({
     queryKey: ['recipes', params],
-    queryFn: () => getRecipes(params),
+    queryFn: async () => {
+      const data = await getRecipes(params);
+      const { numPage, prevPage, nextPage, ...rest } = data;
+
+      setPageData({ numPage, prevPage, nextPage });
+
+      return rest;
+    },
   });
 };
