188d28a2da1d89c5d4cc50498dd24f0c7a4e7ebd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-30 18:25:45 -0300

    about

--- src/app/routes/about.tsx
+++ src/app/routes/about.tsx
@@ -3,8 +3,6 @@ import { Recipes } from '@/features/recipes/components';
 export const About = () => {
   return (
     <main className="container mx-auto px-4">
-      <h1>About</h1>
-
       <Recipes />
     </main>
   );
