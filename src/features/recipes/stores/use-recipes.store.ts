d235de749233f7e1e93c281912f963832169b1b4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-30 16:47:23 -0300

    recipes store

---
+++ src/features/recipes/stores/use-recipes.store.ts
@@ -0,0 +1,14 @@
+import { RecipesStore } from '@/features/recipes/types';
+import { create } from 'zustand';
+
+export const useRecipesStore = create<RecipesStore>()((set) => ({
+  rowsPerPage: 6,
+  page: 1,
+  numPage: 1,
+  prevPage: null,
+  nextPage: null,
+  sortBy: 'id',
+  order: 'asc',
+  setPageData: (pageData) => set(pageData),
+  changePage: (page) => set((state) => (page !== state.page ? { page } : {})),
+}));
