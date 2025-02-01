a2f66ed1e6c4c400a675e45a0ff8c2fb36fcdfee
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-01-31 21:10:59 -0300

    store

--- src/features/recipes/store.ts
+++ src/features/recipes/store.ts
@@ -2,29 +2,16 @@ import { RecipesParams } from '@/features/recipes/types';
 import { create } from 'zustand';
 
 // Types
-interface PageData {
-  total: number;
-  numPage: number;
-  prevPage: number;
-  nextPage: number;
-}
-
 interface RecipesStoreActions {
-  setPageData: (pageData: PageData) => void;
   changePage: (page: number) => void;
 }
 
-type RecipesStore = RecipesParams & PageData & RecipesStoreActions;
+type RecipesStore = RecipesParams & RecipesStoreActions;
 
 export const useRecipesStore = create<RecipesStore>()((set) => ({
-  limit: 6,
   page: 1,
-  total: 0,
-  numPage: 1,
-  prevPage: 0,
-  nextPage: 0,
+  limit: 6,
   sortBy: 'id',
   order: 'asc',
-  setPageData: (pageData) => set(pageData),
   changePage: (page) => set(() => (page ? { page } : {})),
 }));
