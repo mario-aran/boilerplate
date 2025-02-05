37b6aa9f08dbdd5bc49bde0ca6edb5a26869bc8d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-05 17:36:09 -0300

    recipes-pagination

--- src/features/recipes/components/recipes-pagination.tsx
+++ src/features/recipes/components/recipes-pagination.tsx
@@ -1,71 +1,29 @@
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from '@/components/shadcn-ui/pagination';
+import { PaginationMenu } from '@/components/ui/pagination-menu';
 import { useRecipesStore } from '@/features/recipes/store';
-
-// Constants
-const FIRST_PAGE = 1;
+import { useShallow } from 'zustand/react/shallow';
 
 export const RecipesPagination = () => {
-  // Store
-  const page = useRecipesStore((state) => state.page);
-  const totalPages = useRecipesStore((state) => state.totalPages);
-  const prevPage = useRecipesStore((state) => state.prevPage);
-  const nextPage = useRecipesStore((state) => state.nextPage);
-  const changePage = useRecipesStore((state) => state.changePage);
-
-  // Render conditions
-  const showCurrentPage = page !== FIRST_PAGE && page !== totalPages;
-  const showFirstEllipsis = page > 2;
-  const showLastEllipsis = page < totalPages - 1;
+  // "zustand"
+  const { totalItems, page, totalPages, prevPage, nextPage, changePage } =
+    useRecipesStore(
+      useShallow((state) => ({
+        totalItems: state.totalItems,
+        page: state.page,
+        totalPages: state.totalPages,
+        prevPage: state.prevPage,
+        nextPage: state.nextPage,
+        changePage: state.changePage,
+      })),
+    );
 
   return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious
-            onClick={() => prevPage && changePage(prevPage)}
-          />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink onClick={() => changePage(FIRST_PAGE)}>
-            {FIRST_PAGE}
-          </PaginationLink>
-        </PaginationItem>
-
-        {showFirstEllipsis && (
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-        )}
-
-        {showCurrentPage && (
-          <PaginationItem>
-            <PaginationLink isActive>{page}</PaginationLink>
-          </PaginationItem>
-        )}
-
-        {showLastEllipsis && (
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
-        )}
-
-        <PaginationItem>
-          <PaginationLink onClick={() => changePage(totalPages)}>
-            {totalPages}
-          </PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext onClick={() => nextPage && changePage(nextPage)} />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+    <PaginationMenu
+      totalItems={totalItems}
+      page={page}
+      lastPage={totalPages}
+      prevPage={prevPage}
+      nextPage={nextPage}
+      changePage={changePage}
+    />
   );
 };
