686a27a4824aad68df2de5dd460df32e393e0b16
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-06 17:59:43 -0300

    store

--- src/features/recipes/store.ts
+++ src/features/recipes/store.ts
@@ -2,21 +2,20 @@ import { create } from 'zustand';
 import { RecipesParams } from './types';
 
 // Types
-type ItemsPerPage = (typeof ITEMS_PER_PAGE_OPTIONS)[number];
 
 interface RecipesStore extends Pick<RecipesParams, 'sortBy' | 'order'> {
   page: number;
-  itemsPerPage: ItemsPerPage;
+  itemsPerPage: number;
   totalItems: number;
   lastPage: number;
   changePage: (page: number) => void;
-  changeItemsPerPage: (itemsPerPage: ItemsPerPage) => void;
+  changeItemsPerPage: (itemsPerPage: number) => void;
   changeTotalItems: (totalItems: number) => void;
 }
 
 // Constants
 const FIRST_PAGE = 1;
-export const ITEMS_PER_PAGE_OPTIONS = [6, 9, 12] as const;
+export const ITEMS_PER_PAGE_OPTIONS = [6, 9, 12];
 
 export const useRecipesStore = create<RecipesStore>()((set) => ({
   // Params
@@ -32,11 +31,18 @@ export const useRecipesStore = create<RecipesStore>()((set) => ({
   // Actions
   changePage: (page) =>
     set((state) => {
-      if (page < FIRST_PAGE || page > state.lastPage) return {}; // Guard against out bounds
+      if (page < FIRST_PAGE || page > state.lastPage) return {}; // Guard page should be in range
       return { page };
     }),
 
-  changeItemsPerPage: (itemsPerPage) => set({ itemsPerPage }),
+  changeItemsPerPage: (itemsPerPage) =>
+    set((state) => ({
+      itemsPerPage,
+      lastPage: Math.max(
+        Math.ceil(state.totalItems / itemsPerPage),
+        FIRST_PAGE,
+      ),
+    })),
 
   changeTotalItems: (totalItems) =>
     set((state) => ({
