4be8febb788b3d0b4350a6b2e9771dd2f5d71d4f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-09 12:12:29 -0300

    index layout

--- src/components/layouts/public-layout/index.tsx
+++ src/components/layouts/public-layout/index.tsx
@@ -4,10 +4,10 @@ import { Footer } from './footer';
 
 export const PublicLayout = () => {
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex h-screen flex-col">
       <AppBar />
 
-      <main className="container mx-auto flex flex-1 flex-col border-x p-6">
+      <main className="container mx-auto flex-grow border-x p-6">
         {/* Nested routes */}
         <Outlet />
       </main>
