cb475ed153e6889bb14dd7ed360e33088fe42da0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-09 09:17:32 -0300

    recipes.route

--- src/app/routes/recipes.route.tsx
+++ src/app/routes/recipes.route.tsx
@@ -3,9 +3,7 @@ import { Recipes } from '@/features/recipes/components';
 export const RecipesRoute = () => {
   return (
     <>
-      <h1 className="text-2xl font-semibold tracking-tight sm:text-3xl md:text-4xl">
-        Recipes
-      </h1>
+      <h1 className="text-3xl font-semibold">Recipes</h1>
 
       <Recipes />
     </>
