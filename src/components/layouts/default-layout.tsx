1804df0359297fd09eb9cdefe079490f2db9f66a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-10 17:15:31 -0300

    layouts

--- src/components/layouts/default-layout.tsx
+++ src/components/layouts/default-layout.tsx
@@ -1,16 +1,22 @@
 import { Outlet } from 'react-router';
+import { BaseFooter } from './components/base-footer';
+import { BaseHeader } from './components/base-header';
 import { BaseMain } from './components/base-main';
 import { LayoutWrapper } from './components/layout-wrapper';
 
-export const EmptyLayout = () => {
+export const DefaultLayout = () => {
   return (
     <LayoutWrapper>
+      <BaseHeader />
+
       <BaseMain>
-        <div className="flex-1 p-6">
+        <div className="flex-1 border-x p-6">
           {/* "react-router" */}
           <Outlet />
         </div>
       </BaseMain>
+
+      <BaseFooter />
     </LayoutWrapper>
   );
 };
