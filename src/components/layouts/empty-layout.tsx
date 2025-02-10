1e0c7ee5f672a54a4228ae0beed1d32b57ec017e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-10 17:39:35 -0300

    empty-layout

--- src/components/layouts/empty-layout.tsx
+++ src/components/layouts/empty-layout.tsx
@@ -1,16 +1,10 @@
 import { Outlet } from 'react-router';
-import { BaseMain } from './components/base-main';
-import { LayoutWrapper } from './components/layout-wrapper';
 
 export const EmptyLayout = () => {
   return (
-    <LayoutWrapper>
-      <BaseMain>
-        <div className="flex-1 p-6">
-          {/* "react-router" */}
-          <Outlet />
-        </div>
-      </BaseMain>
-    </LayoutWrapper>
+    <main className="container mx-auto flex min-h-screen flex-col p-6">
+      {/* "react-router" */}
+      <Outlet />
+    </main>
   );
 };
