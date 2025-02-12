7cb871099a4c3370069e5941f105a55f01e7d68e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-12 20:11:53 -0300

    useRecipeQuery

--- src/features/recipes/hooks/use-recipe-query.ts
+++ src/features/recipes/hooks/use-recipe-query.ts
@@ -2,7 +2,6 @@ import { getRecipe } from '@/features/recipes/api';
 import { useQuery } from '@tanstack/react-query';
 
 export const useRecipeQuery = (recipeId: string) => {
-  // return "tanstack-query"
   return useQuery({
     queryKey: ['recipe', recipeId],
     queryFn: () => getRecipe(recipeId),
