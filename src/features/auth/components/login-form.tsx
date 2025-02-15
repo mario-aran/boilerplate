e4ea50dd908e468dcba5ec31acf3b4633f3b8397
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-15 15:00:49 -0300

    login-form

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -1,58 +1,38 @@
 import { Button } from '@/components/shadcn-ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/shadcn-ui/form';
-import { Input } from '@/components/shadcn-ui/input';
+import { Form } from '@/components/shadcn-ui/form';
+import { FormFieldInput } from '@/components/ui/form-field-input';
 import { useLoginMutation } from '@/features/auth/api';
 import { loginSchema, LoginSchema } from '@/features/auth/zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 
+// Constants
+const INPUTS: (keyof LoginSchema)[] = ['username', 'password'];
+
 export const LoginForm = () => {
   // "react-hook-form"
   const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
-    defaultValues: {
-      username: '',
-      password: '',
-    },
+    defaultValues: INPUTS.reduce((acc, key) => ({ ...acc, [key]: '' }), {}),
   });
 
   // "tanstack-query"
   const loginMutation = useLoginMutation();
 
-  const onSubmit = (values: LoginSchema) => {
-    console.log(values);
-    loginMutation.mutate(values);
-  };
+  // Utils
+  const onSubmit = (values: LoginSchema) => loginMutation.mutate(values);
 
   return (
-    <div className="w-full max-w-xs border">
+    <div className="w-full max-w-sm">
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Submit</Button>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          {INPUTS.map((input) => (
+            <FormFieldInput key={input} control={form.control} input={input} />
+          ))}
+
+          <Button className="w-full" type="submit">
+            Submit
+          </Button>
         </form>
       </Form>
     </div>
