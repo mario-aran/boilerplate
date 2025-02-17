d8a1d1183dc351c08fd59ee13cbb0799c8630fe4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-17 18:00:16 -0300

    home.route

--- src/app/routes/home.route.tsx
+++ src/app/routes/home.route.tsx
@@ -1,7 +1,29 @@
+import { LoginDialog, RegisterDialog } from '@/features/auth/components';
+import { PropsWithChildren } from 'react';
+
+// Internal components
+const CenteredSection = ({ children }: PropsWithChildren) => {
+  return (
+    <section className="flex flex-1 flex-col items-center justify-center">
+      {children}
+    </section>
+  );
+};
+
 export const HomeRoute = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="flex flex-1 flex-col md:flex-row">
+      <CenteredSection>
+        <h1 className="text-5xl font-bold">Home</h1>
+      </CenteredSection>
+
+      <CenteredSection>
+        <div className="w-full max-w-md space-y-4 p-6 text-center">
+          <span className="text-2xl font-medium">Join today.</span>
+          <RegisterDialog />
+          <LoginDialog />
+        </div>
+      </CenteredSection>
+    </div>
   );
 };
