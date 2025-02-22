b2e4a33caa08844dac5aae4afa226d8cef1d7567
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-02-22 19:27:29 -0300

    vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest/config" />
+// The above line allows "vitest" types in the project
+
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
@@ -7,16 +10,23 @@ export default defineConfig(({ mode }: { mode: string }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the
   // `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), ''); // "process.cwd()" requires "@types/node"
+  const env = loadEnv(mode, process.cwd(), ''); // "process.cwd" requires "@types/node"
 
   // Vite config
   return {
     plugins: [react()],
-    server: { port: parseInt(env.VITE_CLIENT_PORT) || 5173 },
+    server: {
+      port: parseInt(env.VITE_CLIENT_PORT) || 5173, // Client port
+    },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src'), // Absolute path
+        '@': path.resolve(__dirname, './src'), // Absolute path alias
       },
+    },
+    test: {
+      environment: 'jsdom', // "react-testing-library"
+      globals: true, // Global vars and auto clean up "react-testing-library"
+      coverage: { include: ['src/**/*.{ts,tsx}'] }, // Coverage
     },
   };
 });
