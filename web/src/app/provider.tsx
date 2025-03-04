6bc7e9b834672b40292711ff44c5ebd6c49a382b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-04 19:18:13 -0300

    added translations to provider

--- web/src/app/provider.tsx
+++ web/src/app/provider.tsx
@@ -3,6 +3,9 @@ import { ThemeProvider } from '@/lib/shadcn/theme-provider';
 import { TanstackQueryProvider } from '@/lib/tanstack/tanstack-query-provider';
 import { PropsWithChildren } from 'react';
 
+// Translations
+import '@/lib/i18next/i18n';
+
 export const AppProvider = ({ children }: PropsWithChildren) => {
   return (
     <TanstackQueryProvider>
