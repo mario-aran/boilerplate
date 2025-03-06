6e4ab6dc296f4dd4375f420b972d837346dddea1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-06 14:18:17 -0300

    header

--- web/src/components/layouts/default-layout/header.tsx
+++ web/src/components/layouts/default-layout/header.tsx
@@ -1,3 +1,4 @@
+import { LanguageSelect } from '@/components/controls/language-select';
 import { ThemeToggle } from '@/components/controls/theme-toggle';
 import { ROUTES } from '@/constants/routes';
 import { cn } from '@/lib/shadcn/utils';
@@ -12,8 +13,8 @@ const NAV_LINKS = [
 export const Header = () => {
   return (
     <header className="sticky top-0 z-50 border-b backdrop-blur-md">
-      <div className="container mx-auto flex items-center justify-between border-x px-4 py-2">
-        {/* Nav links */}
+      <div className="container mx-auto flex justify-between border-x px-4 py-2">
+        {/* Links */}
         <nav className="flex items-center gap-4 text-sm xl:gap-6">
           {NAV_LINKS.map(({ path, name }) => (
             <NavLink
@@ -31,8 +32,11 @@ export const Header = () => {
           ))}
         </nav>
 
-        {/* Nav buttons */}
-        <ThemeToggle />
+        {/* Buttons */}
+        <div className="flex">
+          <LanguageSelect />
+          <ThemeToggle />
+        </div>
       </div>
     </header>
   );
