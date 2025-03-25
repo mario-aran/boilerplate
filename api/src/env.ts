bbba9335bda2c5c75b18165c4355a925a4e5a6e9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-25 15:19:06 -0300

    minor chores

--- api/src/env.ts
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV !== 'production') import('dotenv/config'); // Don't use dotenv in production
-
-export const {
-  NODE_ENV = '',
-  SERVER_PORT = '',
-  DATABASE_URL = '',
-} = process.env;
