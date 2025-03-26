50422de8ab64fd56c8ff25e8ddb37b67e538cbb2
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-26 10:36:46 -0300

    drizzle connection setup

---
+++ apps/api/src/lib/drizzle/db.ts
@@ -0,0 +1,12 @@
+import { DATABASE_URL, NODE_ENV } from '@/config/env';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import * as schemas from './schemas';
+
+const pool = new Pool({ connectionString: DATABASE_URL });
+
+export const db = drizzle({
+  client: pool, // DB connection with driver
+  logger: NODE_ENV !== 'production' && true, // Query logging
+  schema: schemas, // Enable the use of "drizzle.query" on all schemas
+});
