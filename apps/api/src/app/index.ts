8d097256ad2168bb0a5aa87bba655a583a79d5ea
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-26 16:11:54 -0300

    express entryppoint with router

--- apps/api/src/app/index.ts
+++ apps/api/src/app/index.ts
@@ -2,9 +2,14 @@
 
 import { SERVER_PORT } from '@/config/env';
 import express from 'express';
+import { router } from './router';
 
 const app = express();
 
+// Set the router
+app.use('/api', router);
+
+// Start the server
 app.listen(SERVER_PORT, () => {
   console.log(`Server running on port: ${SERVER_PORT}`);
 });
