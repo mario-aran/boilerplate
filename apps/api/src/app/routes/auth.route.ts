168c20c3d8b0977326ade552b3164fed2161862a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-29 11:24:17 -0300

    added catch-async to routes

--- apps/api/src/app/routes/auth.route.ts
+++ apps/api/src/app/routes/auth.route.ts
@@ -1,13 +1,14 @@
 import { authController } from '@/features/auth/auth.controller';
+import { catchAsync } from '@/utils/catch-async';
 import { Router } from 'express';
 
 export const authRoute = Router();
 
 // Route definitions
-authRoute.post('/register', authController.register);
-authRoute.post('/login', authController.login);
-authRoute.post('/logout', authController.logout);
-authRoute.post('/refresh', authController.refresh);
-authRoute.get('/me', authController.me);
-authRoute.post('/forgot-password', authController.forgotPassword);
-authRoute.post('/reset-password', authController.resetPassword);
+authRoute.post('/register', catchAsync(authController.register));
+authRoute.post('/login', catchAsync(authController.login));
+authRoute.post('/logout', catchAsync(authController.logout));
+authRoute.post('/refresh', catchAsync(authController.refresh));
+authRoute.get('/me', catchAsync(authController.me));
+authRoute.post('/forgot-password', catchAsync(authController.forgotPassword));
+authRoute.post('/reset-password', catchAsync(authController.resetPassword));
--- apps/api/src/app/routes/users.route.ts
+++ apps/api/src/app/routes/users.route.ts
@@ -1,4 +1,5 @@
 import { usersController } from '@/features/users/users.controller';
+import { catchAsync } from '@/utils/catch-async';
 import { Router } from 'express';
 
 // Constants
@@ -7,8 +8,16 @@ const ROUTE_ID_PARAM = '/:id';
 export const usersRoute = Router();
 
 // Route definitions
-usersRoute.get('/', usersController.getAll);
-usersRoute.get(ROUTE_ID_PARAM, usersController.get);
-usersRoute.patch(ROUTE_ID_PARAM, usersController.update);
-usersRoute.patch(`${ROUTE_ID_PARAM}/role`, usersController.updateRole);
-usersRoute.patch(`${ROUTE_ID_PARAM}/password`, usersController.updatePassword);
+usersRoute.get('/', catchAsync(usersController.getAll));
+usersRoute.get(ROUTE_ID_PARAM, catchAsync(usersController.get));
+usersRoute.patch(ROUTE_ID_PARAM, catchAsync(usersController.update));
+
+usersRoute.patch(
+  `${ROUTE_ID_PARAM}/role`,
+  catchAsync(usersController.updateRole),
+);
+
+usersRoute.patch(
+  `${ROUTE_ID_PARAM}/password`,
+  catchAsync(usersController.updatePassword),
+);
