b4e91b3bfed3410cad4250c7cfb4c9162daea3c0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-29 11:43:55 -0300

    users.zod

---
+++ apps/api/src/features/users/users.zod.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod';
+
+// Fields
+const limit = z.number().int().positive().optional();
+const skip = z.number().int().positive().optional();
+const sortOrder = z.enum(['asc', 'desc']).optional();
+
+const sortBy = z
+  .array(z.enum(['id', 'userName', 'email', 'roleId']))
+  .optional()
+  .refine((arr) => !arr || (arr.length && new Set(arr).size === arr.length), {
+    message: 'Array cannot be empty and must contain unique values',
+  });
+
+const id = z.string().uuid();
+const userName = z.string().trim().min(3).max(20).optional();
+const email = z.string().email().trim().min(5).max(60).optional();
+const roleId = z.string().uuid();
+const password = z.string().trim().min(8).max(20);
+
+// Schemas
+export const usersZod = {
+  getAll: z.object({
+    query: z.object({ limit, skip, sortOrder, sortBy }),
+  }),
+  get: z.object({ params: z.object({ id }) }),
+  update: z.object({
+    params: z.object({ id }),
+    body: z.object({ userName, email }),
+  }),
+  updateRoles: z.object({
+    params: z.object({ id }),
+    body: z.object({ roleId }),
+  }),
+  updatePassword: z.object({
+    params: z.object({ id }),
+    body: z.object({ password }),
+  }),
+};
+
+// Schema types
+export type GetAllZod = z.infer<typeof usersZod.getAll>;
+export type GetZod = z.infer<typeof usersZod.get>;
+export type UpdateZod = z.infer<typeof usersZod.update>;
+export type UpdateRolesZod = z.infer<typeof usersZod.updateRoles>;
+export type UpdatePasswordZod = z.infer<typeof usersZod.updatePassword>;
