9c0c667f99649dfe08ed9cd07ccbbc566069ccaa
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-29 11:21:36 -0300

    added api catch-async

---
+++ apps/api/src/utils/catch-async.ts
@@ -0,0 +1,10 @@
+import { NextFunction, Request, Response } from 'express';
+
+// Types
+type Fn<T> = (req: Request, res: Response, next: NextFunction) => Promise<T>;
+
+export const catchAsync = <T>(fn: Fn<T>) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res, next).catch(next);
+  };
+};
