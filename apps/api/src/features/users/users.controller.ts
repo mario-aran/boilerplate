c3f984cabb1cb04656aa7caf7f3b538f94cd028f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-03-30 10:53:25 -0300

    users.controller

--- apps/api/src/features/users/users.controller.ts
+++ apps/api/src/features/users/users.controller.ts
@@ -1,17 +1,39 @@
+import { Response } from 'express';
+import {
+  GetAllRequest,
+  GetRequest,
+  UpdatePasswordRequest,
+  UpdateRequest,
+  UpdateRoleRequest,
+} from './users.zod';
+
 export const usersController = {
-  getAll: async () => {
-    return '';
+  getAll: async (req: GetAllRequest, res: Response) => {
+    const { limit } = req.query;
+
+    res.json({ limit });
   },
-  get: async () => {
-    return '';
+  get: async (req: GetRequest, res: Response) => {
+    const { id } = req.params;
+
+    res.json({ id });
   },
-  update: async () => {
-    return '';
+  update: async (req: UpdateRequest, res: Response) => {
+    const { id } = req.params;
+    const { userName, email } = req.body;
+
+    res.json({ id, userName, email });
   },
-  updateRole: async () => {
-    return '';
+  updateRole: async (req: UpdateRoleRequest, res: Response) => {
+    const { id } = req.params;
+    const { roleId } = req.body;
+
+    res.json({ id, roleId });
   },
-  updatePassword: async () => {
-    return '';
+  updatePassword: async (req: UpdatePasswordRequest, res: Response) => {
+    const { id } = req.params;
+    const { password } = req.body;
+
+    res.json({ id, password });
   },
 };
