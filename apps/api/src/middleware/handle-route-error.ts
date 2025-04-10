39ee04c8c6695cc42f3f5996547faf152102cee4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-10 16:58:11 -0400

    handle-route-error

---
+++ apps/api/src/middleware/handle-route-error.ts
@@ -0,0 +1,19 @@
+import { HTTP_STATUS } from '@/constants/http-status';
+import { HttpError } from '@/utils/http-error';
+import { NextFunction, Request, Response } from 'express';
+
+export const handleRouteError = (
+  err: HttpError,
+  _: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction,
+) => {
+  const errorStatus = err.status ?? HTTP_STATUS.SERVER_ERROR;
+
+  res.status(errorStatus).json({
+    status: errorStatus,
+    message: err.message || 'Internal Server Error',
+    stack: process.env.NODE_ENV !== 'production' ? err.stack : undefined,
+  });
+};
