ca574b4e3c725330e96da2f08a6b6881a42809be
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-10 17:02:38 -0400

    chore

---
+++ apps/api/src/server/index.ts
@@ -0,0 +1,15 @@
+// WARNING: This file is used by a script in "package.json". Do not rename or move
+
+import { SERVER_PORT } from '@/config/env';
+import { app } from './app';
+
+// Start the server
+const server = app.listen(SERVER_PORT, () => {
+  console.log(`Server running on port: ${SERVER_PORT}`);
+});
+
+// Handle server errors
+server.on('error', (error) => {
+  console.error('Server failed to start:', error);
+  process.exit(1);
+});
