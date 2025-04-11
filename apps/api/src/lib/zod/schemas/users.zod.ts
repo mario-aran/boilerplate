c43ee0c5fcd251851e5e01c5dbc07ee0302bebd0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-11 15:28:21 -0400

    users.zod

---
+++ apps/api/src/lib/zod/schemas/users.zod.ts
@@ -0,0 +1,31 @@
+import { USERS_COLUMNS } from '@/lib/drizzle/schemas';
+import { z } from 'zod';
+import { getOrderBy } from './utils/field-helpers';
+import { email, id, limit, name, password, skip } from './utils/fields';
+
+// Fields
+const firstName = name;
+const lastName = name;
+const orderBy = getOrderBy(USERS_COLUMNS);
+
+// Schemas
+export const getAllUsersZod = z.object({ orderBy, limit, skip }).partial();
+
+export const createUsersZod = z.object({
+  firstName,
+  lastName,
+  email,
+  password,
+});
+
+export const updateUsersZod = z
+  .object({ firstName, lastName, email, userRoleId: id })
+  .partial();
+
+export const updatePasswordUsersZod = z.object({ password });
+
+// Schema types
+export type GetAllUsersZod = z.infer<typeof getAllUsersZod>;
+export type CreateUsersZod = z.infer<typeof createUsersZod>;
+export type UpdateUsersZod = z.infer<typeof updateUsersZod>;
+export type UpdatePasswordUsersZod = z.infer<typeof updatePasswordUsersZod>;
