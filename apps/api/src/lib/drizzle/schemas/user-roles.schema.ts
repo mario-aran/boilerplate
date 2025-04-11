56bd18c02f98a81fd4e9e0d4f672c21ec952edeb
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-11 12:57:35 -0400

    schemas

--- apps/api/src/lib/drizzle/schemas/user-roles.schema.ts
+++ apps/api/src/lib/drizzle/schemas/user-roles.schema.ts
@@ -1,10 +1,10 @@
 import { relations } from 'drizzle-orm';
 import { pgTable, varchar } from 'drizzle-orm/pg-core';
 import { usersSchema } from './users.schema';
-import { createdAt, id, updatedAt } from './utils/common-columns';
+import { createdAt, updatedAt } from './utils/common-columns';
 
 export const userRolesSchema = pgTable('user_roles', {
-  id,
+  id: varchar('id', { length: 255 }).primaryKey(),
   createdAt,
   updatedAt,
   name: varchar('name', { length: 255 }).notNull().unique(),
--- apps/api/src/lib/drizzle/schemas/users.schema.ts
+++ apps/api/src/lib/drizzle/schemas/users.schema.ts
@@ -4,19 +4,21 @@ import {
   updatedAt,
 } from '@/lib/drizzle/schemas/utils/common-columns';
 import { relations } from 'drizzle-orm';
-import { pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, varchar } from 'drizzle-orm/pg-core';
 import { userRolesSchema } from './user-roles.schema';
 
 export const usersSchema = pgTable('users', {
   id,
-  userRoleId: uuid('user_role_id')
+  userRoleId: varchar('user_role_id', { length: 255 })
     .notNull()
+    .default('DEFAULT')
     .references(() => userRolesSchema.id),
   createdAt,
   updatedAt,
-  name: varchar('name', { length: 255 }).notNull(),
   email: varchar('email', { length: 255 }).notNull().unique(),
   password: varchar('password', { length: 255 }).notNull(),
+  firstName: varchar('first_name', { length: 255 }).notNull(),
+  lastName: varchar('last_name', { length: 255 }).notNull(),
 });
 
 export const usersRelations = relations(usersSchema, ({ one }) => ({
