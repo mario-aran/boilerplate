67f4389c3b236d4831bee32504404eb4ca187f2c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-11 12:22:24 -0400

    router

--- apps/api/src/server/router.ts
+++ apps/api/src/server/router.ts
@@ -1,15 +1,12 @@
-import { Request, Response, Router } from 'express';
-import { authRoute } from '../features/auth/auth.route';
-import { usersRoute } from '../features/users/users.route';
+import { Router } from 'express';
+import { authRoute } from './routes/auth.route';
+import { homeRoute } from './routes/home.route';
+import { usersRoute } from './routes/users.route';
 
 const router = Router();
 
-// Local routes
-router.get('/', (_req: Request, res: Response) => {
-  res.json({ message: 'Hello API' });
-});
-
 // Imported routes: Combine and declare paths
+router.use('/', homeRoute);
 router.use('/auth', authRoute);
 router.use('/users', usersRoute);
 
