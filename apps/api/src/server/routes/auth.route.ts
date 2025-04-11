8931dbbe8c34922fcf3aee15e809e6ee13cda3bd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-11 16:01:08 -0400

    auth.route

---
+++ apps/api/src/server/routes/auth.route.ts
@@ -0,0 +1,18 @@
+import { loginAuthZod } from '@/lib/zod/schemas/auth.zod';
+import { authController } from '@/server/controllers/auth.controller';
+import { zodValidate } from '@/server/middleware/zod-validate';
+import { routeCatchAsync } from '@/utils/route-catch-async';
+import { Router } from 'express';
+
+const authRoute = Router();
+
+// Route definitions
+authRoute.post(
+  '/login',
+  zodValidate({ body: loginAuthZod }),
+  routeCatchAsync(authController.login),
+);
+
+authRoute.post('/logout', routeCatchAsync(authController.logout));
+
+export { authRoute };
