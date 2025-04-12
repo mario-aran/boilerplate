4034822bc7c2f9835bc2bb395457daf6319b6d2a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-11 21:31:43 -0400

    field-helpers

--- apps/api/src/lib/zod/schemas/utils/field-helpers.ts
+++ apps/api/src/lib/zod/schemas/utils/field-helpers.ts
@@ -1,13 +1,27 @@
 import { z } from 'zod';
 
 // Filters
-const getSort = (allowedFields: string[]) => {
-  return z.string().refine(
-    (value) =>
-      value.split(',').every((field) => {
-        const cleanField = field.startsWith('-') ? field.slice(1) : field;
-        return allowedFields.includes(cleanField);
-      }),
-    { message: 'Invalid sort field(s)' },
-  );
+export const getSort = <T extends string>(allowedFields: readonly T[]) => {
+  return z
+    .string()
+    .refine(
+      (value) =>
+        value.split(',').every((el) => {
+          const field = (el.startsWith('-') ? el.slice(1) : el) as T;
+          return allowedFields.includes(field);
+        }),
+      { message: 'Invalid sort field(s)' },
+    )
+    .transform((value) =>
+      value
+        .split(',')
+        .filter(Boolean)
+        .map((el) => {
+          const desc = el.startsWith('-');
+          return {
+            field: (desc ? el.slice(1) : el) as T,
+            direction: desc ? ('desc' as const) : ('asc' as const),
+          };
+        }),
+    );
 };
