c303082492fb21a64f381749e4b14db9305ede32
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-13 10:11:56 -0400

    users.service

--- apps/api/src/features/users/users.service.ts
+++ apps/api/src/features/users/users.service.ts
@@ -1,6 +1,7 @@
 import { USER_ROLES } from '@/constants/user-roles';
 import { db } from '@/lib/drizzle/db';
 import { usersSchema } from '@/lib/drizzle/schemas';
+import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
 import {
   CreateUsersZod,
   GetAllUsersZod,
@@ -8,29 +9,42 @@ import {
   UpdateUsersZod,
 } from '@/lib/zod/schemas/users.zod';
 import bcrypt from 'bcryptjs';
-import { asc, desc, eq } from 'drizzle-orm';
+import { and, eq, ilike, or } from 'drizzle-orm';
 
 class UsersService {
-  private async hashPassword(password: string) {
-    return bcrypt.hash(password, 10);
-  }
-
   public async getAll({
-    orderBy = [{ field: 'id', order: 'asc' }],
-    limit = 10,
-    skip = 0,
+    limit,
+    page,
+    sort,
+    userRoleId = '',
+    q = '',
   }: GetAllUsersZod) {
-    const orderByFields = orderBy.map(({ field, order }) => {
-      const userField = usersSchema[field];
-      return order === 'desc' ? desc(userField) : asc(userField);
+    // Prepare filters
+    const filters = and(
+      ilike(usersSchema.userRoleId, `%${userRoleId}%`),
+      or(
+        ilike(usersSchema.email, `%${q}%`),
+        ilike(usersSchema.firstName, `%${q}%`),
+        ilike(usersSchema.lastName, `%${q}%`),
+      ),
+    );
+
+    // Query results
+    const { data, ...restOfResults } = await queryPaginatedData({
+      schema: usersSchema,
+      filters,
+      limit,
+      sort,
+      page,
     });
 
-    return db.query.usersSchema.findMany({
-      columns: { password: false },
-      orderBy: orderByFields,
-      limit,
-      offset: skip,
-    });
+    const dataWithNoPassword = data.map(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      ({ password: _, ...restOfData }) => restOfData,
+    );
+
+    // Return results
+    return { ...restOfResults, data: dataWithNoPassword };
   }
 
   public async get(id: string) {
@@ -42,37 +56,41 @@ class UsersService {
 
   public async create(data: CreateUsersZod) {
     const hashedPassword = await this.hashPassword(data.password);
-    const createdUser = await db
+    const [createdUser] = await db
       .insert(usersSchema)
       .values({
         ...data,
         userRoleId: USER_ROLES.DEFAULT,
         password: hashedPassword,
       })
-      .returning({ id: usersSchema.id });
+      .returning({ email: usersSchema.email });
 
-    return createdUser[0];
+    return createdUser;
   }
 
   public async update(id: string, data: UpdateUsersZod) {
-    const updatedUser = await db
+    const [updatedUser] = await db
       .update(usersSchema)
       .set(data)
       .where(eq(usersSchema.id, id))
-      .returning({ id: usersSchema.id });
+      .returning({ email: usersSchema.email });
 
-    return updatedUser[0];
+    return updatedUser;
   }
 
   public async updatePassword(id: string, data: UpdatePasswordUsersZod) {
     const hashedPassword = await this.hashPassword(data.password);
-    const updatedUser = await db
+    const [updatedUser] = await db
       .update(usersSchema)
       .set({ password: hashedPassword })
       .where(eq(usersSchema.id, id))
-      .returning({ id: usersSchema.id });
+      .returning({ email: usersSchema.email });
 
-    return updatedUser[0];
+    return updatedUser;
+  }
+
+  private async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
   }
 }
 
