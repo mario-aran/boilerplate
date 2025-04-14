7dcce205624e1431ba8010add02a759da31c5777
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-14 19:35:11 -0400

    chores

--- apps/api/src/lib/zod/utils/field-helpers.ts
+++ apps/api/src/lib/zod/utils/field-helpers.ts
@@ -2,30 +2,34 @@ import { z } from 'zod';
 
 // Filters
 export const getSort = <T extends string>(allowedFields: readonly T[]) => {
-  return z
-    .string()
-    .nonempty()
-    .transform((value) =>
-      value.split(',').map((el) => {
-        const desc = el.startsWith('-');
-        return {
-          field: (desc ? el.slice(1) : el) as T,
-          direction: desc ? ('desc' as const) : ('asc' as const),
-        };
-      }),
-    )
-    .refine(
-      (values) =>
-        values.every(({ field }) => {
-          return allowedFields.includes(field);
+  return (
+    z
+      .string()
+      .nonempty()
+
+      // Transform values from "field1,-field2,field3" to array
+      .transform((value) =>
+        value.split(',').map((el) => {
+          const desc = el.startsWith('-');
+          return {
+            field: (desc ? el.slice(1) : el) as T,
+            direction: desc ? ('desc' as const) : ('asc' as const),
+          };
         }),
-      { message: 'Invalid sort field(s)' },
-    )
-    .refine(
-      (values) => {
-        const unique = new Set(values.map(({ field }) => field));
-        return unique.size === values.length;
-      },
-      { message: 'Duplicate fields are not allowed' },
-    );
+      )
+      .refine(
+        (values) => {
+          const unique = new Set(values.map(({ field }) => field));
+          return unique.size === values.length;
+        },
+        { message: 'Duplicate fields are not allowed' },
+      )
+      .refine(
+        (values) =>
+          values.every(({ field }) => {
+            return allowedFields.includes(field);
+          }),
+        { message: 'Invalid sort field(s)' },
+      )
+  );
 };
