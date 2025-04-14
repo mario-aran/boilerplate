47176d1b5e95e0b0c8d7cd1761fc343f76ec7b83
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-14 17:36:09 -0400

    chores

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,9 +1,3 @@
-// Never use dotenv in production
-if (process.env.NODE_ENV !== 'production') {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  require('dotenv').config(); // Used "require()" because "import()" uses async
-}
-
 export const {
   NODE_ENV = '',
   SERVER_PORT = '',
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,11 @@
 // WARNING: This file is used by a script in "package.json". Do not rename or move
 
+// Never use dotenv in production
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  require('dotenv').config(); // Used "require()" because "import()" uses async
+}
+
 import { SERVER_PORT } from '@/config/env';
 import { app } from './app';
 
