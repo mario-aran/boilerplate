398646e9404df0fc47756b6d22c3d08eea73d7bc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-14 19:26:01 -0400

    chores

--- apps/api/src/features/auth/auth.controller.ts
+++ apps/api/src/features/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+import { HTTP_STATUS } from '@/constants/http-status';
 import { HttpError } from '@/utils/http-error';
 import { Request, Response } from 'express';
 import { authService } from './auth.service';
@@ -5,7 +6,8 @@ import { authService } from './auth.service';
 class AuthController {
   public async login(req: Request, res: Response) {
     const user = await authService.login(req.body);
-    if (!user) throw new HttpError(401, 'Invalid credentials');
+    if (!user)
+      throw new HttpError(HTTP_STATUS.UNAUTHORIZED, 'Invalid credentials');
 
     res.json({ message: `User ${user.email} logged in successfully` });
   }
