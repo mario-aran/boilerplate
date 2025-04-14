29155bf579bf4d3e394a257178edfe4c4a4297a0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-14 15:27:34 -0400

    query-paginated-data

---
+++ apps/api/src/lib/drizzle/utils/query-paginated-data.ts
@@ -0,0 +1,62 @@
+import { db } from '@/lib/drizzle/db';
+import { asc, count, desc, SQL } from 'drizzle-orm';
+import { AnyPgTable } from 'drizzle-orm/pg-core';
+
+// Types
+interface QueryParams {
+  schema: AnyPgTable;
+  filters?: SQL<unknown>;
+  page?: number;
+  limit?: number;
+  sort?: {
+    field: keyof AnyPgTable['$inferSelect'];
+    direction: 'asc' | 'desc';
+  }[];
+}
+
+export const queryPaginatedData = async ({
+  schema,
+  filters,
+  page = 1,
+  limit = 10,
+  sort = [{ field: 'id', direction: 'asc' }],
+}: QueryParams) => {
+  // Query count data
+  const [{ count: total }] = await db
+    .select({ count: count() })
+    .from(schema)
+    .where(filters);
+
+  const totalPages = total ? Math.ceil(total / limit) : 1;
+
+  const results = {
+    page,
+    prevPage: page > 1 ? page - 1 : null,
+    nextPage: page < totalPages ? page + 1 : null,
+    total,
+    totalPages,
+    limit,
+    data: [],
+  };
+
+  // Return results with empty data
+  if (!total) return results;
+
+  // Query data
+  const orderBy = sort.map(({ field, direction }) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const column = (schema as any)[field];
+    return direction === 'asc' ? asc(column) : desc(column);
+  });
+
+  const data = await db
+    .select()
+    .from(schema)
+    .where(filters)
+    .orderBy(...orderBy) // Spread orderBy array as individual arguments
+    .limit(limit)
+    .offset((page - 1) * limit);
+
+  // Return results with data
+  return { ...results, data };
+};
