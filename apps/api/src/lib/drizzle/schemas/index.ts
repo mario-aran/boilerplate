31c47acba975e606121d27d9416c8b69c704052d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-14 17:48:30 -0400

    chores

--- apps/api/src/lib/drizzle/schemas/index.ts
+++ apps/api/src/lib/drizzle/schemas/index.ts
@@ -1,4 +1,4 @@
-// WARNING: Barrel file is used by "drizzle.config" and database connection. Do not rename or move
+// WARNING: This barrel file is used by "drizzle.config" and database connection. Do not rename or move
 
 export * from './user-roles.schema';
 export * from './users.schema';
