6938fa034d75669252b5b9c5ebda62a3786cc5f7
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-15 18:19:29 -0400

    app

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,5 +1,6 @@
 import { handleNotFound } from '@/middleware/handle-not-found';
 import { handleRouteError } from '@/middleware/handle-route-error';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import passport from 'passport';
@@ -7,9 +8,10 @@ import { routes } from './routes';
 
 const app = express();
 
-app.use(cors());
-app.use(passport.initialize());
-app.use(express.json()); // JSON payloads
+app.use(passport.initialize()); // Auth
+app.use(cors()); // Origins
+app.use(cookieParser()); // Cookies
+app.use(express.json()); // JSON body
 
 // Routes
 app.use('/api', routes);
