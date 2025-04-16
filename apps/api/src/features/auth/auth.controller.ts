290b8d52b0c8a678c47fceecfb9ee249fd4af004
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-15 21:01:57 -0400

    auth.controller

--- apps/api/src/features/auth/auth.controller.ts
+++ apps/api/src/features/auth/auth.controller.ts
@@ -1,3 +1,5 @@
+import { NODE_ENV } from '@/config/env';
+import { COOKIES } from '@/constants/cookies';
 import { HTTP_STATUS } from '@/constants/http-status';
 import { HttpError } from '@/utils/http-error';
 import { Request, Response } from 'express';
@@ -5,11 +7,21 @@ import { authService } from './auth.service';
 
 class AuthController {
   public async login(req: Request, res: Response) {
-    const user = await authService.login(req.body);
-    if (!user)
+    const record = await authService.login(req.body);
+    if (!record)
       throw new HttpError(HTTP_STATUS.UNAUTHORIZED, 'Invalid credentials');
 
-    res.json({ message: `User ${user.email} logged in successfully` });
+    res.cookie(COOKIES.JWT, record.token, {
+      maxAge: 1000 * 60 * 60, // 1 hour
+      sameSite: 'lax',
+      secure: NODE_ENV === 'production',
+      httpOnly: true,
+    });
+    res.json({ message: `User ${record.email} logged in successfully` });
+  }
+
+  public logout(_: Request, res: Response) {
+    res.clearCookie(COOKIES.JWT);
   }
 }
 
