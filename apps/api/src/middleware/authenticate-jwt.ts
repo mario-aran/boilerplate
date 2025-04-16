1a3ab72655c13dcca04ba640cb9b008818538137
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-15 21:04:47 -0400

    authenticate-jwt

--- apps/api/src/middleware/authenticate-jwt.ts
+++ apps/api/src/middleware/authenticate-jwt.ts
@@ -1,3 +1,3 @@
-import { passport } from '@/lib/passport/strategies/passport';
+import { passport } from '@/lib/passport';
 
 export const authenticateJwt = passport.authenticate('jwt', { session: false });
