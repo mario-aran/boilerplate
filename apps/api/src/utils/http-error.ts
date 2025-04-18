6991fb215a50e19eced2104787c55f5f695fd371
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-18 09:47:21 -0400

    http-error and route-catch-async

--- apps/api/src/utils/http-error.ts
+++ apps/api/src/utils/http-error.ts
@@ -1,10 +1,12 @@
 export class HttpError extends Error {
   public status: number | undefined;
+  public details: unknown;
 
-  constructor(status?: number, message = '') {
+  constructor(status: number, message = '', details: unknown = null) {
     super(message);
+    this.name = this.constructor.name;
     this.status = status;
-    this.name = 'HttpError';
-    Error.captureStackTrace(this, this.constructor);
+    this.details = details;
+    Error.captureStackTrace(this, HttpError);
   }
 }
--- apps/api/src/utils/route-catch-async.ts
+++ apps/api/src/utils/route-catch-async.ts
@@ -1,22 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
 
-export const routeCatchAsync = <
-  TParams = unknown,
-  TResBody = unknown,
-  TReqBody = unknown,
-  TReqQuery = unknown,
->(
-  handler: (
-    req: Request<TParams, TResBody, TReqBody, TReqQuery>,
-    res: Response,
-    next: NextFunction,
-  ) => Promise<void>,
-) => {
-  return (
-    req: Request<TParams, TResBody, TReqBody, TReqQuery>,
-    res: Response,
-    next: NextFunction,
-  ) => {
+// Types
+type Handler = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<void>;
+
+export const routeCatchAsync = (handler: Handler) => {
+  return (req: Request, res: Response, next: NextFunction) =>
     handler(req, res, next).catch(next);
-  };
 };
