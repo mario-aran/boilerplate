70327704f5cd3cc2bcd638722223aede5b5351b2
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-19 10:39:49 -0400

    chores

--- apps/api/src/lib/passport/jwt.strategy.ts
+++ apps/api/src/lib/passport/jwt.strategy.ts
@@ -1,7 +1,9 @@
 import { JWT_SECRET } from '@/config/env';
 import { COOKIES } from '@/constants/cookies';
+import { HTTP_STATUS } from '@/constants/http-status';
 import { db } from '@/lib/drizzle/db';
 import { usersSchema } from '@/lib/drizzle/schemas';
+import { HttpError } from '@/utils/http-error';
 import { eq } from 'drizzle-orm';
 import { Request } from 'express';
 import { Strategy } from 'passport-jwt';
@@ -10,24 +12,28 @@ import { Strategy } from 'passport-jwt';
 const jwtCookieExtractor = (req: Request) => req.cookies?.[COOKIES.JWT] ?? null;
 
 // Initial values
-const opts = {
+const strategyOptions = {
   jwtFromRequest: jwtCookieExtractor,
   secretOrKey: JWT_SECRET,
 };
 
-export const jwtStrategy = new Strategy(opts, async (payload, done) => {
-  try {
-    const userExists = await db.query.usersSchema.findFirst({
-      where: eq(usersSchema.id, payload.id),
-    });
+export const jwtStrategy = new Strategy(
+  strategyOptions,
+  async (payload, done) => {
+    try {
+      const userExists = await db.query.usersSchema.findFirst({
+        where: eq(usersSchema.id, payload.id),
+      });
 
-    // User not found
-    if (!userExists) return done(null, false);
+      const unauthorizedError = new HttpError(
+        HTTP_STATUS.UNAUTHORIZED,
+        'Unauthorized',
+      );
+      if (!userExists) return done(unauthorizedError, false);
 
-    // Valid jwt
-    return done(null, userExists);
-  } catch (err) {
-    // Error
-    return done(err, false);
-  }
-});
+      return done(null, userExists);
+    } catch (err) {
+      return done(err, false);
+    }
+  },
+);
