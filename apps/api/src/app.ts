f65e090d8de3bbf58ae08c23e6e135ff401f5450
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-24 19:08:16 -0400

    app

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,24 +1,17 @@
-import { handleNotFound } from '@/middleware/handle-not-found';
-import { handleRouteError } from '@/middleware/handle-route-error';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import express from 'express';
 import { passport } from './lib/passport';
-import { routes } from './routes';
+import { router } from './router';
 
 const app = express();
 
 app.use(cors()); // Cors
-app.use(cookieParser()); // Cookies
-app.use(express.json()); // JSON body
 
-app.use(passport.initialize()); // Passport with all strategies
+app.use(cookieParser()); // Cookie parser
+app.use(express.json()); // JSON body parser
+app.use(passport.initialize()); // Custom passport strategies
 
-// Routes
-app.use('/api', routes);
-
-// Handle route errors
-app.use(handleNotFound); // Must be placed after all route definitions
-app.use(handleRouteError); // Must be the last middleware
+app.use('/', router); // Router
 
 export { app };
