b881ac52849d2a55ab676566e827d9e914bd73a2
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-25 11:53:48 -0400

    router

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -1,28 +1,30 @@
+import { HTTP_STATUS } from '@/constants/http-status';
 import { swaggerSpecV1 } from '@/lib/swagger/swagger-spec-v1';
 import { handleNotFound } from '@/middleware/handle-not-found';
 import { handleRouteError } from '@/middleware/handle-route-error';
 import { Router } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import { authRoute } from './api-routes/auth.route';
-import { homeRoute } from './api-routes/home-route';
 import { userRolesRoute } from './api-routes/user-roles.route';
 import { usersRoute } from './api-routes/users.route';
 
-const API_V1_PATH = '/api/v1';
 const API_DOCS_V1_PATH = '/api-docs/v1';
+const API_V1_PATH = '/api/v1';
 
 const router = Router();
-
-// API
-router.use(`${API_V1_PATH}/auth`, authRoute);
-router.use(`${API_V1_PATH}/users`, usersRoute);
-router.use(`${API_V1_PATH}/user-roles`, userRolesRoute);
-router.use(API_V1_PATH, homeRoute);
 
 // Swagger
 router.use(API_DOCS_V1_PATH, swaggerUi.serve, swaggerUi.setup(swaggerSpecV1));
 
-// Health check
+// API routes
+router.use(`${API_V1_PATH}/auth`, authRoute);
+router.use(`${API_V1_PATH}/users`, usersRoute);
+router.use(`${API_V1_PATH}/user-roles`, userRolesRoute);
+
+router.use(API_V1_PATH, (_req, res) => {
+  res.sendStatus(HTTP_STATUS.NO_CONTENT);
+});
+
 router.get('/', (_req, res) => {
   res.json({ message: 'Service is up and running' });
 });
