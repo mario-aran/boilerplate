db0c3edcd32d12822d139c7950216300f38a9da6
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-25 13:21:50 -0400

    auth.service

--- apps/api/src/features/auth/auth.service.ts
+++ apps/api/src/features/auth/auth.service.ts
@@ -1,12 +1,28 @@
 import { JWT_SECRET } from '@/config/env';
+import { USER_ROLES } from '@/constants/user-roles';
 import { db } from '@/lib/drizzle/db';
 import { usersSchema } from '@/lib/drizzle/schemas';
-import { LoginAuthZod } from '@/lib/zod/schemas/auth.zod';
+import { LoginAuthZod, RegisterAuthZod } from '@/lib/zod/schemas/auth.zod';
+import { hashPassword } from '@/utils/hash-password';
 import bcrypt from 'bcryptjs';
 import { eq } from 'drizzle-orm';
 import jwt from 'jsonwebtoken';
 
 class AuthService {
+  public async register(data: RegisterAuthZod) {
+    const hashedPassword = await hashPassword(data.password);
+    const [createdRecord] = await db
+      .insert(usersSchema)
+      .values({
+        ...data,
+        userRoleId: USER_ROLES.USER,
+        password: hashedPassword,
+      })
+      .returning({ email: usersSchema.email });
+
+    return createdRecord;
+  }
+
   public async login({ email, password }: LoginAuthZod) {
     // Check user
     const userExists = await db.query.usersSchema.findFirst({
