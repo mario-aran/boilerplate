22da2699e473273eb6a6a098654f24a9f4556cd0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-25 16:52:59 -0400

    schemas

--- apps/api/src/lib/drizzle/schemas/index.ts
+++ apps/api/src/lib/drizzle/schemas/index.ts
@@ -1,4 +1,6 @@
 // WARNING: This barrel file is used by "drizzle.config" and database connection. Do not rename or move
 
+export * from './permissions.schema';
+export * from './user-roles-to-permissions.schema';
 export * from './user-roles.schema';
 export * from './users.schema';
