21db6d7ef7b377135f1e2b6293530b5a9f113fcd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-28 16:58:56 -0400

    permissions

--- apps/api/src/constants/permissions.ts
+++ apps/api/src/constants/permissions.ts
@@ -1,12 +1,10 @@
 export const PERMISSIONS = {
-  CREATE_USER_ROLE: 'create_user_role',
-  READ_USER_ROLES: 'read_user_roles',
-  READ_USER_ROLE: 'read_user_role',
-  DELETE_USER_ROLE: 'delete_user_role',
   READ_USERS: 'read_users',
   READ_USER: 'read_user',
   UPDATE_USER: 'update_user',
   UPDATE_USER_PASSWORD: 'update_user_password',
+  READ_USER_ROLES: 'read_user_roles',
+  READ_PERMISSIONS: 'read_permissions',
 } as const;
 
 export const PERMISSION_VALUES = [...new Set(Object.values(PERMISSIONS))] as [
