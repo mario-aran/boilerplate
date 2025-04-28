a46514ca837419e28f822ce01b808599376a99bc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-28 17:32:23 -0400

    user-roles

--- apps/api/src/features/user-roles/user-roles.service.ts
+++ apps/api/src/features/user-roles/user-roles.service.ts
@@ -1,8 +1,21 @@
+import { db } from '@/lib/drizzle/db';
 import { userRolesSchema } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
-import { ReadAllUserRolesZod } from '@/lib/zod/schemas/user-roles.zod';
+import {
+  CreateUserRoleZod,
+  ReadAllUserRolesZod,
+} from '@/lib/zod/schemas/user-roles.zod';
 
 class UserRolesService {
+  public async create(data: CreateUserRoleZod) {
+    const [createdRecord] = await db
+      .insert(userRolesSchema)
+      .values(data)
+      .returning({ id: userRolesSchema.id });
+
+    return createdRecord;
+  }
+
   public async readAll({ limit, page, sort }: ReadAllUserRolesZod) {
     return queryPaginatedData({
       schema: userRolesSchema,
