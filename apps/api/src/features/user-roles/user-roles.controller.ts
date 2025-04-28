2502bf190f848471d6f227904c6c7ca1ba2a44e1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-28 17:03:20 -0400

    chores

--- apps/api/src/features/user-roles/user-roles.controller.ts
+++ apps/api/src/features/user-roles/user-roles.controller.ts
@@ -1,42 +1,14 @@
-import { HTTP_STATUS } from '@/constants/http-status';
 import { ReadAllUserRolesZod } from '@/lib/zod/schemas/user-roles.zod';
-import { HttpError } from '@/utils/http-error';
 import { Request, Response } from 'express';
 import { userRolesService } from './user-roles.service';
 
 class UserRolesController {
-  public async create(req: Request, res: Response) {
-    const createdRecord = await userRolesService.create(req.body);
-
-    res.status(HTTP_STATUS.CREATED).json({
-      message: `User role ${createdRecord.id} created successfully`,
-    });
-  }
-
   public async readAll(
     req: Request<unknown, unknown, unknown, ReadAllUserRolesZod>,
     res: Response,
   ) {
     const results = await userRolesService.readAll(req.query);
     res.json(results);
-  }
-
-  public async read(req: Request, res: Response) {
-    const record = await userRolesService.read(req.params.id);
-    if (!record) this.throwNotFoundHttpError();
-
-    res.json(record);
-  }
-
-  public async delete(req: Request, res: Response) {
-    const deletedRecord = await userRolesService.delete(req.params.id);
-    if (!deletedRecord) this.throwNotFoundHttpError();
-
-    res.json(deletedRecord);
-  }
-
-  private throwNotFoundHttpError() {
-    throw new HttpError(HTTP_STATUS.NOT_FOUND, 'User role not found');
   }
 }
 
--- apps/api/src/features/user-roles/user-roles.service.ts
+++ apps/api/src/features/user-roles/user-roles.service.ts
@@ -1,22 +1,8 @@
-import { db } from '@/lib/drizzle/db';
 import { userRolesSchema } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
-import {
-  CreateUserRoleZod,
-  ReadAllUserRolesZod,
-} from '@/lib/zod/schemas/user-roles.zod';
-import { eq } from 'drizzle-orm';
+import { ReadAllUserRolesZod } from '@/lib/zod/schemas/user-roles.zod';
 
 class UserRolesService {
-  public async create(data: CreateUserRoleZod) {
-    const [createdRecord] = await db
-      .insert(userRolesSchema)
-      .values(data)
-      .returning({ id: userRolesSchema.id });
-
-    return createdRecord;
-  }
-
   public async readAll({ limit, page, sort }: ReadAllUserRolesZod) {
     return queryPaginatedData({
       schema: userRolesSchema,
@@ -24,21 +10,6 @@ class UserRolesService {
       sort,
       page,
     });
-  }
-
-  public async read(id: string) {
-    return db.query.userRolesSchema.findFirst({
-      where: eq(userRolesSchema.id, id),
-    });
-  }
-
-  public async delete(id: string) {
-    const [deletedRecord] = await db
-      .delete(userRolesSchema)
-      .where(eq(userRolesSchema.id, id))
-      .returning({ id: userRolesSchema.id });
-
-    return deletedRecord;
   }
 }
 
