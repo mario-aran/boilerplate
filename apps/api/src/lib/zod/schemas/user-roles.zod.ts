a3d1f28e37f6a7aa54815a4485b140bf35f29a09
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-28 16:58:19 -0400

    user-roles.zod

--- apps/api/src/lib/zod/schemas/user-roles.zod.ts
+++ apps/api/src/lib/zod/schemas/user-roles.zod.ts
@@ -1,16 +1,13 @@
 import { USER_ROLES_COLUMNS } from '@/lib/drizzle/schemas';
 import { getSort } from '@/lib/zod/utils/field-helpers';
-import { limit, page, textId } from '@/lib/zod/utils/fields';
+import { limit, page } from '@/lib/zod/utils/fields';
 import { z } from 'zod';
 
 // Fields
-const id = textId;
 const sort = getSort(USER_ROLES_COLUMNS);
 
 // Schemas
-export const createUserRoleZod = z.object({ id });
 export const readAllUserRolesZod = z.object({ limit, page, sort }).partial();
 
 // Exported schema types
-export type CreateUserRoleZod = z.infer<typeof createUserRoleZod>;
 export type ReadAllUserRolesZod = z.infer<typeof readAllUserRolesZod>;
