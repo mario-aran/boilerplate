31a8ad826158fcaed2a4a7cb6925e08e0f9cac09
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-28 17:26:48 -0400

    permissions

--- apps/api/src/features/permissions/permissions.service.ts
+++ apps/api/src/features/permissions/permissions.service.ts
@@ -1,8 +1,21 @@
+import { db } from '@/lib/drizzle/db';
 import { permissionsSchema } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
-import { ReadAllPermissionsZod } from '@/lib/zod/schemas/permissions.zod';
+import {
+  CreatePermissionZod,
+  ReadAllPermissionsZod,
+} from '@/lib/zod/schemas/permissions.zod';
 
 class PermissionsService {
+  public async create(data: CreatePermissionZod) {
+    const [createdRecord] = await db
+      .insert(permissionsSchema)
+      .values(data)
+      .returning({ id: permissionsSchema.id });
+
+    return createdRecord;
+  }
+
   public async readAll({ limit, page, sort }: ReadAllPermissionsZod) {
     return queryPaginatedData({
       schema: permissionsSchema,
--- apps/api/src/lib/zod/schemas/permissions.zod.ts
+++ apps/api/src/lib/zod/schemas/permissions.zod.ts
@@ -1,13 +1,16 @@
 import { PERMISSIONS_COLUMNS } from '@/lib/drizzle/schemas';
 import { getSort } from '@/lib/zod/utils/field-helpers';
-import { limit, page } from '@/lib/zod/utils/fields';
+import { limit, page, textId } from '@/lib/zod/utils/fields';
 import { z } from 'zod';
 
 // Fields
+const id = textId;
 const sort = getSort(PERMISSIONS_COLUMNS);
 
 // Schemas
+export const createPermissionZod = z.object({ id });
 export const readAllPermissionsZod = z.object({ limit, page, sort }).partial();
 
 // Exported schema types
+export type CreatePermissionZod = z.infer<typeof createPermissionZod>;
 export type ReadAllPermissionsZod = z.infer<typeof readAllPermissionsZod>;
