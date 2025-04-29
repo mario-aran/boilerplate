e66f94f685f9f3d4625487a1d40b3bd56f9f3583
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-29 15:50:56 -0400

    chores

--- apps/api/src/features/user-roles/user-roles.service.ts
+++ apps/api/src/features/user-roles/user-roles.service.ts
@@ -1,10 +1,15 @@
 import { db } from '@/lib/drizzle/db';
-import { userRolesSchema } from '@/lib/drizzle/schemas';
+import {
+  userRolesSchema,
+  userRolesToPermissionsSchema,
+} from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
 import {
   CreateUserRoleZod,
   ReadAllUserRolesZod,
+  UpdateUserRoleZod,
 } from '@/lib/zod/schemas/user-roles.zod';
+import { eq } from 'drizzle-orm';
 
 class UserRolesService {
   public async create(data: CreateUserRoleZod) {
@@ -22,6 +27,58 @@ class UserRolesService {
       limit,
       sort,
       page,
+    });
+  }
+
+  public async read(id: string) {
+    // Check record
+    const record = await db.query.userRolesSchema.findFirst({
+      with: {
+        userRolesToPermissions: {
+          columns: { permissionId: true },
+        },
+      },
+      where: eq(userRolesSchema.id, id),
+    });
+    if (!record) return null;
+
+    // Flatten info
+    const { userRolesToPermissions, ...restOfRecord } = record;
+    const permissionIds = userRolesToPermissions.map(
+      ({ permissionId }) => permissionId,
+    );
+
+    return { ...restOfRecord, permissionIds };
+  }
+
+  public async update(
+    id: string,
+    { permissionIds, ...restOfData }: UpdateUserRoleZod,
+  ) {
+    return db.transaction(async (tx) => {
+      // Check permissionIds
+      if (permissionIds) {
+        // Delete old permissions from userRolesToPermissionsSchema
+        await tx
+          .delete(userRolesToPermissionsSchema)
+          .where(eq(userRolesToPermissionsSchema.userRoleId, id));
+
+        // Insert new permissions into userRolesToPermissionsSchema
+        const values = permissionIds.map((permissionId) => ({
+          userRoleId: id,
+          permissionId,
+        }));
+        await tx.insert(userRolesToPermissionsSchema).values(values);
+      }
+
+      // Update userRoleSchema values
+      const [updatedRecord] = await tx
+        .update(userRolesSchema)
+        .set(restOfData)
+        .where(eq(userRolesSchema.id, id))
+        .returning({ id: userRolesSchema.id });
+
+      return updatedRecord;
     });
   }
 }
