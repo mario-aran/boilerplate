0e042aa094c497e20b96146a3b544e8a376988cc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-29 17:25:04 -0400

    chores

--- apps/api/src/constants/user-roles.ts
+++ apps/api/src/constants/user-roles.ts
@@ -1,8 +1,8 @@
 export const USER_ROLES = {
+  SUPER_ADMIN: 'super_admin',
   USER: 'user',
-  ADMIN: 'admin',
 } as const;
 
-export const USER_ROLES_VALUES = [...new Set(Object.values(USER_ROLES))] as [
+export const USER_ROLE_VALUES = [...new Set(Object.values(USER_ROLES))] as [
   (typeof USER_ROLES)[keyof typeof USER_ROLES],
 ];
