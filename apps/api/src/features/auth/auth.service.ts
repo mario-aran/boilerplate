fd639f64948b1f0b10cae23cc5ceee76e8371b48
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-29 15:37:15 -0400

    chores

--- apps/api/src/features/auth/auth.service.ts
+++ apps/api/src/features/auth/auth.service.ts
@@ -1,27 +1,11 @@
-import { USER_ROLES } from '@/constants/user-roles';
 import { db } from '@/lib/drizzle/db';
 import { usersSchema } from '@/lib/drizzle/schemas';
-import { hashPassword } from '@/lib/passport/utils/hash-password';
 import { signJwtToken } from '@/lib/passport/utils/sign-jwt-token';
-import { LoginAuthZod, RegisterAuthZod } from '@/lib/zod/schemas/auth.zod';
+import { LoginAuthZod } from '@/lib/zod/schemas/auth.zod';
 import bcrypt from 'bcryptjs';
 import { eq } from 'drizzle-orm';
 
 class AuthService {
-  public async register(data: RegisterAuthZod) {
-    const hashedPassword = await hashPassword(data.password);
-    const [createdRecord] = await db
-      .insert(usersSchema)
-      .values({
-        ...data,
-        userRoleId: USER_ROLES.USER,
-        password: hashedPassword,
-      })
-      .returning({ email: usersSchema.email });
-
-    return createdRecord;
-  }
-
   public async login({ email, password }: LoginAuthZod) {
     // Check user
     const userExists = await db.query.usersSchema.findFirst({
