6dea4db4f6592fefbd6b45fcf1da8117c9c0cf32
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-07 19:05:33 -0400

    chores

---
+++ src/scripts/run-script.ts
@@ -0,0 +1,16 @@
+import '@/config/load-dotenv';
+
+interface RunScriptParams {
+  processName: string;
+  scriptFn: () => Promise<void>;
+}
+
+export const runScript = async ({ processName, scriptFn }: RunScriptParams) => {
+  try {
+    await scriptFn();
+    console.log(`${processName} completed successfully`);
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : 'Unknown error';
+    throw new Error(`${processName} failed: ${errorMessage}`);
+  }
+};
