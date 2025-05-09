c933a42a90cd511c26280afe689f304eed989fb5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 16:47:16 -0400

    chores

--- src/lib/zod/index.ts
+++ src/lib/zod/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-imports */
 
 import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
-import * as zod from 'zod';
+import { z } from 'zod';
 
-extendZodWithOpenApi(zod.z); // Adds ".openapi()" support to zod
+extendZodWithOpenApi(z); // Adds ".openapi()" support to zod
 export * from 'zod'; // Re-export everything from Zod
