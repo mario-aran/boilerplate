cd95415e9e653ffe5f7c57f0311634966cb82fd7
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 15:11:34 -0400

    chores

--- src/lib/zod/openapi-documents.ts
+++ src/lib/zod/openapi-documents.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
 import { ROUTES_V1 } from '@/constants/routes';
 import {
   OpenApiGeneratorV31,
@@ -7,19 +9,19 @@ import {
 // Registries
 export const registryV1 = new OpenAPIRegistry();
 
-export const openAPIDocumentV1 = (async () => {
-  // Load schemas into registry
-  await import('@/lib/zod/schemas/v1');
+// Load schema registry.definitions synchronously
+require('@/lib/zod/schemas/v1');
 
-  const generator = new OpenApiGeneratorV31(registryV1.definitions);
+// Generators
+const generatorV1 = new OpenApiGeneratorV31(registryV1.definitions);
 
-  return generator.generateDocument({
-    openapi: '3.1.0',
-    info: {
-      version: '1.0.0',
-      title: 'My API',
-      description: 'This is the API',
-    },
-    servers: [{ url: ROUTES_V1.API }],
-  });
-})();
+// OpenAPI docs
+export const openAPIDocumentV1 = generatorV1.generateDocument({
+  openapi: '3.1.0',
+  info: {
+    version: '1.0.0',
+    title: 'My API',
+    description: 'This is the API',
+  },
+  servers: [{ url: ROUTES_V1.API }],
+});
