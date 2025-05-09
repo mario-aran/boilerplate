8d0b6d24bdd05fd6494ca8a977f092bfdb297944
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 12:42:54 -0400

    chores

---
+++ src/lib/zod/schemas/v1/user-roles.zod.ts
@@ -0,0 +1,22 @@
+import { USER_ROLES_COLUMNS } from '@/lib/drizzle/schemas';
+import { z } from '@/lib/zod';
+import {
+  createSortSchema,
+  createUniqueArraySchema,
+} from '@/lib/zod/utils/field-helpers';
+import { limit, page, textId } from '@/lib/zod/utils/fields';
+
+// Types
+export type CreateUserRoleZod = z.infer<typeof CreateUserRoleZod>;
+export type ReadAllUserRolesZod = z.infer<typeof ReadAllUserRolesZod>;
+export type UpdateUserRoleZod = z.infer<typeof UpdateUserRoleZod>;
+
+// Fields
+const id = textId;
+const sort = createSortSchema(USER_ROLES_COLUMNS);
+const permissionIds = createUniqueArraySchema(textId);
+
+// Schemas
+export const CreateUserRoleZod = z.object({ id });
+export const ReadAllUserRolesZod = z.object({ limit, page, sort }).partial();
+export const UpdateUserRoleZod = z.object({ permissionIds }).partial();
