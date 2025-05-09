db6aa1461cbac4615cf0d8739ebc2f4477d363c4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 16:32:43 -0400

    chores

--- src/lib/zod/schemas/v1/auth.zod.ts
+++ src/lib/zod/schemas/v1/auth.zod.ts
@@ -1,12 +1,8 @@
 import { z } from '@/lib/zod';
-import { emailField, passwordField } from '@/lib/zod/utils/fields';
+import { email, password } from '@/lib/zod/utils/fields';
 
 // Types
 export type LoginAuthZod = z.infer<typeof LoginAuthZod>;
-
-// Fields with openapi info
-const email = emailField.openapi({ example: 'john.doe@example.com' });
-const password = passwordField.openapi({ example: '12345678' });
 
 // Schemas
 export const LoginAuthZod = z.object({ email, password });
