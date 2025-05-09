b1c41d801bf5942a22d34e9f4c6ee76e011736c3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 17:55:02 -0400

    chores

--- src/lib/zod/openapi/openapi-documents.ts
+++ src/lib/zod/openapi/openapi-documents.ts
@@ -1,6 +1,10 @@
+import { SERVER_PORT } from '@/config/env';
 import { ROUTES_V1 } from '@/constants/routes';
 import { OpenApiGeneratorV31 } from '@asteasolutions/zod-to-openapi';
 import { registryV1 } from './registries';
+
+// Constants
+const DEV_SERVER_URL = `http://localhost:${SERVER_PORT}`;
 
 // Generators
 const generatorV1 = new OpenApiGeneratorV31(registryV1.definitions);
@@ -13,5 +17,7 @@ export const openAPIDocumentV1 = generatorV1.generateDocument({
     title: 'My API',
     description: 'This is the API',
   },
-  servers: [{ url: ROUTES_V1.API }],
+  servers: [
+    { url: `${DEV_SERVER_URL}${ROUTES_V1.API}`, description: 'development' },
+  ],
 });
