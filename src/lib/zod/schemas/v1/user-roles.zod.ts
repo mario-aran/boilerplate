b0504049c91cdb8c748b9c31e46b91e197993db8
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 16:54:01 -0400

    chores

--- src/lib/zod/schemas/v1/user-roles.zod.ts
+++ src/lib/zod/schemas/v1/user-roles.zod.ts
@@ -1,22 +1,20 @@
-import { USER_ROLES_COLUMNS } from '@/lib/drizzle/schemas';
 import { z } from '@/lib/zod';
 import {
-  createSortSchema,
-  createUniqueArraySchema,
-} from '@/lib/zod/utils/field-helpers';
-import { limit, page, textId } from '@/lib/zod/utils/fields';
+  userRoleId as id,
+  limit,
+  page,
+  permissionIds,
+  sortUserRoles as sort,
+} from '@/lib/zod/utils/fields';
 
 // Types
-export type CreateUserRoleZod = z.infer<typeof CreateUserRoleZod>;
 export type ReadAllUserRolesZod = z.infer<typeof ReadAllUserRolesZod>;
+export type CreateUserRoleZod = z.infer<typeof CreateUserRoleZod>;
 export type UpdateUserRoleZod = z.infer<typeof UpdateUserRoleZod>;
 
-// Fields
-const id = textId;
-const sort = createSortSchema(USER_ROLES_COLUMNS);
-const permissionIds = createUniqueArraySchema(textId);
-
 // Schemas
+export const ReadAllUserRolesZod = z.object({ sort, limit, page }).partial();
 export const CreateUserRoleZod = z.object({ id });
-export const ReadAllUserRolesZod = z.object({ limit, page, sort }).partial();
 export const UpdateUserRoleZod = z.object({ permissionIds }).partial();
+
+// OpenAPI registries
