f81db58b168a6753ad0f9ada3cf8eab0d794e3cc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-09 15:24:57 -0400

    chores

--- src/config/load-dotenv.ts
+++ src/config/load-dotenv.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 
-// Load dotenv only in non-production environments
+// Guard: Load dotenv only in non-production environments
 if (process.env.NODE_ENV !== 'production') {
+  // Load dotenv synchronously
   require('dotenv').config();
 }
