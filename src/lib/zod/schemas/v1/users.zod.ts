7809b84df3e7bff584e74358cf4cfd016c6c5814
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-12 17:25:45 -0400

    chores

--- src/lib/zod/schemas/v1/users.zod.ts
+++ src/lib/zod/schemas/v1/users.zod.ts
@@ -1,6 +1,4 @@
-import { HTTP_STATUS } from '@/constants/http-status';
 import { z } from '@/lib/zod';
-import { registryV1 } from '@/lib/zod/openapi/registries';
 import {
   email,
   firstName,
@@ -33,20 +31,3 @@ export const UpdateUserZod = z
 export const UpdateUserPasswordZod = z.object({ password });
 
 // OpenAPI registries
-registryV1.registerPath({
-  tags: ['users'],
-  method: 'get',
-  path: '/users',
-  summary: 'Get all users',
-  request: { query: ReadAllUsersZod },
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: 'Array of objects with user data.',
-      content: {
-        'application/json': {
-          schema: {},
-        },
-      },
-    },
-  },
-});
