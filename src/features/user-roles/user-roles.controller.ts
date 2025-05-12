aac7aa57ecac5f8fe64f7d3229ce87f1567ef4af
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-12 16:18:14 -0400

    user-roles

--- src/features/user-roles/user-roles.controller.ts
+++ src/features/user-roles/user-roles.controller.ts
@@ -1,14 +1,36 @@
-import { ReadAllUserRolesZod } from '@/lib/zod/schemas/v1/user-roles.zod';
+import { HTTP_STATUS } from '@/constants/http-status';
+import { HttpError } from '@/utils/http-error';
 import { Request, Response } from 'express';
 import { userRolesService } from './user-roles.service';
 
 class UserRolesController {
-  public async readAll(
-    req: Request<unknown, unknown, unknown, ReadAllUserRolesZod>,
-    res: Response,
-  ) {
+  public async readAll(req: Request, res: Response) {
     const results = await userRolesService.readAll(req.query);
     res.json(results);
+  }
+
+  public async read(req: Request, res: Response) {
+    const record = await userRolesService.read(req.params.id);
+    if (!record) this.throwNotFoundHttpError();
+
+    res.json(record);
+  }
+
+  public async update(req: Request, res: Response) {
+    const updatedRecord = await userRolesService.update(
+      req.params.id,
+      req.body,
+    );
+    if (!updatedRecord) this.throwNotFoundHttpError();
+
+    res.json({ message: `User role ${updatedRecord.id} updated successfully` });
+  }
+
+  private throwNotFoundHttpError(): never {
+    throw new HttpError({
+      status: HTTP_STATUS.NOT_FOUND,
+      message: 'User role not found',
+    });
   }
 }
 
--- src/features/user-roles/user-roles.service.ts
+++ src/features/user-roles/user-roles.service.ts
@@ -5,50 +5,31 @@ import {
 } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
 import {
-  CreateUserRoleZod,
   ReadAllUserRolesZod,
   UpdateUserRoleZod,
 } from '@/lib/zod/schemas/v1/user-roles.zod';
 import { eq } from 'drizzle-orm';
 
 class UserRolesService {
-  public async create(data: CreateUserRoleZod) {
-    const [createdRecord] = await db
-      .insert(userRolesSchema)
-      .values(data)
-      .returning({ id: userRolesSchema.id });
-
-    return createdRecord;
-  }
-
   public async readAll({ limit, page, sort }: ReadAllUserRolesZod) {
-    return queryPaginatedData({
-      schema: userRolesSchema,
-      limit,
-      sort,
-      page,
-    });
+    return queryPaginatedData({ schema: userRolesSchema, limit, sort, page });
   }
 
   public async read(id: string) {
-    // Check record
     const record = await db.query.userRolesSchema.findFirst({
-      with: {
-        userRolesToPermissions: {
-          columns: { permissionId: true },
-        },
-      },
+      with: { userRolesToPermissions: { columns: { permissionId: true } } },
       where: eq(userRolesSchema.id, id),
     });
     if (!record) return null;
 
     // Flatten info
     const { userRolesToPermissions, ...restOfRecord } = record;
-    const permissionIds = userRolesToPermissions.map(
-      ({ permissionId }) => permissionId,
-    );
-
-    return { ...restOfRecord, permissionIds };
+    return {
+      ...restOfRecord,
+      permissionIds: userRolesToPermissions.map(
+        ({ permissionId }) => permissionId,
+      ),
+    };
   }
 
   public async update(
@@ -56,14 +37,13 @@ class UserRolesService {
     { permissionIds, ...restOfData }: UpdateUserRoleZod,
   ) {
     return db.transaction(async (tx) => {
-      // Check permissionIds
       if (permissionIds) {
-        // Delete old permissions from userRolesToPermissionsSchema
+        // Delete old permissions
         await tx
           .delete(userRolesToPermissionsSchema)
           .where(eq(userRolesToPermissionsSchema.userRoleId, id));
 
-        // Insert new permissions into userRolesToPermissionsSchema
+        // Insert new permissions
         const values = permissionIds.map((permissionId) => ({
           userRoleId: id,
           permissionId,
@@ -71,13 +51,12 @@ class UserRolesService {
         await tx.insert(userRolesToPermissionsSchema).values(values);
       }
 
-      // Update userRoleSchema values
+      // Update values
       const [updatedRecord] = await tx
         .update(userRolesSchema)
         .set(restOfData)
         .where(eq(userRolesSchema.id, id))
         .returning({ id: userRolesSchema.id });
-
       return updatedRecord;
     });
   }
