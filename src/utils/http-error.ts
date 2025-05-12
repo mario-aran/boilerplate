20575809584967f445c35da1d049d4d562b306b3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-12 15:46:29 -0400

    http-error

--- src/utils/http-error.ts
+++ src/utils/http-error.ts
@@ -1,12 +1,25 @@
-export class HttpError extends Error {
-  public status: number | undefined;
-  public details: unknown;
+// Types
+type ValidationError = Record<string, string>;
 
-  constructor(status: number, message = '', details: unknown = undefined) {
+interface HttpErrorOptions {
+  status: number;
+  message?: string;
+  validationErrors?: ValidationError[];
+}
+
+export class HttpError extends Error {
+  public status: number;
+  public validationErrors?: ValidationError[];
+
+  constructor({ status, message = '', validationErrors }: HttpErrorOptions) {
     super(message);
     this.name = this.constructor.name;
     this.status = status;
-    this.details = details;
+    this.validationErrors = validationErrors;
     Error.captureStackTrace(this, HttpError);
+  }
+
+  static throw({ status, message = '', validationErrors }: HttpErrorOptions) {
+    throw new HttpError({ status, message, validationErrors });
   }
 }
