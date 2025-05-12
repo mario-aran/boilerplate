feb57bc41d7cf396654461deb1b8e61c6c482c51
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-12 17:26:16 -0400

    chores

--- src/lib/zod/schemas/v1/user-roles.zod.ts
+++ src/lib/zod/schemas/v1/user-roles.zod.ts
@@ -1,6 +1,4 @@
-import { HTTP_STATUS } from '@/constants/http-status';
 import { z } from '@/lib/zod';
-import { registryV1 } from '@/lib/zod/openapi/registries';
 import {
   limit,
   page,
@@ -17,20 +15,3 @@ export const ReadAllUserRolesZod = z.object({ sort, limit, page }).partial();
 export const UpdateUserRoleZod = z.object({ permissionIds }).partial();
 
 // OpenAPI registries
-registryV1.registerPath({
-  tags: ['user roles'],
-  method: 'get',
-  path: '/user-roles',
-  summary: 'Get all user roles',
-  request: { query: ReadAllUserRolesZod },
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: 'Array of user role objects',
-      content: {
-        'application/json': {
-          schema: {},
-        },
-      },
-    },
-  },
-});
