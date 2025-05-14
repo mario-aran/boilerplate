70d01178da2e984bf9460166f3e37ea1a7f256ed
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-14 18:59:50 -0400

    chores

---
+++ src/lib/zod/openapi/utils/responses.ts
@@ -0,0 +1,29 @@
+import { z } from '@/lib/zod';
+
+export const messageResponse = {
+  description: 'Object with message',
+  content: {
+    'application/json': {
+      schema: z.object({
+        message: z.string().openapi({ example: 'Any message' }),
+      }),
+    },
+  },
+};
+
+export const invalidInputsResponse = {
+  description: 'Object with error details',
+  content: {
+    'application/json': {
+      schema: z.object({
+        message: z.string().openapi({ example: 'Invalid inputs' }),
+        validationErrors: z.array(
+          z.object({
+            field: z.string().openapi({ example: 'id' }),
+            message: z.string().openapi({ example: 'Invalid id format' }),
+          }),
+        ),
+      }),
+    },
+  },
+};
