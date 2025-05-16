5936c09847c5b037534fe1a0202848ae390889fd
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-16 14:33:07 -0400

    chores

--- src/lib/zod/schemas/v1/auth.schema.ts
+++ src/lib/zod/schemas/v1/auth.schema.ts
@@ -1,44 +1,12 @@
-import { HTTP_STATUS } from '@/constants/http-status';
-import { OPENAPI_PATHS } from '@/constants/routes';
 import { z } from '@/lib/zod';
-import { registryV1 } from '@/lib/zod/openapi/registries';
 import { email, password } from '@/lib/zod/utils/fields';
-import {
-  getMessageResponse,
-  invalidInputsResponse,
-} from '@/lib/zod/utils/openapi-responses';
 
 // Types
 export type LoginAuth = z.infer<typeof loginAuthSchema>;
 
 // Schemas
 export const loginAuthSchema = z.object({ email, password });
+const logoutAuthSchema = z.object({});
 
 // OpenAPI registries
 const tags = ['auth'];
-
-registryV1.registerPath({
-  tags,
-  method: 'post',
-  path: OPENAPI_PATHS.AUTH_LOGIN,
-  summary: 'Login user',
-  responses: {
-    [HTTP_STATUS.OK]: getMessageResponse(
-      'Set jwt cookie and returns a message object',
-    ),
-    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
-    [HTTP_STATUS.UNAUTHORIZED]: getMessageResponse(),
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'post',
-  path: OPENAPI_PATHS.AUTH_LOGOUT,
-  summary: 'Logout user',
-  responses: {
-    [HTTP_STATUS.OK]: getMessageResponse(
-      'Clear jwt cookie and returns a message object',
-    ),
-  },
-});
--- src/lib/zod/schemas/v1/permissions.schema.ts
+++ src/lib/zod/schemas/v1/permissions.schema.ts
@@ -1,49 +1,13 @@
-import { HTTP_STATUS } from '@/constants/http-status';
-import { OPENAPI_PATHS } from '@/constants/routes';
 import { z } from '@/lib/zod';
-import { registryV1 } from '@/lib/zod/openapi/registries';
-import {
-  dateTime,
-  positiveNumber,
-  sortPermissions,
-  textId,
-} from '@/lib/zod/utils/fields';
-import { invalidInputsResponse } from '@/lib/zod/utils/openapi-responses';
+import { limit, page, sortPermissions as sort } from '@/lib/zod/utils/fields';
 
 // Types
 export type GetAllPermissions = z.infer<typeof getAllPermissionsSchema>;
-
-// Fields
-const sort = sortPermissions;
-const limit = positiveNumber;
-const page = positiveNumber;
-const id = textId;
-const createdAt = dateTime;
-const updatedAt = dateTime;
 
 // Schemas
 export const getAllPermissionsSchema = z
   .object({ sort, limit, page })
   .partial();
 
-const permissionResponseSchema = z.object({ id, createdAt, updatedAt });
-
 // OpenAPI registries
 const tags = ['permissions'];
-
-registryV1.registerPath({
-  tags,
-  method: 'get',
-  path: OPENAPI_PATHS.PERMISSIONS,
-  summary: 'Get all permissions',
-  request: { query: getAllPermissionsSchema },
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: 'Array of permission objects',
-      content: {
-        'application/json': { schema: permissionResponseSchema.array() },
-      },
-    },
-    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
-  },
-});
