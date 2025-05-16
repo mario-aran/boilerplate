4c02458c5137d91746ab1563cae3d838d70ebc39
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-16 14:44:01 -0400

    chores

--- src/lib/zod/schemas/v1/users.schema.ts
+++ src/lib/zod/schemas/v1/users.schema.ts
@@ -1,28 +1,20 @@
-import { HTTP_STATUS } from '@/constants/http-status';
-import { OPENAPI_PATHS } from '@/constants/routes';
 import { z } from '@/lib/zod';
-import { registryV1 } from '@/lib/zod/openapi/registries';
 import {
-  dateTime,
   email,
   firstName,
+  uuid as id,
   lastName,
   limit,
   page,
   password,
   search,
-  userRoleId,
-  uuid,
-} from '@/lib/zod/utils/base-fields';
-import { idSchema } from '@/lib/zod/utils/base-schemas';
-import { sortUsers as sort } from '@/lib/zod/utils/derived-fields';
-import {
-  invalidInputsResponse,
-  messageResponse,
-} from '@/lib/zod/utils/openapi-responses';
+  sortUsers as sort,
+  textId as userRoleId,
+} from '@/lib/zod/utils/fields';
 
 // Types
 export type GetAllUsers = z.infer<typeof getAllUsersSchema>;
+export type GetUser = z.infer<typeof getUserSchema>;
 export type CreateUser = z.infer<typeof createUserSchema>;
 export type UpdateUser = z.infer<typeof updateUserSchema>;
 export type UpdateUserPassword = z.infer<typeof updateUserPasswordSchema>;
@@ -31,6 +23,8 @@ export type UpdateUserPassword = z.infer<typeof updateUserPasswordSchema>;
 export const getAllUsersSchema = z
   .object({ sort, limit, page, search, userRoleId })
   .partial();
+
+export const getUserSchema = z.object({ id });
 
 export const createUserSchema = z.object({
   email,
@@ -46,94 +40,4 @@ export const updateUserSchema = z
 export const updateUserPasswordSchema = z.object({ password });
 
 // OpenAPI registries
-const getUserResponseSchema = z.object({
-  id: uuid,
-  userRoleId,
-  createdAt: dateTime,
-  updatedAt: dateTime,
-  email,
-  firstName,
-  lastName,
-});
-
-const getAllUsersResponseSchema = getUserResponseSchema.array();
-
 const tags = ['users'];
-
-registryV1.registerPath({
-  tags,
-  method: 'get',
-  path: OPENAPI_PATHS.USERS,
-  summary: 'Get all users',
-  request: { query: getAllUsersSchema },
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: 'Array of user objects',
-      content: { 'application/json': { schema: getAllUsersResponseSchema } },
-    },
-    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'get',
-  path: OPENAPI_PATHS.USERS_ID,
-  summary: 'Get user',
-  request: { params: idSchema },
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: 'User object',
-      content: { 'application/json': { schema: getUserResponseSchema } },
-    },
-    [HTTP_STATUS.NOT_FOUND]: messageResponse,
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'post',
-  path: OPENAPI_PATHS.USERS,
-  summary: 'Create user',
-  request: {
-    body: { content: { 'application/json': { schema: createUserSchema } } },
-  },
-  responses: {
-    [HTTP_STATUS.CREATED]: messageResponse,
-    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'patch',
-  path: OPENAPI_PATHS.USERS_ID,
-  summary: 'Update user',
-  request: {
-    params: idSchema,
-    body: { content: { 'application/json': { schema: updateUserSchema } } },
-  },
-  responses: {
-    [HTTP_STATUS.OK]: messageResponse,
-    [HTTP_STATUS.NOT_FOUND]: messageResponse,
-    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'patch',
-  path: OPENAPI_PATHS.USERS_ID_PASSWORD,
-  summary: 'Update user password',
-  request: {
-    params: idSchema,
-    body: {
-      content: { 'application/json': { schema: updateUserPasswordSchema } },
-    },
-  },
-  responses: {
-    [HTTP_STATUS.OK]: messageResponse,
-    [HTTP_STATUS.NOT_FOUND]: messageResponse,
-    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
-  },
-});
