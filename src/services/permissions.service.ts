10e539b9b7318d3574c82a653c546fbb559c0a1e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-16 15:50:49 -0400

    chores

--- src/services/permissions.service.ts
+++ src/services/permissions.service.ts
@@ -1,10 +1,19 @@
 import { permissionsTable } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
 import { GetAllPermissions } from '@/lib/zod/schemas/v1';
+import { ilike } from 'drizzle-orm';
 
 class PermissionsService {
-  public async getAll({ limit, page, sort }: GetAllPermissions) {
-    return queryPaginatedData({ schema: permissionsTable, limit, sort, page });
+  public async getAll({ limit, page, sort, search = '' }: GetAllPermissions) {
+    const filters = ilike(permissionsTable.id, `%${search}%`);
+
+    return queryPaginatedData({
+      schema: permissionsTable,
+      filters,
+      limit,
+      sort,
+      page,
+    });
   }
 }
 
