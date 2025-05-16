b4b2fa7f5826c91aee1c68880a619c1e703b5764
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-15 22:16:57 -0400

    chores

--- src/lib/zod/schemas/v1/permissions.schema.ts
+++ src/lib/zod/schemas/v1/permissions.schema.ts
@@ -2,16 +2,31 @@ import { HTTP_STATUS } from '@/constants/http-status';
 import { OPENAPI_PATHS } from '@/constants/routes';
 import { z } from '@/lib/zod';
 import { registryV1 } from '@/lib/zod/openapi/registries';
-import { limit, page } from '@/lib/zod/utils/base-fields';
-import { sortPermissions as sort } from '@/lib/zod/utils/derived-fields';
+import {
+  dateTime,
+  positiveNumber,
+  sortPermissions,
+  textId,
+} from '@/lib/zod/utils/fields';
+import { invalidInputsResponse } from '@/lib/zod/utils/openapi-responses';
 
 // Types
 export type GetAllPermissions = z.infer<typeof getAllPermissionsSchema>;
 
 // Schemas
 export const getAllPermissionsSchema = z
-  .object({ sort, limit, page })
+  .object({
+    sort: sortPermissions,
+    limit: positiveNumber,
+    page: positiveNumber,
+  })
   .partial();
+
+const permissionResponseSchema = z.object({
+  id: textId,
+  createdAt: dateTime,
+  updatedAt: dateTime,
+});
 
 // OpenAPI registries
 const tags = ['permissions'];
@@ -20,12 +35,15 @@ registryV1.registerPath({
   tags,
   method: 'get',
   path: OPENAPI_PATHS.PERMISSIONS,
-  summary: 'Get permissions',
+  summary: 'Get all permissions',
   request: { query: getAllPermissionsSchema },
   responses: {
     [HTTP_STATUS.OK]: {
-      description: '',
-      content: { 'application/json': { schema: {} } },
+      description: 'Array of permission objects',
+      content: {
+        'application/json': { schema: permissionResponseSchema.array() },
+      },
     },
+    [HTTP_STATUS.UNPROCESSABLE]: invalidInputsResponse,
   },
 });
