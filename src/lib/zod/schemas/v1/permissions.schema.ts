4648d6bbdcee270aafe60328bc339b55cbaacd9f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-16 16:45:37 -0400

    chores

--- src/lib/zod/schemas/v1/permissions.schema.ts
+++ src/lib/zod/schemas/v1/permissions.schema.ts
@@ -1,11 +1,16 @@
+import { PERMISSIONS_COLUMNS } from '@/lib/drizzle/schemas';
 import { z } from '@/lib/zod';
-import { limit, page, search, sortPermissions } from '@/lib/zod/utils/fields';
+import { limit, page, search } from '@/lib/zod/utils/fields';
+import { generateSortColumns } from '@/lib/zod/utils/helpers';
+import { refineUniqueValues } from '@/lib/zod/utils/refines';
 
 // Types
 export type GetAllPermissions = z.infer<typeof getAllPermissionsSchema>;
 
 // Fields
-const sort = sortPermissions;
+const sort = refineUniqueValues(
+  z.enum(generateSortColumns(PERMISSIONS_COLUMNS)).array().min(1).max(50),
+);
 
 // Schemas
 export const getAllPermissionsSchema = z
--- src/lib/zod/schemas/v1/user-roles.schema.ts
+++ src/lib/zod/schemas/v1/user-roles.schema.ts
@@ -1,12 +1,9 @@
+import { PERMISSION_VALUES } from '@/constants/permissions';
+import { USER_ROLES_COLUMNS } from '@/lib/drizzle/schemas';
 import { z } from '@/lib/zod';
-import {
-  limit,
-  page,
-  permissionIds,
-  search,
-  sortUserRoles,
-  textId,
-} from '@/lib/zod/utils/fields';
+import { limit, page, search, textId } from '@/lib/zod/utils/fields';
+import { generateSortColumns } from '@/lib/zod/utils/helpers';
+import { refineUniqueValues } from '@/lib/zod/utils/refines';
 
 // Types
 export type UserRoleId = z.infer<typeof userRoleIdSchema>;
@@ -15,7 +12,14 @@ export type UpdateUserRole = z.infer<typeof updateUserRoleSchema>;
 
 // Fields
 const id = textId;
-const sort = sortUserRoles;
+
+const sort = refineUniqueValues(
+  z.enum(generateSortColumns(USER_ROLES_COLUMNS)).array().min(1).max(50),
+);
+
+const permissionIds = refineUniqueValues(
+  textId.array().max(PERMISSION_VALUES.length),
+);
 
 // Schemas
 export const userRoleIdSchema = z.object({ id });
