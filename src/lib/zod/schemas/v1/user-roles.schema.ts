7afaf7d8b67f9b7f758797b4c5a09d2ca85dfcb3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-16 14:35:55 -0400

    chores

--- src/lib/zod/schemas/v1/user-roles.schema.ts
+++ src/lib/zod/schemas/v1/user-roles.schema.ts
@@ -1,60 +1,21 @@
-import { HTTP_STATUS } from '@/constants/http-status';
-import { OPENAPI_PATHS } from '@/constants/routes';
 import { z } from '@/lib/zod';
-import { registryV1 } from '@/lib/zod/openapi/registries';
-import { limit, page } from '@/lib/zod/utils/base-fields';
 import {
+  textId as id,
+  limit,
+  page,
   permissionIds,
   sortUserRoles as sort,
-} from '@/lib/zod/utils/derived-fields';
+} from '@/lib/zod/utils/fields';
 
 // Types
 export type GetAllUserRoles = z.infer<typeof getAllUserRolesSchema>;
+export type GetUserRole = z.infer<typeof getUserRoleSchema>;
 export type UpdateUserRole = z.infer<typeof updateUserRoleSchema>;
 
 // Schemas
 export const getAllUserRolesSchema = z.object({ sort, limit, page }).partial();
+export const getUserRoleSchema = z.object({ id });
 export const updateUserRoleSchema = z.object({ permissionIds }).partial();
 
 // OpenAPI registries
 const tags = ['user roles'];
-
-registryV1.registerPath({
-  tags,
-  method: 'get',
-  path: OPENAPI_PATHS.USER_ROLES,
-  summary: 'Get user roles',
-  request: { query: getAllUserRolesSchema },
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: '',
-      content: { 'application/json': { schema: {} } },
-    },
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'get',
-  path: OPENAPI_PATHS.USER_ROLES_ID,
-  summary: 'Get user role',
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: '',
-      content: { 'application/json': { schema: {} } },
-    },
-  },
-});
-
-registryV1.registerPath({
-  tags,
-  method: 'put',
-  path: OPENAPI_PATHS.USER_ROLES_ID,
-  summary: 'Update user role',
-  responses: {
-    [HTTP_STATUS.OK]: {
-      description: '',
-      content: { 'application/json': { schema: {} } },
-    },
-  },
-});
