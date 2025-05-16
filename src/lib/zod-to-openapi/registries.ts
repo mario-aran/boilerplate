6caf3603fd95ae5d620a31015f4af4c21e444194
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-16 17:09:17 -0400

    chores

--- src/lib/zod-to-openapi/registries.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-
-import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
-
-// Registries
-export const registryV1 = new OpenAPIRegistry();
-
-// Synchronously load schema registry definitions
-require('@/lib/zod/schemas/v1');
