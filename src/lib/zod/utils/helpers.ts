d93fb0ef303848a753a27a8d1a362709eda43f2b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-17 17:47:21 -0400

    chores

--- src/lib/zod/utils/helpers.ts
+++ src/lib/zod/utils/helpers.ts
@@ -1,11 +1,11 @@
 import { ZodArray, ZodTypeAny } from '@/lib/zod';
 
+export const getSortColumns = <T extends string>(ascColumns: T[]) =>
+  ascColumns.flatMap((column) => [column, `-${column}`]) as [T | `-${T}`];
+
 export const refineUniqueValues = <T extends ZodTypeAny>(
   arraySchema: ZodArray<T>,
 ) =>
   arraySchema.refine((values) => new Set(values).size === values.length, {
     message: 'Array cannot contain duplicate values',
   });
-
-export const getSortColumns = <T extends string>(ascColumns: T[]) =>
-  ascColumns.flatMap((column) => [column, `-${column}`]) as [T | `-${T}`];
