0aea2185d6acdbd444fa9e8eb3099666ba94dff5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-18 17:14:03 -0400

    chores

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,21 +1,30 @@
 import { NODE_ENV } from '@/config/env';
-import { HTTP_STATUS } from '@/constants/http-status';
+import { ERROR_CODES } from '@/constants/error-codes';
+import { HTTP_STATUS_CODES } from '@/constants/http-status-codes';
 import { JWT_COOKIE } from '@/lib/passport/constants';
 import { authService } from '@/services/auth.service';
 import { controllerCatchAsync } from '@/utils/controller-catch-async';
 import { HttpError } from '@/utils/http-error';
 import { Request, Response } from 'express';
 
-// Errors
-const unauthorizedHttpError = new HttpError({
-  status: HTTP_STATUS.UNAUTHORIZED,
-  message: 'Invalid credentials',
-});
-
 class AuthController {
   public login = controllerCatchAsync(async (req: Request, res: Response) => {
     const record = await authService.login(req.body);
-    if (!record) throw unauthorizedHttpError;
+
+    if ('errorCode' in record) {
+      switch (record.errorCode) {
+        case ERROR_CODES.NOT_FOUND:
+          throw new HttpError({
+            status: HTTP_STATUS_CODES.NOT_FOUND,
+            message: 'User not found',
+          });
+        case ERROR_CODES.UNAUTHORIZED:
+          throw new HttpError({
+            status: HTTP_STATUS_CODES.UNAUTHORIZED,
+            message: 'Invalid credentials',
+          });
+      }
+    }
 
     res.cookie(JWT_COOKIE, record.token, {
       httpOnly: true,
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { ERROR_CODES } from '@/constants/error-codes';
 import { db } from '@/lib/drizzle/db';
 import { usersTable } from '@/lib/drizzle/schemas';
 import { signJwtToken } from '@/lib/passport/utils';
@@ -5,15 +6,25 @@ import { LoginAuth } from '@/lib/zod/schemas/auth.schema';
 import bcrypt from 'bcryptjs';
 import { eq } from 'drizzle-orm';
 
+// Types
+type LoginResult =
+  | {
+      errorCode: typeof ERROR_CODES.NOT_FOUND | typeof ERROR_CODES.UNAUTHORIZED;
+    }
+  | { token: string };
+
 class AuthService {
-  public login = async ({ email, password }: LoginAuth) => {
+  public login = async ({
+    email,
+    password,
+  }: LoginAuth): Promise<LoginResult> => {
     const userExists = await db.query.usersTable.findFirst({
       where: eq(usersTable.email, email),
     });
-    if (!userExists) return null;
+    if (!userExists) return { errorCode: ERROR_CODES.NOT_FOUND };
 
     const passwordIsValid = await bcrypt.compare(password, userExists.password);
-    if (!passwordIsValid) return null;
+    if (!passwordIsValid) return { errorCode: ERROR_CODES.UNAUTHORIZED };
 
     return {
       token: signJwtToken({ id: userExists.id, email: userExists.email }),
