c3c81a022c222d7c63616f3719e4d8787e5c8e6a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-18 17:24:00 -0400

    chores

--- src/middleware/handle-not-found.ts
+++ src/middleware/handle-not-found.ts
@@ -1,15 +1,15 @@
-import { HTTP_STATUS } from '@/constants/http-status';
+import { HTTP_STATUS_CODES } from '@/constants/http-status-codes';
 import { HttpError } from '@/utils/http-error';
 import { NextFunction, Request, Response } from 'express';
-
-// Errors
-const notFoundHttpError = new HttpError({
-  status: HTTP_STATUS.NOT_FOUND,
-  message: 'Not found',
-});
 
 export const handleNotFound = (
   _: Request,
   _res: Response,
   next: NextFunction,
-) => next(notFoundHttpError);
+) =>
+  next(
+    new HttpError({
+      status: HTTP_STATUS_CODES.NOT_FOUND,
+      message: 'Not found',
+    }),
+  );
