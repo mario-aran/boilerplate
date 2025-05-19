3917181f757919b92472b3fc228ce1a270e6c4a7
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-19 12:46:48 -0400

    chores

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,30 +1,20 @@
-import { ERROR_CODES } from '@/constants/error-codes';
 import { db } from '@/lib/drizzle/db';
 import { usersTable } from '@/lib/drizzle/schemas';
 import { signJwtToken } from '@/lib/passport/utils';
 import { LoginAuth } from '@/lib/zod/schemas/auth.schema';
+import { NotFoundError, UnauthorizedError } from '@/utils/domain-errors';
 import bcrypt from 'bcryptjs';
 import { eq } from 'drizzle-orm';
 
-// Types
-type LoginResult =
-  | {
-      errorCode: typeof ERROR_CODES.NOT_FOUND | typeof ERROR_CODES.UNAUTHORIZED;
-    }
-  | { token: string };
-
 class AuthService {
-  public login = async ({
-    email,
-    password,
-  }: LoginAuth): Promise<LoginResult> => {
+  public login = async ({ email, password }: LoginAuth) => {
     const userExists = await db.query.usersTable.findFirst({
       where: eq(usersTable.email, email),
     });
-    if (!userExists) return { errorCode: ERROR_CODES.NOT_FOUND };
+    if (!userExists) throw new NotFoundError('User not found');
 
     const passwordIsValid = await bcrypt.compare(password, userExists.password);
-    if (!passwordIsValid) return { errorCode: ERROR_CODES.UNAUTHORIZED };
+    if (!passwordIsValid) throw new UnauthorizedError('Invalid password');
 
     return {
       token: signJwtToken({ id: userExists.id, email: userExists.email }),
