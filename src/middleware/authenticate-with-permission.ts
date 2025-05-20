2110f9c9f6b475b90dbda016853199fe3cbced08
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-20 00:08:09 -0400

    chores

--- src/middleware/authenticate-with-permission.ts
+++ src/middleware/authenticate-with-permission.ts
@@ -1,45 +1,33 @@
-import { HTTP_STATUS } from '@/constants/http-status';
 import { passport } from '@/lib/passport';
 import { JwtUser } from '@/lib/passport/types';
 import { usersService } from '@/services/users.service';
-import { HttpError } from '@/utils/http-error';
+import { ForbiddenError, UnauthorizedError } from '@/utils/errors';
 import { NextFunction, Request, Response } from 'express';
-
-// Errors
-const unauthorizedHttpError = new HttpError({
-  status: HTTP_STATUS.UNAUTHORIZED,
-  message: 'Unauthorized',
-});
-
-const forbiddenHttpError = new HttpError({
-  status: HTTP_STATUS.FORBIDDEN,
-  message: 'Forbidden',
-});
 
 export const authenticateWithPermission = (requiredPermission?: string) => {
   return (req: Request, res: Response, next: NextFunction) =>
     passport.authenticate(
       'jwt',
       { session: false },
-      async (err: unknown, user: JwtUser | undefined) => {
-        if (err) return next(err); // Failed: internal error
-        if (!user) return next(unauthorizedHttpError); // Failed: invalid or missing JWT
+      async (error: unknown, user: JwtUser | undefined) => {
+        if (error) return next(error); // Failed: internal error
+        if (!user) return next(new UnauthorizedError()); // Failed: invalid or missing JWT
 
-        // Attach user manually when using callback
-        req.user = user;
+        req.user = user; // Attach user manually when using callback
 
-        // Succeeded: user is the owner or doesn't require permission
         const userIsOwner = req.params.id === user.id;
-        if (userIsOwner || !requiredPermission) return next();
+        if (userIsOwner || !requiredPermission) return next(); // Succeeded: user is the owner or doesn't require permission
 
-        // Succeeded: user has required permission
-        const dbUser = await usersService.get({ id: user.id });
-        const hasPermission =
-          dbUser?.permissionIds.includes(requiredPermission);
-        if (hasPermission) return next();
+        try {
+          const dbUser = await usersService.get({ id: user.id });
+          const hasPermission =
+            dbUser.permissionIds.includes(requiredPermission);
+          if (hasPermission) return next(); // Succeeded: user has required permission
 
-        // Failed: user missing or lacks permission
-        return next(forbiddenHttpError);
+          return next(new ForbiddenError()); // Failed: user missing or lacks permission
+        } catch (err) {
+          return next(err); // Failed: users service error
+        }
       },
     )(req, res, next);
 };
