48d411a350f895f850dc70ffd67778bc4cb5ad45
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-24 19:24:05 -0400

    chores

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -18,28 +18,23 @@ class UsersController {
   );
 
   public create = controllerCatchAsync(async (req: Request, res: Response) => {
-    const createdRecord = await usersService.create(req.body);
+    const { email } = await usersService.create(req.body);
     res
       .status(HTTP_STATUS.CREATED)
-      .json({ message: `User ${createdRecord.email} created successfully` });
+      .json({ message: `User ${email} created successfully` });
   });
 
   public update = controllerCatchAsync(
     async (req: Request<UserId>, res: Response) => {
-      const updatedRecord = await usersService.update(req.params, req.body);
-      res.json({ message: `User ${updatedRecord.email} updated successfully` });
+      const { email } = await usersService.update(req.params, req.body);
+      res.json({ message: `User ${email} updated successfully` });
     },
   );
 
   public updatePassword = controllerCatchAsync(
     async (req: Request<UserId>, res: Response) => {
-      const updatedRecord = await usersService.updatePassword(
-        req.params,
-        req.body,
-      );
-      res.json({
-        message: `Password for user ${updatedRecord.email} updated successfully`,
-      });
+      const { email } = await usersService.updatePassword(req.params, req.body);
+      res.json({ message: `Password for user ${email} updated successfully` });
     },
   );
 }
