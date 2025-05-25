885b0a8076434afd09032e404abcceebd29cc9e5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-24 20:07:55 -0400

    chores

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,28 +1,12 @@
-import { NODE_ENV } from '@/config/env';
-import { JWT_COOKIE } from '@/lib/passport/constants';
 import { authService } from '@/services/auth.service';
 import { controllerCatchAsync } from '@/utils/controller-catch-async';
 import { Request, Response } from 'express';
 
 class AuthController {
   public login = controllerCatchAsync(async (req: Request, res: Response) => {
-    const record = await authService.login(req.body);
-
-    res.cookie(JWT_COOKIE, record.token, {
-      httpOnly: true,
-      secure: NODE_ENV === 'production',
-      sameSite: 'strict',
-      maxAge: 60 * 60 * 1000, // 1 hour
-    });
-
-    res.json({ message: 'User logged in successfully' });
+    const { token } = await authService.login(req.body);
+    res.json({ token });
   });
-
-  public logout = (_: Request, res: Response) => {
-    res.clearCookie(JWT_COOKIE);
-
-    res.json({ message: 'User logged out successfully' });
-  };
 }
 
 export const authController = new AuthController();
