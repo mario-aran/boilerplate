eaddd378e945f8cac5f721ec255c32a823910d06
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-28 18:28:10 -0400

    chores

---
+++ src/lib/swagger/utils/generate-document.ts
@@ -0,0 +1,45 @@
+import { SERVER_PORT } from '@/config/env';
+
+// Types
+interface GenerateDocumentProps {
+  version: string;
+  paths: Record<string, unknown>;
+}
+
+// Constants
+const SECURITY_SCHEME = 'bearerAuth';
+export const SECURITY = [{ [SECURITY_SCHEME]: [] }] as const;
+
+// Utils
+export const generateDocument = ({
+  version,
+  paths,
+}: GenerateDocumentProps) => ({
+  // Base
+  openapi: '3.1.0',
+  info: {
+    title: 'My API',
+    description: `API docs for version ${version}`,
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: `http://localhost:${SERVER_PORT}/api/${version}`,
+      description: 'development',
+    },
+  ],
+
+  // Auth
+  components: {
+    securitySchemes: {
+      [SECURITY_SCHEME]: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+
+  // Paths
+  paths,
+});
