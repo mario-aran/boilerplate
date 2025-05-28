0396256521649e29654b8918669f49b655485c36
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-28 16:00:49 -0400

    chores

--- src/lib/drizzle/utils/helpers.ts
+++ src/lib/drizzle/utils/helpers.ts
@@ -2,4 +2,7 @@ import { getTableColumns } from 'drizzle-orm';
 import { AnyPgTable } from 'drizzle-orm/pg-core';
 
 export const getColumnNames = <T extends AnyPgTable>(schema: T) =>
-  Object.keys(getTableColumns(schema)) as [keyof T['$inferInsert']];
+  Object.keys(getTableColumns(schema)) as [
+    keyof T['$inferInsert'],
+    ...(keyof T['$inferInsert'])[],
+  ];
