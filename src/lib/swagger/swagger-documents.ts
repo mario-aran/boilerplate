68b9b8b7000b270861f905fd2fc6e05f14e27536
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-29 15:29:18 -0400

    chores

--- src/lib/swagger/swagger-documents.ts
+++ src/lib/swagger/swagger-documents.ts
@@ -1,7 +1,6 @@
 import { pathsV1 } from './paths/v1';
 import { generateDocument } from './utils/generate-document';
 
-// Swagger documents
 export const swaggerDocumentV1 = generateDocument({
   version: 'v1',
   paths: pathsV1,
