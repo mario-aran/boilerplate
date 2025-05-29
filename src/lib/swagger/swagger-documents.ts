6aacd24d41df1c7590581d4c859607aa96ef69fc
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-05-29 16:03:20 -0400

    chores

--- src/lib/swagger/swagger-documents.ts
+++ src/lib/swagger/swagger-documents.ts
@@ -1,6 +1,85 @@
+import { SERVER_PORT } from '@/config/env';
+import { HTTP_STATUS } from '@/constants/http-status';
+import { BEARER_AUTH_COMPONENT } from './constants/components';
 import { pathsV1 } from './paths/v1';
-import { generateDocument } from './utils/generate-document';
 
+// Types
+interface GenerateDocumentProps {
+  version: string;
+  paths: Record<string, unknown>;
+}
+
+// Utils
+const generateMessageResponse = (example: string) => ({
+  description: 'Object with message',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        properties: { message: { type: 'string', example } },
+      },
+    },
+  },
+});
+
+const generateDocument = ({ version, paths }: GenerateDocumentProps) => ({
+  // Base
+  openapi: '3.1.0',
+  info: {
+    title: 'My API',
+    description: `API documentation for version ${version}`,
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: `http://localhost:${SERVER_PORT}/api/${version}`,
+      description: 'development',
+    },
+  ],
+
+  // Components
+  components: {
+    securitySchemes: {
+      [BEARER_AUTH_COMPONENT]: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+    responses: {
+      [HTTP_STATUS.NOT_FOUND]: generateMessageResponse('Not found'),
+      [HTTP_STATUS.UNAUTHORIZED]: generateMessageResponse('Unauthorized'),
+      [HTTP_STATUS.UNPROCESSABLE]: {
+        description: 'Object with message and validation error details',
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+              properties: {
+                message: { type: 'string', example: 'Invalid inputs' },
+                validationErrors: {
+                  type: 'array',
+                  items: {
+                    type: 'object',
+                    properties: {
+                      field: { type: 'string', example: 'id' },
+                      message: { type: 'string', example: 'Invalid id format' },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+
+  // Paths
+  paths,
+});
+
+// Swagger documents
 export const swaggerDocumentV1 = generateDocument({
   version: 'v1',
   paths: pathsV1,
