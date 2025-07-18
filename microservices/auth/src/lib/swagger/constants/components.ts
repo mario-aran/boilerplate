7c207dbd34bcc00b4f557a36908a9c6e49c599b8
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-18 17:35:25 -0400

    format

--- microservices/auth/src/lib/swagger/constants/components.ts
+++ microservices/auth/src/lib/swagger/constants/components.ts
@@ -1,11 +1,13 @@
-import { HTTP_STATUS } from '@/constants/http-status';
+import { StatusCodes } from 'http-status-codes';
 
 export const BEARER_AUTH_COMPONENT = 'bearerAuth';
 export const SECURITY = [{ [BEARER_AUTH_COMPONENT]: [] }] as const;
 
 const responsePath = '#/components/responses';
 export const RESPONSES = {
-  NOT_FOUND: { $ref: `${responsePath}/${HTTP_STATUS.NOT_FOUND}` },
-  UNAUTHORIZED: { $ref: `${responsePath}/${HTTP_STATUS.UNAUTHORIZED}` },
-  UNPROCESSABLE: { $ref: `${responsePath}/${HTTP_STATUS.UNPROCESSABLE}` },
+  NOT_FOUND: { $ref: `${responsePath}/${StatusCodes.NOT_FOUND}` },
+  UNAUTHORIZED: { $ref: `${responsePath}/${StatusCodes.UNAUTHORIZED}` },
+  UNPROCESSABLE: {
+    $ref: `${responsePath}/${StatusCodes.UNPROCESSABLE_ENTITY}`,
+  },
 } as const;
