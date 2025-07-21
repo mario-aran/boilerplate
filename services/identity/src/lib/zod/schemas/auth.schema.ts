effb9ff487f85ab7de438d91555a82e871a18336
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-21 17:26:39 -0400

    format

--- services/identity/src/lib/zod/schemas/auth.schema.ts
+++
@@ -1,8 +0,0 @@
-import { email, password } from '@/lib/zod/utils/fields';
-import { z } from 'zod';
-
-// Types
-export type LoginAuth = z.infer<typeof loginAuthSchema>;
-
-// Schemas
-export const loginAuthSchema = z.strictObject({ email, password });
