bd6fc34ebbaaf19c2feb397ae3e57f80de32fd22
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-21 15:50:44 -0400

    format

--- services/identity/src/lib/drizzle/schemas/permissions.schema.ts
+++ services/identity/src/lib/drizzle/schemas/permissions.schema.ts
@@ -2,7 +2,7 @@ import { createdAt, updatedAt } from '@/lib/drizzle/utils/columns';
 import { getSortColumns } from '@/lib/drizzle/utils/get-sort-columns';
 import { relations } from 'drizzle-orm';
 import { pgTable, varchar } from 'drizzle-orm/pg-core';
-import { userRolesToPermissionsTable } from './roles-to-permissions.schema';
+import { rolesToPermissionsTable } from './roles-to-permissions.schema';
 
 // Constants
 export const PERMISSIONS_TABLE_NAME = 'permissions';
@@ -15,7 +15,7 @@ export const permissionsTable = pgTable(PERMISSIONS_TABLE_NAME, {
 });
 
 export const permissionsRelations = relations(permissionsTable, ({ many }) => ({
-  userRolesToPermissions: many(userRolesToPermissionsTable),
+  rolesToPermissions: many(rolesToPermissionsTable),
 }));
 
 // Constants
