6e545f5ac784ef8d5d27f309fe4b1a09554684a7
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-21 15:36:40 -0400

    format

--- services/identity/src/lib/drizzle/schemas/index.ts
+++ services/identity/src/lib/drizzle/schemas/index.ts
@@ -1,6 +1,6 @@
 // DO NOT RENAME OR MOVE THIS FILE — used by "drizzle.config" setup.
 
 export * from './permissions.schema';
-export * from './user-roles-to-permissions.schema';
-export * from './user-roles.schema';
+export * from './roles-to-permissions.schema';
+export * from './roles.schema';
 export * from './users.schema';
