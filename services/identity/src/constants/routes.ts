3ff27ef602f9c64f136b1ea0105094d86d9f55f9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-21 15:21:49 -0400

    format

--- services/identity/src/constants/routes.ts
+++ services/identity/src/constants/routes.ts
@@ -14,52 +14,57 @@ const replaceDotIdPaths = <T extends Paths>(paths: T) => {
   return Object.fromEntries(entries) as { [K in keyof T]: ReplaceDotId<T[K]> };
 };
 
-const generateRoutes = <T extends Paths>(paths: T) => {
-  const apiPrefix = '/api';
+const addPrefixToPaths = <T extends Paths, U extends string>({
+  paths,
+  prefix,
+}: {
+  paths: T;
+  prefix: U;
+}) => {
   const entries = Object.entries(paths).map(([key, value]) => [
     key,
-    `${apiPrefix}${value}`,
+    `${prefix}${value}`,
   ]);
-  const routes = Object.fromEntries(entries) as {
-    [K in keyof T]: `${typeof apiPrefix}${T[K]}`;
+  return Object.fromEntries(entries) as {
+    [K in keyof T]: `${typeof prefix}${T[K]}`;
   };
-
-  return {
-    API_DOCS: '/api-docs',
-    API: apiPrefix,
-    ...routes,
-  } as const;
 };
 
 // Constants
 export const SEGMENTS = {
+  API_DOCS: '/api-docs',
+  API: '/api',
   ID: '/:id',
   AUTH: '/auth',
   REGISTER: '/register',
   LOGIN: '/login',
   REFRESH: '/refresh',
   LOGOUT: '/logout',
-  CHANGE_PASSWORD: '/change-password',
   USERS: '/users',
   ME: '/me',
+  PASSWORD: '/password',
   ROLES: '/roles',
   PERMISSIONS: '/permissions',
 } as const;
 
 const PATHS = {
-  AUTH: SEGMENTS.AUTH,
   AUTH_REGISTER: `${SEGMENTS.AUTH}${SEGMENTS.REGISTER}`,
   AUTH_LOGIN: `${SEGMENTS.AUTH}${SEGMENTS.LOGIN}`,
   AUTH_REFRESH: `${SEGMENTS.AUTH}${SEGMENTS.REFRESH}`,
   AUTH_LOGOUT: `${SEGMENTS.AUTH}${SEGMENTS.LOGOUT}`,
-  AUTH_CHANGE_PASSWORD: `${SEGMENTS.AUTH}${SEGMENTS.CHANGE_PASSWORD}`,
+  USERS_ME: `${SEGMENTS.USERS}${SEGMENTS.ME}`,
+  USERS_ME_PASSWORD: `${SEGMENTS.AUTH}${SEGMENTS.PASSWORD}`,
   USERS: SEGMENTS.USERS,
   USERS_ID: `${SEGMENTS.USERS}${SEGMENTS.ID}`,
-  USERS_ME: `${SEGMENTS.USERS}${SEGMENTS.ME}`,
   ROLES: SEGMENTS.ROLES,
   ROLES_ID: `${SEGMENTS.ROLES}${SEGMENTS.ID}`,
   PERMISSIONS: SEGMENTS.PERMISSIONS,
 } as const;
 
 export const SWAGGER_PATHS = replaceDotIdPaths(PATHS);
-export const ROUTES = generateRoutes(PATHS);
+
+export const ROUTES = {
+  API_DOCS: SEGMENTS.API_DOCS,
+  API: SEGMENTS.API,
+  ...addPrefixToPaths({ paths: PATHS, prefix: SEGMENTS.API }),
+};
