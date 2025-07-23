ea30de3178dedba67d956ddd5a7370f372308dd1
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-23 19:22:16 -0400

    format

---
+++ services/identity/src/constants/permissions.ts
@@ -0,0 +1,16 @@
+// Types
+export type Permissions = (typeof PERMISSIONS)[keyof typeof PERMISSIONS];
+
+// Constants
+export const PERMISSIONS = {
+  READ_USERS: 'read_users',
+  READ_USER: 'read_user',
+  CREATE_USER: 'create_user',
+  UPDATE_USER: 'update_user',
+  UPDATE_USER_PASSWORD: 'update_user_password',
+} as const;
+
+export const SYSTEM_PERMISSION_VALUES = Object.values(PERMISSIONS) as [
+  Permissions,
+  ...Permissions[],
+];
