2a4b42ae3a9593e0802687dd364c1def6c964cd4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-23 16:08:34 -0400

    format

--- services/identity/src/controllers/auth.controller.ts
+++ services/identity/src/controllers/auth.controller.ts
@@ -1,12 +1,16 @@
-import { authService } from '@/services/auth.service';
+import { usersService } from '@/services/users.service';
 import { controllerCatchAsync } from '@/utils/controller-catch-async';
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 class AuthController {
   public register = controllerCatchAsync(
     async (req: Request, res: Response) => {
-      const result = await authService.register(req.body);
-      res.json(result);
+      const result = await usersService.create(req.body);
+
+      res
+        .status(StatusCodes.CREATED)
+        .json({ message: `User ${result.email} created successfully` });
     },
   );
 }
