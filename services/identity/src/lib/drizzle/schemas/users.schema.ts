b8999e1d181b7b18c63feca53e8ebe2068f4c41e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-23 16:09:23 -0400

    format

--- services/identity/src/lib/drizzle/schemas/users.schema.ts
+++ services/identity/src/lib/drizzle/schemas/users.schema.ts
@@ -5,12 +5,18 @@ import { boolean, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 // Constants
 export const USERS_TABLE_NAME = 'users';
 
+export const USERS_CONSTRAINTS = {
+  USERS_EMAIL_KEY: 'users_email_key',
+} as const;
+
 // Schema
 export const usersTable = pgTable(USERS_TABLE_NAME, {
   id,
   createdAt,
   updatedAt,
-  email: varchar('email', { length: 255 }).unique().notNull(),
+  email: varchar('email', { length: 255 })
+    .unique(USERS_CONSTRAINTS.USERS_EMAIL_KEY)
+    .notNull(),
   password: varchar('password', { length: 255 }).notNull(),
   firstName: varchar('first_name', { length: 255 }),
   lastName: varchar('last_name', { length: 255 }),
