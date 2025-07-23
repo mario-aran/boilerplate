24890ea1248c688f53ba900ce0a94e162387373d
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-23 16:36:07 -0400

    format

---
+++ services/identity/src/constants/system-roles.ts
@@ -0,0 +1,13 @@
+// Types
+type SystemRole = (typeof SYSTEM_ROLES)[keyof typeof SYSTEM_ROLES];
+
+// Constants
+export const SYSTEM_ROLES = {
+  SUPER_ADMIN: 'super_admin',
+  USER: 'user',
+} as const;
+
+export const SYSTEM_ROLE_VALUES = Object.values(SYSTEM_ROLES) as [
+  SystemRole,
+  ...SystemRole[],
+];
--- services/identity/src/lib/drizzle/utils/db-error-checks.ts
+++
@@ -1,4 +0,0 @@
-import { DatabaseError } from 'pg';
-
-export const isUniqueViolationError = (error: unknown) =>
-  error instanceof DatabaseError && error.code === '23505';
