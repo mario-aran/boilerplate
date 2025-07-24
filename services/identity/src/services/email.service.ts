f1665673e2b15eb112b937408a696eb52914bfaa
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-24 17:14:46 -0400

    format

---
+++ services/identity/src/services/email.service.ts
@@ -0,0 +1,31 @@
+import {
+  BASE_URL,
+  SMTP_HOST,
+  SMTP_PASS,
+  SMTP_PORT,
+  SMTP_USER,
+  VERIFY_EMAIL_FROM,
+} from '@/config/env';
+import { SEGMENTS } from '@/constants/routes';
+import nodemailer from 'nodemailer';
+
+class EmailService {
+  private transporter = nodemailer.createTransport({
+    host: SMTP_HOST,
+    port: SMTP_PORT,
+    auth: { user: SMTP_USER, pass: SMTP_PASS },
+  });
+
+  public sendVerificationEmail = async (email: string, emailToken: string) => {
+    const verifyEmailUrl = `${BASE_URL}${SEGMENTS.VERIFY_EMAIL}/${emailToken}`;
+
+    await this.transporter.sendMail({
+      from: VERIFY_EMAIL_FROM,
+      to: email,
+      subject: 'Verify your email address',
+      text: `Please verify your email by visiting this URL: ${verifyEmailUrl}`,
+    });
+  };
+}
+
+export const emailService = new EmailService();
