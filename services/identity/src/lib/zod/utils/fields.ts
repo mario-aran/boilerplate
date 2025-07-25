a967ead3a5312e9d34fd65a66a20ee93ab6e62d0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-25 19:24:06 -0400

    format

--- services/identity/src/lib/zod/utils/fields.ts
+++ services/identity/src/lib/zod/utils/fields.ts
@@ -1,5 +1,22 @@
 import { z } from 'zod';
 
+// Utils
+const noSpaces = (field: z.ZodString) =>
+  field.refine((val) => /^[^\s]+$/.test(val), {
+    message: 'Must not contain spaces',
+  });
+
+const lowerAlphanumUnder = (field: z.ZodString) =>
+  field.refine((val) => /^[a-z0-9_]+$/.test(val), {
+    message: 'Must be lowercase alphanumeric and may include underscores (_)',
+  });
+
+const threeDotSeparated = (field: z.ZodString) =>
+  field.refine((val) => val.split('.').length === 3, {
+    message: 'Must have three dot-separated parts',
+  });
+
+// Fields
 export const positiveInt = z.number().int().positive();
 export const stringToPositiveInt = z
   .string()
@@ -8,25 +25,8 @@ export const stringToPositiveInt = z
   .pipe(positiveInt);
 
 export const text = z.string().trim().min(1).max(60);
-
-export const textId = z
-  .string()
-  .min(4)
-  .max(12)
-  .refine((value) => /^[a-z0-9_]+$/.test(value), {
-    message: 'Must be lowercase alphanumeric and may include underscores (_)',
-  })
-  .refine((value) => value.trim() === value, {
-    message: 'No leading or trailing spaces',
-  });
-
+export const textId = noSpaces(lowerAlphanumUnder(z.string().min(4).max(12)));
 export const uuid = z.uuid();
+export const jwtToken = noSpaces(threeDotSeparated(z.string().min(1)));
 export const email = z.email().min(5).max(60);
-
-export const password = z
-  .string()
-  .min(8)
-  .max(20)
-  .refine((val) => /^[^\s]+$/.test(val), {
-    message: 'Must not contain spaces',
-  });
+export const password = noSpaces(z.string().min(8).max(20));
