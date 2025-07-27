ebad3e9d89958d07157a19f6c322726ed3e2dad5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-27 13:16:56 -0400

    format

--- services/identity/src/router/auth.route.ts
+++ services/identity/src/router/auth.route.ts
@@ -1,6 +1,10 @@
 import { SEGMENTS } from '@/constants/routes';
 import { authController } from '@/controllers/auth.controller';
-import { createUserSchema } from '@/lib/zod/schemas/users.schema';
+import {
+  loginSchema,
+  registerSchema,
+  verifyEmailSchema,
+} from '@/lib/zod/schemas/auth.schema';
 import { validateWithZod } from '@/middleware/validate-with-zod';
 import { Router } from 'express';
 
@@ -8,6 +12,18 @@ export const authRoute = Router();
 
 authRoute.post(
   SEGMENTS.REGISTER,
-  validateWithZod({ body: createUserSchema }),
+  validateWithZod({ body: registerSchema }),
   authController.register,
+);
+
+authRoute.post(
+  SEGMENTS.VERIFY_EMAIL,
+  validateWithZod({ body: verifyEmailSchema }),
+  authController.verifyEmail,
+);
+
+authRoute.post(
+  SEGMENTS.LOGIN,
+  validateWithZod({ body: loginSchema }),
+  authController.login,
 );
