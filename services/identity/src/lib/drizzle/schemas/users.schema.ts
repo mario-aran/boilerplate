303fe6681d18e98163c6a831fc45f3923162a391
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-27 10:45:48 -0400

    format

--- services/identity/src/lib/drizzle/schemas/users.schema.ts
+++ services/identity/src/lib/drizzle/schemas/users.schema.ts
@@ -1,32 +1,41 @@
+import { SYSTEM_ROLES } from '@/constants/system-roles';
 import { createdAt, id, updatedAt } from '@/lib/drizzle/utils/columns';
 import { getSortColumns } from '@/lib/drizzle/utils/get-sort-columns';
+import { relations } from 'drizzle-orm';
 import { boolean, pgTable, timestamp, varchar } from 'drizzle-orm/pg-core';
 import { rolesTable } from './roles.schema';
 
+// Types
+export type UserInsert = typeof usersTable.$inferInsert;
+export type UserSelect = typeof usersTable.$inferSelect;
+
 // Constants
 export const USERS_TABLE_NAME = 'users';
-
-export const USERS_CONSTRAINTS = {
-  USERS_EMAIL_KEY: 'users_email_key',
-} as const;
 
 // Schema
 export const usersTable = pgTable(USERS_TABLE_NAME, {
   id,
   roleId: varchar('user_role_id', { length: 255 })
+    .default(SYSTEM_ROLES.USER)
     .notNull()
     .references(() => rolesTable.id),
   createdAt,
   updatedAt,
-  email: varchar('email', { length: 255 })
-    .unique(USERS_CONSTRAINTS.USERS_EMAIL_KEY)
-    .notNull(),
+  email: varchar('email', { length: 255 }).unique().notNull(),
+  emailVerified: boolean('email_verified').default(false).notNull(),
+  emailVerifiedAt: timestamp('email_verified_at'),
+  pendingEmail: varchar('pending_email', { length: 255 }),
   password: varchar('password', { length: 255 }).notNull(),
   firstName: varchar('first_name', { length: 255 }),
   lastName: varchar('last_name', { length: 255 }),
-  isActive: boolean().default(true).notNull(),
-  deactivatedAt: timestamp('deactivated_at'),
 });
+
+export const usersRelations = relations(usersTable, ({ one }) => ({
+  role: one(rolesTable, {
+    fields: [usersTable.roleId],
+    references: [rolesTable.id],
+  }),
+}));
 
 // Constants
 export const USERS_SORT_COLUMNS_NO_PASSWORD = getSortColumns({
