e91db0df0e098138bfd2c133ca3a3b213274a50b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-27 12:46:22 -0400

    format

--- services/identity/src/utils/controller-catch-async.ts
+++ services/identity/src/utils/controller-catch-async.ts
@@ -1,12 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const controllerCatchAsync =
-  <T extends Record<string, string>>(
-    handler: (
-      req: Request<T>,
-      res: Response,
-      next: NextFunction,
-    ) => Promise<void>,
+  <T extends Request<unknown, unknown, unknown, unknown>>(
+    handler: (req: T, res: Response, next: NextFunction) => Promise<void>,
   ) =>
-  (req: Request<T>, res: Response, next: NextFunction) =>
+  (req: T, res: Response, next: NextFunction) =>
     handler(req, res, next).catch(next);
