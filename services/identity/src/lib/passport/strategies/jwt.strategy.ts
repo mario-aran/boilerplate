95ca82a58ac8bf758b99d540cd0313b24fffccf3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-28 17:41:41 -0400

    format

--- services/identity/src/lib/passport/strategies/jwt.strategy.ts
+++ services/identity/src/lib/passport/strategies/jwt.strategy.ts
@@ -1,25 +1,23 @@
-import { JWT_SECRET } from '@/config/env';
+import { JWT_ACCESS_SECRET } from '@/config/env';
 import { JwtPayload } from '@/lib/jwt/types';
 import { usersService } from '@/services/users.service';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: JWT_SECRET,
+  secretOrKey: JWT_ACCESS_SECRET,
 };
 
 export const jwtStrategy = new Strategy(
   opts,
   async (payload: JwtPayload, done) => {
     try {
-      // Failed: User not found
-      const user = await usersService.get(payload.userId);
-      if (!user) return done(null, false);
+      const user = await usersService.read(payload.userId);
 
       // Succeeded: Attached values to "req.user"
       return done(null, { id: user.id });
     } catch (err) {
-      // Failed: Internal error
+      // Failed
       return done(err, false);
     }
   },
