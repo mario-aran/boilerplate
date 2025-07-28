eba15fcd09954c4149baad8c8849b8b11c5e3688
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-28 16:41:24 -0400

    format

--- services/identity/src/services/email.service.ts
+++ services/identity/src/services/email.service.ts
@@ -1,5 +1,6 @@
 import {
   BASE_URL,
+  JWT_VERIFY_EMAIL_SECRET,
   SMTP_HOST,
   SMTP_PASS,
   SMTP_PORT,
@@ -7,13 +8,8 @@ import {
   VERIFY_EMAIL_FROM,
 } from '@/config/env';
 import { SEGMENTS } from '@/constants/routes';
+import jwt from 'jsonwebtoken';
 import nodemailer from 'nodemailer';
-
-// Types
-interface SendVerificationEmailProps {
-  email: string;
-  token: string;
-}
 
 class EmailService {
   private transporter = nodemailer.createTransport({
@@ -22,17 +18,17 @@ class EmailService {
     auth: { user: SMTP_USER, pass: SMTP_PASS },
   });
 
-  public sendVerificationEmail = async ({
-    email,
-    token,
-  }: SendVerificationEmailProps) => {
-    const verifyEmailUrl = `${BASE_URL}${SEGMENTS.VERIFY_EMAIL}/${token}`;
+  public sendVerificationEmail = async (userId: string, email: string) => {
+    const token = jwt.sign({ userId }, JWT_VERIFY_EMAIL_SECRET, {
+      expiresIn: '1d',
+    });
 
+    const tokenUrl = `${BASE_URL}${SEGMENTS.VERIFY_EMAIL}/${token}`;
     await this.transporter.sendMail({
       from: VERIFY_EMAIL_FROM,
       to: email,
       subject: 'Verify your email address',
-      text: `Please verify your email by visiting this URL: ${verifyEmailUrl}`,
+      text: `Please verify your email by visiting this URL: ${tokenUrl}`,
     });
   };
 }
