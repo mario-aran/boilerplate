5a8bb755f576c127bb850566b03f9087568aac4f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-28 18:46:51 -0400

    format

--- services/identity/src/lib/swagger/constants/components.ts
+++ services/identity/src/lib/swagger/constants/components.ts
@@ -1,13 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 
-export const BEARER_AUTH_COMPONENT = 'bearerAuth';
-export const SECURITY = [{ [BEARER_AUTH_COMPONENT]: [] }] as const;
+export const BEARER_AUTH = 'bearerAuth';
+export const SECURITY = [{ [BEARER_AUTH]: [] }] as const;
 
 const responsePath = '#/components/responses';
 export const RESPONSES = {
   NOT_FOUND: { $ref: `${responsePath}/${StatusCodes.NOT_FOUND}` },
   UNAUTHORIZED: { $ref: `${responsePath}/${StatusCodes.UNAUTHORIZED}` },
-  UNPROCESSABLE: {
+  UNPROCESSABLE_ENTITY: {
     $ref: `${responsePath}/${StatusCodes.UNPROCESSABLE_ENTITY}`,
   },
 } as const;
