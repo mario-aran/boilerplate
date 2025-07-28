756f71e768f653925676bd78b540e23816f1e9b9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-28 17:30:20 -0400

    format

--- services/identity/src/lib/zod/schemas/auth.schema.ts
+++ services/identity/src/lib/zod/schemas/auth.schema.ts
@@ -3,15 +3,21 @@ import { z } from 'zod';
 
 // Types
 export type Register = z.infer<typeof registerSchema>;
+export type ResendEmailVerification = z.infer<
+  typeof resendEmailVerificationSchema
+>;
+
 export type VerifyEmail = z.infer<typeof verifyEmailSchema>;
 export type Login = z.infer<typeof loginSchema>;
 
 // Schemas
-export const verifyEmailSchema = z.strictObject({ token });
-export const loginSchema = z.strictObject({ email, password });
-
 export const registerSchema = z.strictObject({
-  ...loginSchema.shape,
+  email,
+  password,
   firstName: text.optional(),
   lastName: text.optional(),
 });
+
+export const resendEmailVerificationSchema = z.strictObject({ email });
+export const verifyEmailSchema = z.strictObject({ token });
+export const loginSchema = z.strictObject({ email, password });
