7f7cfe62acb7904da19150950c876bb738d43621
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-29 16:23:05 -0400

    format

--- services/identity/src/config/env.ts
+++ services/identity/src/config/env.ts
@@ -18,7 +18,10 @@ export const BASE_URL = getEnv('BASE_URL');
 export const PORT = Number(getEnv('PORT'));
 export const JWT_ACCESS_SECRET = getEnv('JWT_ACCESS_SECRET');
 export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
-export const JWT_VERIFY_EMAIL_SECRET = getEnv('JWT_VERIFY_EMAIL_SECRET');
+export const JWT_EMAIL_VERIFICATION_SECRET = getEnv(
+  'JWT_EMAIL_VERIFICATION_SECRET',
+);
+
 export const DATABASE_URL = getEnv('DATABASE_URL');
 export const SMTP_HOST = getEnv('SMTP_HOST');
 export const SMTP_PORT = Number(getEnv('SMTP_PORT'));
