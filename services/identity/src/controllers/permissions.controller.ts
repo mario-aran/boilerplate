536db9415373db80e2683807874f86bbae2d4039
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-29 16:00:50 -0400

    format

---
+++ services/identity/src/controllers/permissions.controller.ts
@@ -0,0 +1,12 @@
+import { permissionsService } from '@/services/permissions';
+import { controllerCatchAsync } from '@/utils/controller-catch-async';
+import { Request, Response } from 'express';
+
+class PermissionsController {
+  public getAll = controllerCatchAsync(async (req: Request, res: Response) => {
+    const results = await permissionsService.getAll(req.query);
+    res.json(results);
+  });
+}
+
+export const permissionsController = new PermissionsController();
