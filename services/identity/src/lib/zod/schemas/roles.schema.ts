13db554a3d27bcd8c8c4822c37945a428f430ec8
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-29 18:16:22 -0400

    format

--- services/identity/src/lib/zod/schemas/roles.schema.ts
+++ services/identity/src/lib/zod/schemas/roles.schema.ts
@@ -7,7 +7,7 @@ import { z } from 'zod';
 
 // Types
 export type GetAllRoles = z.infer<typeof getAllRolesSchema>;
-export type UpdateRolePermissions = z.infer<typeof updateRolePermissionsSchema>;
+export type UpdateRole = z.infer<typeof updateRoleSchema>;
 
 // Fields
 const sort = generateSortField(ROLES_SORT_COLUMNS);
@@ -26,4 +26,4 @@ export const getAllRolesSchema = z
   })
   .partial();
 
-export const updateRolePermissionsSchema = z.strictObject({ permissionIds });
+export const updateRoleSchema = z.strictObject({ permissionIds });
