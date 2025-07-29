2d76338c89f93a57d83f324a3f3937919b197f99
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-29 19:38:29 -0400

    format

---
+++ services/identity/src/features/auth/auth.controller.ts
@@ -0,0 +1,41 @@
+import { VerifyEmail } from '@/lib/zod/schemas/auth.schema';
+import { controllerCatchAsync } from '@/utils/controller-catch-async';
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { authService } from './auth.service';
+
+class AuthController {
+  public register = controllerCatchAsync(
+    async (req: Request, res: Response) => {
+      const { email } = await authService.register(req.body);
+
+      res.status(StatusCodes.CREATED).json({
+        message: `Registration successful. Verification email sent to ${email}.`,
+      });
+    },
+  );
+
+  public resendEmailVerification = controllerCatchAsync(
+    async (req: Request, res: Response) => {
+      const { email } = await authService.resendEmailVerification(req.body);
+      res.json({ message: `Verification email sent to ${email}.` });
+    },
+  );
+
+  public verifyEmail = controllerCatchAsync(
+    async (
+      req: Request<unknown, unknown, unknown, VerifyEmail>,
+      res: Response,
+    ) => {
+      const { email } = await authService.verifyEmail(req.query);
+      res.json({ message: `Email ${email} verified successfully.` });
+    },
+  );
+
+  public login = controllerCatchAsync(async (req: Request, res: Response) => {
+    const result = await authService.login(req.body);
+    res.json(result);
+  });
+}
+
+export const authController = new AuthController();
---
+++ services/identity/src/features/auth/auth.service.ts
@@ -0,0 +1,76 @@
+import { emailService } from '@/features/email/email.service';
+import { usersService } from '@/features/users/users.service';
+import { validatePassword } from '@/lib/bcrypt/utils';
+import {
+  signAccessToken,
+  signRefreshToken,
+  verifyEmailVerificationToken,
+} from '@/lib/jwt/utils';
+import {
+  Login,
+  Register,
+  ResendEmailVerification,
+  VerifyEmail,
+} from '@/lib/zod/schemas/auth.schema';
+import { HttpError } from '@/utils/http-error';
+import { StatusCodes } from 'http-status-codes';
+
+class AuthService {
+  private emailAlreadyVerifiedError = new HttpError({
+    message: 'Email already verified',
+    httpStatus: StatusCodes.CONFLICT,
+  });
+
+  public register = async (props: Register) => {
+    const { id, email } = await usersService.create(props);
+
+    await emailService.sendEmailVerification(id, email);
+
+    return { email };
+  };
+
+  public resendEmailVerification = async ({
+    email,
+  }: ResendEmailVerification) => {
+    const user = await usersService.getByEmailWithPassword(email);
+    if (user.emailVerified && !user.pendingEmail)
+      throw this.emailAlreadyVerifiedError;
+
+    const targetEmail = user.pendingEmail ?? user.email;
+    await emailService.sendEmailVerification(user.id, targetEmail);
+
+    return { email };
+  };
+
+  public verifyEmail = async ({ token }: VerifyEmail) => {
+    const { userId } = verifyEmailVerificationToken(token);
+
+    const { id, emailVerified, pendingEmail } = await usersService.get(userId);
+    if (emailVerified && !pendingEmail) throw this.emailAlreadyVerifiedError;
+
+    const updateData = {
+      emailVerifiedAt: new Date(),
+      pendingEmail: null,
+      ...(!emailVerified
+        ? { emailVerified: true }
+        : pendingEmail
+          ? { email: pendingEmail }
+          : {}),
+    };
+    const { email } = await usersService.update(id, updateData);
+    return { email };
+  };
+
+  public login = async ({ email, password }: Login) => {
+    const user = await usersService.getByEmailWithPassword(email);
+
+    await validatePassword(password, user.password);
+
+    const payload = { userId: user.id };
+    const accessToken = signAccessToken(payload);
+    const refreshToken = signRefreshToken(payload);
+    return { accessToken, refreshToken };
+  };
+}
+
+export const authService = new AuthService();
