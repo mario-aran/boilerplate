30ab1e533feaac42ec63c4598bfa20adac10de9a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-30 18:48:02 -0400

    format

--- services/identity/src/features/roles/roles.service.ts
+++ services/identity/src/features/roles/roles.service.ts
@@ -1,7 +1,12 @@
 import { db } from '@/lib/drizzle/db';
 import { rolesTable, rolesToPermissionsTable } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
-import { GetAllRoles, UpdateRole } from '@/lib/zod/schemas/roles.schema';
+import {
+  CreateRole,
+  GetAllRoles,
+  RoleId,
+  UpdateRole,
+} from '@/lib/zod/schemas/roles.schema';
 import { HttpError } from '@/utils/http-error';
 import { eq, ilike } from 'drizzle-orm';
 import { StatusCodes } from 'http-status-codes';
@@ -21,7 +26,7 @@ class RolesService {
       sort,
     });
 
-  public get = async (id: string) => {
+  public get = async (id: RoleId['id']) => {
     const records = await db.query.rolesTable.findFirst({
       with: { rolesToPermissions: { columns: { permissionId: true } } },
       where: eq(rolesTable.id, id),
@@ -36,19 +41,59 @@ class RolesService {
     return { ...restOfRecords, permissionIds };
   };
 
-  public update = async (id: string, { permissionIds }: UpdateRole) =>
+  public create = async (props: CreateRole) => {
+    const [createdRecord] = await db
+      .insert(rolesTable)
+      .values(props)
+      .returning();
+    return createdRecord;
+  };
+
+  public update = async (
+    id: RoleId['id'],
+    { permissionIds, ...restOfProps }: UpdateRole,
+  ) => {
+    // Update roles
+    if (Object.keys(restOfProps).length) {
+      await db.update(rolesTable).set(restOfProps).where(eq(rolesTable.id, id));
+    }
+
+    // Update roles to permissions
+    if (permissionIds) {
+      await this.updatePermissions(id, permissionIds);
+    }
+
+    // Get new values
+    return this.get(id);
+  };
+
+  public delete = async (id: RoleId['id']) => {
+    const [deletedRecord] = await db
+      .delete(rolesTable)
+      .where(eq(rolesTable.id, id))
+      .returning();
+    if (!deletedRecord) throw this.roleNotFoundError;
+
+    return deletedRecord;
+  };
+
+  private updatePermissions = async (id: string, permissionIds: string[]) =>
     db.transaction(async (tx) => {
-      // Remove all existing permissions for this role
+      // Delete all existing permissions for this role
       await tx
         .delete(rolesToPermissionsTable)
         .where(eq(rolesToPermissionsTable.roleId, id));
+      if (!permissionIds.length) return [];
 
       // Add new permissions for this role
+      const newPermissions = permissionIds.map((permissionId) => ({
+        roleId: id,
+        permissionId,
+      }));
       return tx
         .insert(rolesToPermissionsTable)
-        .values(
-          permissionIds.map((permissionId) => ({ roleId: id, permissionId })),
-        );
+        .values(newPermissions)
+        .returning();
     });
 }
 
