45e76a80101bb4c572f7985f03dd7d3b8c40be72
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-30 15:02:39 -0400

    format

--- services/identity/src/app.ts
+++ services/identity/src/app.ts
@@ -1,13 +1,13 @@
 import cors from 'cors';
 import express from 'express';
-import { passport } from './lib/passport';
+import { passportInit } from './features/auth/passport';
 import { router } from './router';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); // Body parser
-app.use(passport.initialize()); // Strategies, must be placed after all parsers
+app.use(passportInit); // Must be placed after all parsers
 app.use(router); // Must be placed last
 
 export { app };
--- services/identity/src/lib/passport/index.ts
+++
@@ -1,7 +0,0 @@
-import passport from 'passport';
-import { jwtStrategy } from './strategies/jwt.strategy';
-
-// Strategies
-passport.use(jwtStrategy);
-
-export { passport };
