2366e662f9a22df19cdf3d52391eb0b7c03c2753
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-30 14:13:27 -0400

    format

--- services/identity/src/scripts/seeds/seed-prod.ts
+++ services/identity/src/scripts/seeds/seed-prod.ts
@@ -1,3 +1,5 @@
+// DO NOT RENAME OR MOVE THIS FILE — used by a script in "package.json"
+
 import { scriptCatchAsync } from '@/scripts/utils/script-with-catch';
 import { seedSystemData } from './utils/seed-system-data';
 
