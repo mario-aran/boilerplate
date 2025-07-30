4352c53253c8d792af3887c4eca376e163292235
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-29 20:01:02 -0400

    format

---
+++ services/identity/src/features/users/users-seed.service.ts
@@ -0,0 +1,42 @@
+import { SYSTEM_ROLES } from '@/constants/system-roles';
+import { hashPassword } from '@/lib/bcrypt/utils';
+import { db } from '@/lib/drizzle/db';
+import {
+  UserInsert,
+  USERS_TABLE_NAME,
+  usersTable,
+} from '@/lib/drizzle/schemas';
+import { getSeedMessage } from '@/utils/get-seed-message';
+
+class UsersSeedService {
+  public seed = async (users: UserInsert[]) => {
+    const hashedUserPromises = users.map(({ password, ...restOfUser }) =>
+      hashPassword(password).then((hashedPassword) => ({
+        ...restOfUser,
+        password: hashedPassword,
+      })),
+    );
+    const usersWithHashedPassword = await Promise.all(hashedUserPromises);
+
+    const createdRecords = await db
+      .insert(usersTable)
+      .values(usersWithHashedPassword)
+      .onConflictDoNothing()
+      .returning({ email: usersTable.email });
+
+    const createdKeys = createdRecords.map(({ email }) => email);
+    const seedMessage = getSeedMessage(USERS_TABLE_NAME, createdKeys);
+    console.log(seedMessage);
+  };
+
+  public seedProductionData = async () =>
+    this.seed([
+      {
+        roleId: SYSTEM_ROLES.SUPER_ADMIN,
+        email: 'superadmin@superadmin.com',
+        password: SYSTEM_ROLES.SUPER_ADMIN,
+      },
+    ]);
+}
+
+export const usersSeedService = new UsersSeedService();
