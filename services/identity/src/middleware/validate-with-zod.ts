cc17352690ff45af3847da4f0ef781343f290e35
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-31 18:28:22 -0400

    format

--- services/identity/src/middleware/validate-with-zod.ts
+++ services/identity/src/middleware/validate-with-zod.ts
@@ -15,9 +15,9 @@ export const validateWithZod =
   (req: Request, _: Response, next: NextFunction) => {
     try {
       // Validate and transform request data
-      if (params) req.params = params.parse(req.params) as typeof req.params;
-      if (query) req.query = query.parse(req.query) as typeof req.query;
-      if (body) req.body = body.parse(req.body);
+      if (params) Object.assign(req.params, params.parse(req.params));
+      if (query) Object.assign(req.query, query.parse(req.query));
+      if (body) Object.assign(req.body, body.parse(req.body));
 
       // Succeeded
       return next();
