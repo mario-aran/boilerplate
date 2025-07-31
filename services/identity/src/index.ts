ce76af704bc64b9c19db0cbf65a64b7c4258296c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-07-31 14:54:48 -0400

    format

--- services/identity/src/index.ts
+++ services/identity/src/index.ts
@@ -1,7 +1,18 @@
 // DO NOT RENAME OR MOVE THIS FILE — used by a script in "package.json"
 
-import { app } from './app';
+import cors from 'cors';
+import express from 'express';
 import { PORT } from './config/env';
+import { passportInit } from './features/auth/passport';
+import { router } from './router';
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); // Body parser
+app.use(passportInit); // Must be placed after all parsers
+app.use(router); // Must be placed last
 
 // Start the server
 app
