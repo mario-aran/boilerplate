22d72096a276937e49e3408b4fa3a35aeedcabc3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-01 16:53:29 -0400

    format

--- services/identity/src/router/index.ts
+++ services/identity/src/router/index.ts
@@ -1,68 +1,24 @@
-import { BASE_URL, NODE_ENV } from '@/config/env';
+import { BASE_URL } from '@/config/env';
 import { ROUTES } from '@/constants/routes';
 import { swaggerDocument } from '@/lib/swagger/swagger-document';
 import { HttpError } from '@/utils/http-error';
-import { DrizzleQueryError } from 'drizzle-orm';
-import { NextFunction, Request, Response, Router } from 'express';
+import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import swaggerUi from 'swagger-ui-express';
 import { authRoute } from './auth.route';
 import { permissionsRoute } from './permissions.route';
 import { rolesRoute } from './roles.route';
 
-// Utils
-const notFoundHandler = (_: Request, _res: Response, next: NextFunction) =>
-  next(
-    new HttpError({ message: 'Not found', httpStatus: StatusCodes.NOT_FOUND }),
-  );
-
-const globalErrorHandler = (
-  err: Error,
-  _: Request,
-  res: Response,
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  _next: NextFunction,
-) => {
-  let message = 'Server error';
-  let httpStatus = StatusCodes.INTERNAL_SERVER_ERROR;
-  let validationErrors;
-
-  // Http error values
-  if (err instanceof HttpError) {
-    message = err.message;
-    httpStatus = err.httpStatus;
-    validationErrors = err.validationErrors;
-  }
-
-  // DB error values
-  if (err instanceof DrizzleQueryError && err.cause && 'code' in err.cause) {
-    switch (err.cause.code) {
-      case '23503':
-        message = 'Foreign key constraint error';
-        httpStatus = StatusCodes.CONFLICT;
-        break;
-      case '23505':
-        message = 'Unique key constraint error';
-        httpStatus = StatusCodes.CONFLICT;
-        break;
-      default:
-        message = 'Database error';
-    }
-  }
-
-  // Prepare response
-  const stack = NODE_ENV !== 'production' ? err.stack?.split('\n') : undefined;
-  res.status(httpStatus).json({ message, validationErrors, stack });
-};
-
-// Router
 export const router = Router();
 
-// API routes
-router.use(ROUTES.AUTH, authRoute);
-router.use(ROUTES.ROLES, rolesRoute);
-router.use(ROUTES.PERMISSIONS, permissionsRoute);
+// Root routes
+router.get('/', (_, res) => res.json({ message: 'Service is up and running' }));
 
+// "swagger-ui-express" routes
+router.use(ROUTES.API_DOCS, swaggerUi.serve);
+router.get(ROUTES.API_DOCS, swaggerUi.setup(swaggerDocument));
+
+// API routes
 router.get(ROUTES.API, (_, res) =>
   res.json({
     message: 'Identity service',
@@ -71,13 +27,13 @@ router.get(ROUTES.API, (_, res) =>
   }),
 );
 
-// "swagger-ui-express" routes
-router.use(ROUTES.API_DOCS, swaggerUi.serve);
-router.get(ROUTES.API_DOCS, swaggerUi.setup(swaggerDocument));
+router.use(ROUTES.AUTH, authRoute);
+router.use(ROUTES.ROLES, rolesRoute);
+router.use(ROUTES.PERMISSIONS, permissionsRoute);
 
-// Root routes
-router.get('/', (_, res) => res.json({ message: 'Service is up and running' }));
-
-// Middlewares
-router.use(notFoundHandler); // Must be placed after all routes
-router.use(globalErrorHandler); // Must be placed last
+// Not found middleware: must be placed after all routes
+router.use((_, _res, next) =>
+  next(
+    new HttpError({ message: 'Not found', httpStatus: StatusCodes.NOT_FOUND }),
+  ),
+);
