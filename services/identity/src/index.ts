c63d64799be254501fe1c7f72c9c68f05475555b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-05 16:26:25 -0400

    format

--- services/identity/src/index.ts
+++ services/identity/src/index.ts
@@ -4,28 +4,27 @@ import cors from 'cors';
 import express from 'express';
 import { BASE_URL, PORT } from './config/env';
 import { passportInit } from './features/auth/passport/passport-init';
-import { globalErrorHandler } from './middleware/global-error-handler';
+import { logger } from './lib/winston/logger';
+import { errorHandler } from './middleware/error-handler';
+import { morganLogger } from './middleware/morgan-logger';
 import { router } from './router';
 
 const app = express();
 
 // Middleware setup
+app.use(morganLogger);
 app.use(cors());
 app.use(express.json()); // Body parser
-app.use(passportInit); // Must be placed after all parsers
-
-// Router middleware: must be placed before global error
-app.use(router);
-
-// Global error middleware: must be placed last
-app.use(globalErrorHandler);
+app.use(passportInit); // Must be placed after express.json
+app.use(router); // Must be placed after all but before error handler
+app.use(errorHandler); // Must be placed last
 
 // Start the server
 app
   .listen(PORT, () => {
-    console.log(`Server running on: ${BASE_URL}`);
+    logger.info(`Server running on ${BASE_URL}`);
   })
   .on('error', (error) => {
-    console.error(`Server failed to start: ${error}`);
+    logger.error(`Server failed to start: ${error}`);
     process.exit(1);
   });
