61ef45f09acacfa60751a758276f524f9b1daa1e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-05 17:02:37 -0400

    format

--- services/identity/src/lib/bullmq/connection.ts
+++ services/identity/src/lib/bullmq/connection.ts
@@ -3,6 +3,7 @@ import IORedis from 'ioredis';
 
 const { hostname, port, username, password, protocol } = new URL(REDIS_URL);
 
+// Exported options for QueueScheduler and QueueEvents
 export const connectionOptions = {
   host: hostname,
   port: Number(port),
@@ -11,7 +12,7 @@ export const connectionOptions = {
   tls: protocol === 'rediss:' ? {} : undefined,
 };
 
-// Reusable connection for Queues and Workers only
+// Reusable connection for Queues and Workers
 export const connection = new IORedis({
   maxRetriesPerRequest: null, // Required for "bullmq"
   ...connectionOptions,
