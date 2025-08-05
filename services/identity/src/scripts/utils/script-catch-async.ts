88fb95ea4440df4c3801d608ea0a01c9d8808d14
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-05 17:09:19 -0400

    format

--- services/identity/src/scripts/utils/script-catch-async.ts
+++ services/identity/src/scripts/utils/script-catch-async.ts
@@ -1,10 +1,12 @@
+import { logger } from '@/lib/winston/logger';
+
 export const scriptCatchAsync = async (asyncFn: () => Promise<void>) => {
   try {
     await asyncFn();
-    console.log('Script completed successfully');
+    logger.info('Script completed successfully');
   } catch (err) {
     const errorMessage = err instanceof Error ? err.message : 'unknown error';
-    console.error(`Script failed: ${errorMessage}`);
+    logger.error(`Script failed: ${errorMessage}`);
     process.exit(1);
   }
 };
