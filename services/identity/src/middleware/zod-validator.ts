a666dcf4de506d2bce0d2f5d9152fdc86c1a63bb
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-05 16:26:51 -0400

    format

---
+++ services/identity/src/middleware/zod-validator.ts
@@ -0,0 +1,43 @@
+import { HttpError } from '@/utils/http-error';
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ZodError, ZodObject } from 'zod';
+
+// Types
+interface ZodValidatorProps {
+  params?: ZodObject;
+  query?: ZodObject;
+  body?: ZodObject;
+}
+
+export const zodValidator =
+  ({ params, query, body }: ZodValidatorProps) =>
+  (req: Request, _: Response, next: NextFunction) => {
+    try {
+      // Validate request data
+      params?.parse(req.params);
+      query?.parse(req.query);
+      body?.parse(req.body);
+
+      // Succeeded
+      return next();
+    } catch (err) {
+      // Failed: zod error
+      if (err instanceof ZodError) {
+        const validationErrors = err.issues.map((issue) => ({
+          field: `${issue.path.join('.')}`,
+          message: issue.message,
+        }));
+        return next(
+          new HttpError({
+            message: 'Unprocessable',
+            httpStatus: StatusCodes.UNPROCESSABLE_ENTITY,
+            validationErrors,
+          }),
+        );
+      }
+
+      // Failed: internal error
+      return next(err);
+    }
+  };
