0acabc9fc9b9d65266758b1a683bc65c5cefade5
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-06 10:07:50 -0400

    migrations

--- services/identity/migrations/0000_bouncy_black_bird.sql
+++ services/identity/migrations/0000_bouncy_black_bird.sql
@@ -20,7 +20,7 @@ CREATE TABLE "roles" (
 --> statement-breakpoint
 CREATE TABLE "users" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"user_role_id" varchar(255) DEFAULT 'user' NOT NULL,
+	"role_id" varchar(255) DEFAULT 'user' NOT NULL,
 	"created_at" timestamp DEFAULT now() NOT NULL,
 	"updated_at" timestamp DEFAULT now() NOT NULL,
 	"email" varchar(255) NOT NULL,
@@ -35,4 +35,4 @@ CREATE TABLE "users" (
 --> statement-breakpoint
 ALTER TABLE "roles_to_permissions" ADD CONSTRAINT "roles_to_permissions_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "roles_to_permissions" ADD CONSTRAINT "roles_to_permissions_permission_id_permissions_id_fk" FOREIGN KEY ("permission_id") REFERENCES "public"."permissions"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_user_role_id_roles_id_fk" FOREIGN KEY ("user_role_id") REFERENCES "public"."roles"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "users" ADD CONSTRAINT "users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE no action ON UPDATE no action;
--- services/identity/migrations/0001_nasty_smasher.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "user_role_id" TO "role_id";--> statement-breakpoint
-ALTER TABLE "users" DROP CONSTRAINT "users_user_role_id_roles_id_fk";
---> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_role_id_roles_id_fk" FOREIGN KEY ("role_id") REFERENCES "public"."roles"("id") ON DELETE no action ON UPDATE no action;
