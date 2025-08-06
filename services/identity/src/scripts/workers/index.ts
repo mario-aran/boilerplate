a0f163fc5e5d9f2dab23e02eb412e0073f289005
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-05 23:12:44 -0400

    format

--- services/identity/src/scripts/workers/index.ts
+++ services/identity/src/scripts/workers/index.ts
@@ -1,3 +1,5 @@
+// DO NOT RENAME OR MOVE THIS FILE — used by a script in "package.json"
+
 import { startEmailVerificationWorker } from './email-verification-worker';
 
 startEmailVerificationWorker();
