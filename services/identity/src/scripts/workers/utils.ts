781f0273d54e67606c2643a34193e4038ec223ed
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-05 23:11:51 -0400

    format

---
+++ services/identity/src/scripts/workers/utils.ts
@@ -0,0 +1,16 @@
+import { logger } from '@/lib/winston/logger';
+import { Worker } from 'bullmq';
+
+export const registerWorkerEvents = (workerName: string, worker: Worker) => {
+  // Log startup
+  logger.info(`${workerName} worker started`);
+
+  // Register events
+  worker.on('completed', (job) => {
+    logger.info(`${job.id} has completed`);
+  });
+
+  worker.on('failed', (job, err) => {
+    logger.error(`${job?.id} has failed with ${err.message}`);
+  });
+};
