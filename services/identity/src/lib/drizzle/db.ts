88a3146009bf233868a74d8d4a76825c171890c3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-06 21:59:12 -0400

    format

--- services/identity/src/lib/drizzle/db.ts
+++ services/identity/src/lib/drizzle/db.ts
@@ -1,13 +1,20 @@
-import { DATABASE_URL, NODE_ENV } from '@/config/env';
+import { DATABASE_URL } from '@/config/env';
+import { logger } from '@/lib/winston/logger';
+import { Logger } from 'drizzle-orm/logger';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import * as schemas from './schemas';
 
-const pool = new Pool({ connectionString: DATABASE_URL });
+// Utils
+class DrizzleLogger implements Logger {
+  logQuery(query: string, params: unknown[]) {
+    logger.debug(`${query} -- params: ${JSON.stringify(params)}`);
+  }
+}
 
 // Database connection
 export const db = drizzle({
-  client: pool, // DB connection with driver
-  logger: NODE_ENV !== 'production',
-  schema: schemas, // Enable usage of "drizzle.query" with all schemas
+  client: new Pool({ connectionString: DATABASE_URL }), // Connection with driver
+  schema: schemas, // Enable "drizzle.query" usage for all schemas
+  logger: new DrizzleLogger(),
 });
