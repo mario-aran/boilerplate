067a7e5db0accf6a62d789bd2ed667d464544f21
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-08 16:51:14 -0400

    format

--- services/identity/src/index.ts
+++ services/identity/src/index.ts
@@ -1,32 +1,8 @@
-// DO NOT RENAME OR MOVE THIS FILE — used by a script in "package.json"
+// DO NOT RENAME OR MOVE THIS FILE — used by "package.json"
 
-import { Server } from 'http';
-import { app } from './app';
-import { BASE_URL, PORT } from './config/env';
-import { verifyDBConnection } from './lib/drizzle/utils/verify-db-connection';
-import { logger } from './lib/logger/winston-logger';
-import { verifyRedisConnection } from './lib/redis/utils/verify-redis-connection';
+import { startServer } from './server';
 
-export const startServer = async (): Promise<Server> => {
-  // Verify connections
-  await verifyDBConnection();
-  await verifyRedisConnection();
-
-  return new Promise((resolve) => {
-    const server = app.listen(PORT, () => {
-      logger.info(`Application started successfully: ${BASE_URL}`);
-      resolve(server);
-    });
-
-    // Verify the app
-    server.on('error', (err) => {
-      logger.error(`Application failed at startup: ${err}`);
-      process.exit(1); // Exit on failure
-    });
-  });
-};
-
-// Start the server
+// Run the script
 (async () => {
   await startServer();
 })();
