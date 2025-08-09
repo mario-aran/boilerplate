9dfe05f4f899a51229b6aecdfc8c8003a46e6479
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-09 09:02:33 -0400

    format

---
+++ services/identity/src/scripts/repl/drizzle-repl.script.ts
@@ -0,0 +1,19 @@
+// DO NOT RENAME OR MOVE THIS FILE — used by "package.json"
+
+import { NODE_ENV } from '@/config/env';
+import { db } from '@/lib/drizzle/db';
+import * as schemas from '@/lib/drizzle/schemas';
+import * as orm from 'drizzle-orm';
+import repl from 'node:repl';
+
+// Guards
+if (NODE_ENV === 'production')
+  throw new Error('Script not allowed in production');
+
+// Start REPL
+const replServer = repl.start();
+
+// Expose values to REPL context
+replServer.context.db = db; // Load drizzle instance
+replServer.context.schemas = schemas; // Load drizzle schemas
+replServer.context.orm = orm; // Load drizzle orm
