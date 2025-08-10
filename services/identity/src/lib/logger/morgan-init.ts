baf784d11feaa4a13f2b0c8fc40a379d7fc870c4
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-10 14:37:36 -0400

    format

--- services/identity/src/lib/logger/morgan-init.ts
+++ services/identity/src/lib/logger/morgan-init.ts
@@ -3,6 +3,9 @@ import { logger } from './winston-logger';
 
 export const morganInit = morgan('combined', {
   stream: {
-    write: (message) => logger.http(message.trim()), // Trim to avoid morgan default newline (\n)
+    write: (message) =>
+      logger.http(
+        message.trim(), // Trim to avoid morgan default newline (\n)
+      ),
   },
 });
