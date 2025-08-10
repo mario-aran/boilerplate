9f4c5a951cc3d9aff9e84927000e7e6729df8ee9
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-10 16:23:59 -0400

    format

--- services/identity/src/scripts/workers/utils/email-verification-worker.ts
+++ services/identity/src/scripts/workers/utils/email-verification-worker.ts
@@ -1,17 +1,16 @@
 import { QUEUES } from '@/constants/queues';
 import { emailService } from '@/features/email/email.service';
-import { connection } from '@/lib/redis/connection';
+import { bullMQConnection } from '@/lib/redis/bullmq-connection';
 import { Worker } from 'bullmq';
 import { registerWorkerEvents } from './register-worker-events';
 
 export const startEmailVerificationWorker = () => {
-  // Start the worker
   const worker = new Worker(
     QUEUES.EMAIL_VERIFICATION,
     async (job) => {
       await emailService.sendEmailVerification(job.data);
     },
-    { connection },
+    { connection: bullMQConnection.connection },
   );
 
   // Events
