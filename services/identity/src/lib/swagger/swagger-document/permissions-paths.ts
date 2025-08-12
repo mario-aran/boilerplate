7b1f0fab1a5234bd9a77966d000681d29d98e9cf
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-12 19:15:09 -0400

    format

--- services/identity/src/lib/swagger/swagger-document/permissions-paths.ts
+++ services/identity/src/lib/swagger/swagger-document/permissions-paths.ts
@@ -1,5 +1,12 @@
 import { DOC_PATHS } from '@/constants/routes';
-import { RESPONSES, SECURITY } from '@/lib/swagger/constants';
+import { PERMISSIONS_EXAMPLE_COLUMNS } from '@/lib/drizzle/schemas';
+import { SECURITY } from '@/lib/swagger/constants';
+import { paginatedQueryParams } from '@/lib/swagger/utils/inputs';
+import {
+  getPaginatedResponse,
+  messageResponse,
+  unprocessableEntityResponse,
+} from '@/lib/swagger/utils/responses';
 import { StatusCodes } from 'http-status-codes';
 
 // Values
@@ -10,56 +17,11 @@ export const permissionsPaths = {
     get: {
       tags,
       security: SECURITY,
-      parameters: [
-        {
-          in: 'query',
-          name: 'limit',
-          schema: { type: 'integer', example: 1 },
-        },
-        {
-          in: 'query',
-          name: 'page',
-          schema: { type: 'integer', example: 1 },
-        },
-        {
-          in: 'query',
-          name: 'sort',
-          schema: { type: 'array', items: { type: 'string', example: '-id' } },
-          explode: true,
-        },
-        {
-          in: 'query',
-          name: 'search',
-          schema: { type: 'string', example: 'user' },
-        },
-      ],
+      parameters: paginatedQueryParams,
       responses: {
-        [StatusCodes.OK]: {
-          content: {
-            'application/json': {
-              schema: {
-                type: 'object',
-                properties: {
-                  total: { type: 'number', example: 50 },
-                  limit: { type: 'number', example: 10 },
-                  page: { type: 'number', example: 1 },
-                  prevPage: { type: 'number', nullable: true, example: null },
-                  nextPage: { type: 'number', nullable: true, example: 2 },
-                  totalPages: { type: 'number', example: 5 },
-                  data: {
-                    type: 'array',
-                    items: {
-                      type: 'object',
-                      example: {},
-                    },
-                  },
-                },
-              },
-            },
-          },
-        },
-        [StatusCodes.UNPROCESSABLE_ENTITY]: RESPONSES.UNPROCESSABLE_ENTITY,
-        [StatusCodes.UNAUTHORIZED]: RESPONSES.UNAUTHORIZED,
+        [StatusCodes.OK]: getPaginatedResponse(PERMISSIONS_EXAMPLE_COLUMNS),
+        [StatusCodes.UNAUTHORIZED]: messageResponse,
+        [StatusCodes.UNPROCESSABLE_ENTITY]: unprocessableEntityResponse,
       },
     },
   },
