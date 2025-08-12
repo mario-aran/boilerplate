5f25504946e49171883b6dadf0fb07b0360c5ce0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-12 18:22:07 -0400

    format

--- services/identity/src/lib/swagger/constants.ts
+++ services/identity/src/lib/swagger/constants.ts
@@ -1,13 +1,2 @@
-import { StatusCodes } from 'http-status-codes';
-
 export const BEARER_AUTH = 'bearerAuth';
-export const SECURITY = [{ [BEARER_AUTH]: [] }] as const;
-
-const responsePath = '#/components/responses';
-export const RESPONSES = {
-  NOT_FOUND: { $ref: `${responsePath}/${StatusCodes.NOT_FOUND}` },
-  UNAUTHORIZED: { $ref: `${responsePath}/${StatusCodes.UNAUTHORIZED}` },
-  UNPROCESSABLE_ENTITY: {
-    $ref: `${responsePath}/${StatusCodes.UNPROCESSABLE_ENTITY}`,
-  },
-} as const;
+export const SECURITY = [{ [BEARER_AUTH]: [] }];
---
+++ services/identity/src/lib/swagger/utils/responses.ts
@@ -0,0 +1,35 @@
+export const messageResponse = {
+  description: 'Object with message',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        properties: { message: { type: 'string' } },
+      },
+    },
+  },
+};
+
+export const unprocessableResponse = {
+  description: 'Object with message and validation errors',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+          validationErrors: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                field: { type: 'string', example: 'name' },
+                message: { type: 'string', example: 'Invalid input' },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
