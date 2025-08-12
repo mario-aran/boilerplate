4fce584f25ffc7b7f8f7ad817d74de944e926e88
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-12 19:56:26 -0400

    format

--- services/identity/src/controllers/roles.controller.ts
+++ services/identity/src/controllers/roles.controller.ts
@@ -1,6 +1,7 @@
 import { rolesService } from '@/features/roles/roles.service';
 import { RoleId } from '@/lib/zod/schemas/roles.schema';
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 import { controllerCatchAsync } from './utils/controller-catch-async';
 
 class RolesController {
@@ -16,7 +17,9 @@ class RolesController {
 
   create = controllerCatchAsync(async (req: Request, res: Response) => {
     const { id } = await rolesService.create(req.body);
-    res.json({ message: `Role ${id} created successfully` });
+    res
+      .status(StatusCodes.CREATED)
+      .json({ message: `Role ${id} created successfully` });
   });
 
   update = controllerCatchAsync(async (req: Request<RoleId>, res: Response) => {
