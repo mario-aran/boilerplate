2c3f4929afaef5ceee547f3194c80d3875c2a540
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-14 15:04:06 -0400

    commented wip

--- wip/users.route.ts
+++ wip/users.route.ts
@@ -1,26 +1,13 @@
-/*
-2. User Management
-/users/me	GET	Get current user profile
-/users/me	PATCH	Update current user (name, avatar, etc.)
-/users/me/password	PATCH	Change own password
-/users/me/deactivate	DELETE	Soft-delete own account
+// import { SEGMENTS } from "@/constants/routes";
+// import { Router } from "express";
 
-/admin/users	GET	[Admin] List users (filter/pagination)
-/admin/users/:id	GET	[Admin] Get user by ID
-/admin/users/:id	PATCH	[Admin] Update user (roles, status)
-/admin/users/:id/ban	POST	[Admin] Ban/unban user
-*/
+// export const usersRoute = Router();
 
-import { SEGMENTS } from "@/constants/routes";
-import { Router } from "express";
+// usersRoute.get(SEGMENTS.ME);
+// usersRoute.patch(SEGMENTS.ME);
+// usersRoute.patch(`${SEGMENTS.ME}${SEGMENTS.PASSWORD}`);
+// usersRoute.delete(SEGMENTS.ME);
 
-export const usersRoute = Router();
-
-usersRoute.get(SEGMENTS.ME);
-usersRoute.patch(SEGMENTS.ME);
-usersRoute.patch(`${SEGMENTS.ME}${SEGMENTS.PASSWORD}`);
-usersRoute.delete(SEGMENTS.ME);
-
-usersRoute.get("/");
-usersRoute.get(SEGMENTS.ID);
-usersRoute.patch(SEGMENTS.ID);
+// usersRoute.get("/");
+// usersRoute.get(SEGMENTS.ID);
+// usersRoute.patch(SEGMENTS.ID);
