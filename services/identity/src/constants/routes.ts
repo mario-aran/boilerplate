54bf2212a0d229780f3d0e69fb8ee48de6450f9a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-26 18:23:27 -0400

    routes

--- services/identity/src/constants/routes.ts
+++ services/identity/src/constants/routes.ts
@@ -6,7 +6,7 @@ type ReplaceDotId<TK extends string> = TK extends `${infer P}:id${infer S}`
   : TK;
 
 // Utils
-const replaceDotIdPaths = <T extends Paths>(paths: T) => {
+const replaceDotIds = <T extends Paths>(paths: T) => {
   const entries = Object.entries(paths).map(([key, value]) => [
     key,
     value.replace(':id', '{id}'),
@@ -14,23 +14,10 @@ const replaceDotIdPaths = <T extends Paths>(paths: T) => {
   return Object.fromEntries(entries) as { [K in keyof T]: ReplaceDotId<T[K]> };
 };
 
-const generateRoutes = <T extends Paths>(paths: T) => {
-  const keyPrefix = 'API_';
-  const valuePrefix = '/api';
-  const entries = Object.entries(paths).map(([key, value]) => [
-    `${keyPrefix}${key}`,
-    `${valuePrefix}${value}`,
-  ]);
-  const routes = Object.fromEntries(entries) as {
-    [K in keyof T as `${typeof keyPrefix}${K & string}`]: `${typeof valuePrefix}${T[K]}`;
-  };
-
-  return { API_DOCS: '/api-docs', API: valuePrefix, ...routes } as const;
-};
-
 // Constants
 export const SEGMENTS = {
   ID: '/:id',
+  DOCS: '/docs',
   AUTH: '/auth',
   REGISTER: '/register',
   RESEND_EMAIL_VERIFICATION: '/resend-email-verification',
@@ -40,8 +27,8 @@ export const SEGMENTS = {
   PERMISSIONS: '/permissions',
 } as const;
 
-const PATHS = {
-  AUTH: SEGMENTS.AUTH,
+export const ROUTES = {
+  DOCS: SEGMENTS.DOCS,
   AUTH_REGISTER: `${SEGMENTS.AUTH}${SEGMENTS.REGISTER}`,
   AUTH_RESEND_EMAIL_VERIFICATION: `${SEGMENTS.AUTH}${SEGMENTS.RESEND_EMAIL_VERIFICATION}`,
   AUTH_VERIFY_EMAIL: `${SEGMENTS.AUTH}${SEGMENTS.VERIFY_EMAIL}`,
@@ -51,5 +38,4 @@ const PATHS = {
   PERMISSIONS: SEGMENTS.PERMISSIONS,
 } as const;
 
-export const DOC_PATHS = replaceDotIdPaths(PATHS);
-export const ROUTES = generateRoutes(PATHS);
+export const SWAGGER_ROUTES = replaceDotIds(ROUTES);
