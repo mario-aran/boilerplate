d525ee786d047f5a68397155dc1f970c6e156c4b
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-26 18:36:19 -0400

    app

--- services/identity/src/app.ts
+++ services/identity/src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { passportInit } from './features/auth/passport/passport-init';
 import { morganInit } from './lib/logger/morgan-init';
 import { errorHandler } from './middleware/error-handler';
+import { notFound } from './middleware/not-found';
 import { router } from './router';
 
 export const app = express();
@@ -12,5 +13,6 @@ app.use(morganInit);
 app.use(cors());
 app.use(express.json());
 app.use(passportInit); // Must be placed after "express.json"
-app.use(router); // Must be placed after all but before error handler
-app.use(errorHandler); // Must be placed last
+app.use(router); // Must be placed after all but before not found and error handler
+app.use(notFound); // Must be placed after router
+app.use(errorHandler); // Must be the placed last
