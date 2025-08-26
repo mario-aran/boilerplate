6c460bb2366326616c0c65a89ee197f9bd05b31e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-08-26 18:05:36 -0400

    env

--- services/identity/src/config/env.ts
+++ services/identity/src/config/env.ts
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // Utils
-const getEnv = (key: string) => {
+const getRequiredEnv = (key: string) => {
   const value = process.env[key];
   if (!value) throw new Error(`Environment variable ${key} is required`);
 
@@ -13,19 +13,18 @@ const getEnv = (key: string) => {
 };
 
 // Constants
-export const NODE_ENV = getEnv('NODE_ENV');
-export const BASE_URL = getEnv('BASE_URL');
-export const PORT = Number(getEnv('PORT'));
-export const JWT_ACCESS_SECRET = getEnv('JWT_ACCESS_SECRET');
-export const JWT_REFRESH_SECRET = getEnv('JWT_REFRESH_SECRET');
-export const JWT_EMAIL_VERIFICATION_SECRET = getEnv(
+export const NODE_ENV = getRequiredEnv('NODE_ENV');
+export const BASE_URL = getRequiredEnv('BASE_URL');
+export const PORT = Number(getRequiredEnv('PORT'));
+export const JWT_ACCESS_SECRET = getRequiredEnv('JWT_ACCESS_SECRET');
+export const JWT_REFRESH_SECRET = getRequiredEnv('JWT_REFRESH_SECRET');
+export const JWT_EMAIL_VERIFICATION_SECRET = getRequiredEnv(
   'JWT_EMAIL_VERIFICATION_SECRET',
 );
-
-export const DATABASE_URL = getEnv('DATABASE_URL');
-export const REDIS_URL = getEnv('REDIS_URL');
-export const SMTP_HOST = getEnv('SMTP_HOST');
-export const SMTP_PORT = Number(getEnv('SMTP_PORT'));
-export const SMTP_USER = getEnv('SMTP_USER');
-export const SMTP_PASS = getEnv('SMTP_PASS');
-export const VERIFY_EMAIL_FROM = getEnv('VERIFY_EMAIL_FROM');
+export const DATABASE_URL = getRequiredEnv('DATABASE_URL');
+export const REDIS_URL = getRequiredEnv('REDIS_URL');
+export const SMTP_HOST = getRequiredEnv('SMTP_HOST');
+export const SMTP_PORT = Number(getRequiredEnv('SMTP_PORT'));
+export const SMTP_USER = getRequiredEnv('SMTP_USER');
+export const SMTP_PASS = getRequiredEnv('SMTP_PASS');
+export const VERIFY_EMAIL_FROM = getRequiredEnv('VERIFY_EMAIL_FROM');
