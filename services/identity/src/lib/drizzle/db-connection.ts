69b19d30b9aebca1f06ac20a70e4fb42bda42e10
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-03 18:18:01 -0400

    db

--- services/identity/src/lib/drizzle/db-connection.ts
+++ services/identity/src/lib/drizzle/db-connection.ts
@@ -23,5 +23,4 @@ class DBConnection {
   }
 }
 
-// Singleton
 export const dbConnection = new DBConnection();
--- services/identity/src/lib/drizzle/index.ts
+++ services/identity/src/lib/drizzle/index.ts
@@ -4,9 +4,6 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { dbConnection } from './db-connection';
 import * as schemas from './schemas';
 
-// Types
-export type DrizzleDb = typeof drizzleDb;
-
 // Utils
 class DrizzleLogger implements Logger {
   logQuery(query: string, params: unknown[]) {
@@ -14,7 +11,7 @@ class DrizzleLogger implements Logger {
   }
 }
 
-export const drizzleDb = drizzle({
+export const db = drizzle({
   client: dbConnection.pool,
   logger: new DrizzleLogger(),
   schema: schemas, // Enable "db.query" for all schemas
