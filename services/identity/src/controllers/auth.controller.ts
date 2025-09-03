7ad536112006cfa0a32dee330b6c2a486a039286
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-03 17:55:06 -0400

    auth.controller

--- services/identity/src/controllers/auth.controller.ts
+++ services/identity/src/controllers/auth.controller.ts
@@ -1,24 +1,22 @@
-import { AuthService } from '@/features/auth/auth.service';
+import { authService } from '@/features/auth/auth.service';
 import { VerifyEmailAuth } from '@/lib/zod/schemas/auth.schema';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { controllerCatchAsync } from './utils';
 
-export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
+class AuthController {
   verifyEmail = controllerCatchAsync(
     async (
       req: Request<unknown, unknown, unknown, VerifyEmailAuth>,
       res: Response,
     ) => {
-      const { email } = await this.authService.verifyEmail(req.query);
+      const { email } = await authService.verifyEmail(req.query);
       res.json({ message: `Email ${email} verified successfully` });
     },
   );
 
   register = controllerCatchAsync(async (req: Request, res: Response) => {
-    const { email } = await this.authService.register(req.body);
+    const { email } = await authService.register(req.body);
     res.status(StatusCodes.CREATED).json({
       message: `Registration successful. Verification will be sent to ${email} shortly`,
     });
@@ -26,15 +24,15 @@ export class AuthController {
 
   resendEmailVerification = controllerCatchAsync(
     async (req: Request, res: Response) => {
-      const { email } = await this.authService.resendEmailVerification(
-        req.body,
-      );
+      const { email } = await authService.resendEmailVerification(req.body);
       res.json({ message: `Verification will be sent to ${email} shortly` });
     },
   );
 
   login = controllerCatchAsync(async (req: Request, res: Response) => {
-    const result = await this.authService.login(req.body);
+    const result = await authService.login(req.body);
     res.json(result);
   });
 }
+
+export const authController = new AuthController();
