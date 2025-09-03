6c6b7601a6621365882f2a6069f6bbc6f5253218
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-02 21:17:29 -0400

    permissions.service

--- services/identity/src/features/permissions/permissions-seed.service.ts
+++ services/identity/src/features/permissions/permissions-seed.service.ts
@@ -1,10 +1,21 @@
 import { PERMISSION_VALUES } from '@/constants/permissions';
-import { drizzleDb } from '@/lib/drizzle';
+import { DrizzleDb } from '@/lib/drizzle';
 import { permissionsTable } from '@/lib/drizzle/schemas';
 
-class PermissionsSeedService {
+// Types
+interface PermissionsSeedServiceProps {
+  db: DrizzleDb;
+}
+
+export class PermissionsSeedService {
+  private readonly db: DrizzleDb;
+
+  constructor({ db }: PermissionsSeedServiceProps) {
+    this.db = db;
+  }
+
   async seed() {
-    const createdRecords = await drizzleDb
+    const createdRecords = await this.db
       .insert(permissionsTable)
       .values(PERMISSION_VALUES.map((id) => ({ id })))
       .onConflictDoNothing()
@@ -14,5 +25,3 @@ class PermissionsSeedService {
     return { createdKeys };
   }
 }
-
-export const permissionsSeedService = new PermissionsSeedService();
--- services/identity/src/features/permissions/permissions.service.ts
+++ services/identity/src/features/permissions/permissions.service.ts
@@ -1,12 +1,23 @@
-import { drizzleDb } from '@/lib/drizzle';
+import { DrizzleDb } from '@/lib/drizzle';
 import { permissionsTable } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
 import { GetAllPermissions } from '@/lib/zod/schemas/permissions.schema';
 import { ilike } from 'drizzle-orm';
 
-class PermissionsService {
+// Types
+interface PermissionsServiceProps {
+  db: DrizzleDb;
+}
+
+export class PermissionsService {
+  private readonly db: DrizzleDb;
+
+  constructor({ db }: PermissionsServiceProps) {
+    this.db = db;
+  }
+
   async getAll({ limit, page, sort, search = '' }: GetAllPermissions) {
-    return queryPaginatedData(drizzleDb, {
+    return queryPaginatedData(this.db, {
       schema: permissionsTable,
       filters: ilike(permissionsTable.id, `%${search}%`),
       limit,
@@ -15,5 +26,3 @@ class PermissionsService {
     });
   }
 }
-
-export const permissionsService = new PermissionsService();
