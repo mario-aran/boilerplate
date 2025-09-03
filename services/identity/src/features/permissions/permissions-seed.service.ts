45d6ae63285adfc2077f2c785bdb0b58b7ae15aa
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-03 18:13:34 -0400

    permissions

--- services/identity/src/features/permissions/permissions-seed.service.ts
+++ services/identity/src/features/permissions/permissions-seed.service.ts
@@ -1,21 +1,10 @@
 import { PERMISSION_VALUES } from '@/constants/permissions';
-import { DrizzleDb } from '@/lib/drizzle';
+import { db } from '@/lib/drizzle';
 import { permissionsTable } from '@/lib/drizzle/schemas';
 
-// Types
-interface PermissionsSeedServiceProps {
-  db: DrizzleDb;
-}
-
 export class PermissionsSeedService {
-  private readonly db: DrizzleDb;
-
-  constructor({ db }: PermissionsSeedServiceProps) {
-    this.db = db;
-  }
-
   async seed() {
-    const createdRecords = await this.db
+    const createdRecords = await db
       .insert(permissionsTable)
       .values(PERMISSION_VALUES.map((id) => ({ id })))
       .onConflictDoNothing()
@@ -25,3 +14,5 @@ export class PermissionsSeedService {
     return { createdKeys };
   }
 }
+
+export const permissionsSeedService = new PermissionsSeedService();
--- services/identity/src/features/permissions/permissions.service.ts
+++ services/identity/src/features/permissions/permissions.service.ts
@@ -1,23 +1,11 @@
-import { DrizzleDb } from '@/lib/drizzle';
 import { permissionsTable } from '@/lib/drizzle/schemas';
 import { queryPaginatedData } from '@/lib/drizzle/utils/query-paginated-data';
 import { GetAllPermissions } from '@/lib/zod/schemas/permissions.schema';
 import { ilike } from 'drizzle-orm';
 
-// Types
-interface PermissionsServiceProps {
-  db: DrizzleDb;
-}
-
-export class PermissionsService {
-  private readonly db: DrizzleDb;
-
-  constructor({ db }: PermissionsServiceProps) {
-    this.db = db;
-  }
-
+class PermissionsService {
   async getAll({ limit, page, sort, search = '' }: GetAllPermissions) {
-    return queryPaginatedData(this.db, {
+    return queryPaginatedData({
       schema: permissionsTable,
       filters: ilike(permissionsTable.id, `%${search}%`),
       limit,
@@ -26,3 +14,5 @@ export class PermissionsService {
     });
   }
 }
+
+export const permissionsService = new PermissionsService();
