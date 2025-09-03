690e1af424c332ae107a7eba657e37973855f521
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-02 21:48:44 -0400

    email-queue.service

--- services/identity/src/features/email/email-queue.service.ts
+++ services/identity/src/features/email/email-queue.service.ts
@@ -1,16 +1,20 @@
 import { QUEUES } from '@/constants/queues';
-import { bullMQConnection } from '@/lib/redis/bullmq-connection';
 import { Queue } from 'bullmq';
 import { EmailVerificationProps } from './types';
 
-class EmailQueueService {
-  private emailVerificationQueue = new Queue(QUEUES.EMAIL_VERIFICATION, {
-    connection: bullMQConnection.connection,
-  });
+// Types
+interface EmailQueueServiceProps {
+  emailVerificationQueue: Queue;
+}
 
-  async enqueueEmailVerification(props: EmailVerificationProps) {
+export class EmailQueueService {
+  private readonly emailVerificationQueue: Queue;
+
+  constructor({ emailVerificationQueue }: EmailQueueServiceProps) {
+    this.emailVerificationQueue = emailVerificationQueue;
+  }
+
+  async queueEmailVerification(props: EmailVerificationProps) {
     await this.emailVerificationQueue.add(QUEUES.EMAIL_VERIFICATION, props);
   }
 }
-
-export const emailQueueService = new EmailQueueService();
