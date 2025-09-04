ce01f1e4c4dad26f3c0ef930efee214313e038db
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-04 17:16:06 -0400

    drizzle.config.ts

--- services/identity/drizzle.config.ts
+++ services/identity/drizzle.config.ts
@@ -1,10 +1,10 @@
 import { DATABASE_URL } from '@/config/env';
 import { defineConfig } from 'drizzle-kit';
 
-// Must use a relative paths
+// Paths must be relative to project root
 export default defineConfig({
-  out: './migrations',
-  schema: './src/lib/drizzle/schemas/index.ts',
+  out: 'migrations',
+  schema: 'src/lib/drizzle/schemas/index.ts',
   dialect: 'postgresql',
   dbCredentials: { url: DATABASE_URL },
 });
