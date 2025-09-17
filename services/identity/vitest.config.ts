5a0cc74be9d1e119e47f23b1c5da5aef6a848f76
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-17 20:50:50 -0300

    vitest.config.ts

--- services/identity/vitest.config.ts
+++ services/identity/vitest.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
             'tests/integration/**/*.integration.test.ts',
           ],
           globalSetup: 'tests/config/global-setup.e2e.ts',
+          setupFiles: ['tests/config/vitest-setup.e2e.ts'],
         },
       },
     ],
