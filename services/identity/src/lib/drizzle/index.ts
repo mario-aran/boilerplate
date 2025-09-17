34971b661ec4b512f6d14273b0e024cde322896f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-17 20:51:26 -0300

    added type Db

--- services/identity/src/lib/drizzle/index.ts
+++ services/identity/src/lib/drizzle/index.ts
@@ -5,7 +5,8 @@ import { pool } from './pool';
 import * as schemas from './schemas';
 
 // Types
-export type Tx = Parameters<Parameters<typeof db.transaction>[0]>[0];
+export type Db = typeof db;
+export type Tx = Parameters<Parameters<Db['transaction']>[0]>[0];
 
 // Utils
 class DrizzleLogger implements Logger {
