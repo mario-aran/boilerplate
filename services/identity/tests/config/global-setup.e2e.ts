ffa898c3bfa0d21ee3a07ad01bc5309e9bbc8698
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-23 16:06:35 -0300

    added redis testcontainer in vitest global setup

--- services/identity/tests/config/global-setup.e2e.ts
+++ services/identity/tests/config/global-setup.e2e.ts
@@ -5,28 +5,30 @@ process.env.DOCKER_AUTH_CONFIG = '{}';
 
 // Imports
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
+import { RedisContainer } from '@testcontainers/redis';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 // Constants
 const POSTGRES_IMAGE = 'postgres:17.5-alpine';
+const REDIS_IMAGE = 'redis:8.0-alpine';
 
 export default async function globalSetup() {
   // Start Testcontainers
   const pgContainer = await new PostgreSqlContainer(POSTGRES_IMAGE).start();
+  const redisContainer = await new RedisContainer(REDIS_IMAGE).start();
 
-  // Override connections to use Testcontainers
+  // Swap connections to use Testcontainers
   process.env.DATABASE_URL = pgContainer.getConnectionUri();
+  process.env.REDIS_URL = redisContainer.getConnectionUrl();
 
-  // Prepare database
+  // Migrate and seed database
   const { db } = await import('@/lib/drizzle');
   await migrate(db, { migrationsFolder: 'migrations' }); // Paths must be relative to project root
-  await import('@/scripts/seeds/seed-dev.script'); // Seed database
+  await import('@/scripts/seeds/seed-dev.script');
 
   return async function globalTeardown() {
-    // Close connections
-    await db.$client.end();
-
-    // Stop Testcontainers
+    // Stop Testcontainers in reverse order
+    await redisContainer.stop();
     await pgContainer.stop();
   };
 }
