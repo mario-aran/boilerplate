f378e57c38a07946069911331ebf969be7ded33c
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-09-23 16:10:19 -0300

    restore specific mocks in vitest-setup

--- services/identity/tests/config/vitest-setup.e2e.ts
+++ services/identity/tests/config/vitest-setup.e2e.ts
@@ -5,20 +5,21 @@ import { pool } from '@/lib/drizzle/pool';
 import * as schemas from '@/lib/drizzle/schemas';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { PoolClient } from 'pg';
-import { afterEach, beforeEach } from 'vitest';
+import { afterEach, beforeEach, MockInstance } from 'vitest';
 
 let client: PoolClient;
+let dbSpy: MockInstance;
 
 beforeEach(async () => {
   // Start transaction
   client = await pool.connect();
   await client.query('BEGIN;');
 
-  // Replace drizzle connection with a transaction
+  // Override db connection with a transaction
   const txDb = drizzle({ client, schema: schemas });
-  vi.spyOn(drizzleModule, 'db', 'get').mockReturnValue(
-    txDb as unknown as drizzleModule.Db,
-  );
+  dbSpy = vi
+    .spyOn(drizzleModule, 'db', 'get')
+    .mockReturnValue(txDb as unknown as drizzleModule.Db);
 });
 
 afterEach(async () => {
@@ -26,6 +27,6 @@ afterEach(async () => {
   await client.query('ROLLBACK;');
   client.release();
 
-  // Remove all mocks
-  vi.restoreAllMocks();
+  // Remove mocks
+  dbSpy.mockRestore();
 });
