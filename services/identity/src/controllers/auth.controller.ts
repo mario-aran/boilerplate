525e8f4412bd60beffdfe723b89ba7636aa9ed06
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-11 23:08:41 -0300

    format

--- services/identity/src/controllers/auth.controller.ts
+++ services/identity/src/controllers/auth.controller.ts
@@ -1,18 +1,8 @@
 import { authService } from '@/features/auth/auth.service';
-import { VerifyEmailAuth } from '@/lib/zod/schemas/auth.schema';
+import { VerifyEmail } from '@/lib/zod/schemas/auth.schema';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { controllerCatchAsync } from './utils';
-
-export const verifyEmail = controllerCatchAsync(
-  async (
-    req: Request<unknown, unknown, unknown, VerifyEmailAuth>,
-    res: Response,
-  ) => {
-    const { email } = await authService.verifyEmail(req.query);
-    res.json({ message: `Email ${email} verified successfully` });
-  },
-);
 
 export const register = controllerCatchAsync(
   async (req: Request, res: Response) => {
@@ -27,6 +17,16 @@ export const resendEmailVerification = controllerCatchAsync(
   async (req: Request, res: Response) => {
     const { email } = await authService.resendEmailVerification(req.body);
     res.json({ message: `Verification will be sent to ${email} shortly` });
+  },
+);
+
+export const verifyEmail = controllerCatchAsync(
+  async (
+    req: Request<unknown, unknown, unknown, VerifyEmail>,
+    res: Response,
+  ) => {
+    const { email } = await authService.verifyEmail(req.query);
+    res.json({ message: `Email ${email} verified successfully` });
   },
 );
 
