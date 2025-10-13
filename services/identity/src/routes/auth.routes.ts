585d6c8db44fe83a84985969fa6e0d4caa074aca
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-13 10:19:05 -0300

    format

--- services/identity/src/routes/auth.routes.ts
+++ services/identity/src/routes/auth.routes.ts
@@ -1,15 +1,17 @@
 import { PATH_SEGMENTS } from '@/constants/paths';
 import {
   login,
+  refreshToken,
   register,
   resendEmailVerification,
   verifyEmail,
 } from '@/controllers/auth.controller';
 import {
-  loginAuthSchema,
-  registerAuthSchema,
-  resendEmailVerificationAuthSchema,
-  verifyEmailAuthSchema,
+  loginSchema,
+  refreshTokenSchema,
+  registerSchema,
+  resendEmailVerificationSchema,
+  verifyEmailSchema,
 } from '@/lib/zod/schemas/auth.schema';
 import { zodValidator } from '@/middleware/zod-validator';
 import { Router } from 'express';
@@ -18,24 +20,30 @@ export const authRoutes = Router();
 
 authRoutes.get(
   PATH_SEGMENTS.VERIFY_EMAIL,
-  zodValidator({ query: verifyEmailAuthSchema }),
+  zodValidator({ query: verifyEmailSchema }),
   verifyEmail,
 );
 
 authRoutes.post(
   PATH_SEGMENTS.REGISTER,
-  zodValidator({ body: registerAuthSchema }),
+  zodValidator({ body: registerSchema }),
   register,
 );
 
 authRoutes.post(
   PATH_SEGMENTS.RESEND_EMAIL_VERIFICATION,
-  zodValidator({ body: resendEmailVerificationAuthSchema }),
+  zodValidator({ body: resendEmailVerificationSchema }),
   resendEmailVerification,
 );
 
 authRoutes.post(
   PATH_SEGMENTS.LOGIN,
-  zodValidator({ body: loginAuthSchema }),
+  zodValidator({ body: loginSchema }),
   login,
+);
+
+authRoutes.post(
+  PATH_SEGMENTS.REFRESH_TOKEN,
+  zodValidator({ body: refreshTokenSchema }),
+  refreshToken,
 );
