53b1acb2545504ac91c4802b4d2817390bb8138e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-13 11:25:46 -0300

    format

--- services/identity/src/controllers/users.controller.ts
+++ services/identity/src/controllers/users.controller.ts
@@ -1,20 +1,12 @@
 import { RequestWithUser } from '@/features/auth/types';
 import { usersService } from '@/features/users/users.service';
-import { UserId } from '@/lib/zod/schemas/users.schema';
+import { controllerCatchAsync } from '@/utils/controller-catch-async';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { controllerCatchAsync } from './utils';
 
 export const getUsers = controllerCatchAsync(
   async (req: Request, res: Response) => {
-    const results = await usersService.getAll(req.query);
-    res.json(results);
-  },
-);
-
-export const getUser = controllerCatchAsync(
-  async (req: Request<UserId>, res: Response) => {
-    const result = await usersService.get(req.params.id);
+    const result = await usersService.getAll(req.query);
     res.json(result);
   },
 );
@@ -26,10 +18,10 @@ export const getUsersMe = controllerCatchAsync(
   },
 );
 
-export const updateUser = controllerCatchAsync(
-  async (req: Request<UserId>, res: Response) => {
-    const { id } = await usersService.update(req.params.id, req.body);
-    res.json({ message: `User ${id} updated successfully` });
+export const getUser = controllerCatchAsync(
+  async (req: Request, res: Response) => {
+    const result = await usersService.get(req.params.id);
+    res.json(result);
   },
 );
 
@@ -40,8 +32,15 @@ export const updateUsersMe = controllerCatchAsync(
   },
 );
 
+export const updateUser = controllerCatchAsync(
+  async (req: Request, res: Response) => {
+    const { id } = await usersService.update(req.params.id, req.body);
+    res.json({ message: `User ${id} updated successfully` });
+  },
+);
+
 export const deleteUser = controllerCatchAsync(
-  async (req: Request<UserId>, res: Response) => {
+  async (req: Request, res: Response) => {
     await usersService.delete(req.params.id);
     res.sendStatus(StatusCodes.NO_CONTENT);
   },
