6b4a0b7f6ffefc350e4fb41b26cfc8872ebec692
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-13 11:34:34 -0300

    format

--- services/identity/src/app.ts
+++ services/identity/src/app.ts
@@ -8,11 +8,10 @@ import { routes } from './routes';
 
 export const app = express();
 
-// Middlewares
 app.use(morganInit);
 app.use(cors());
 app.use(express.json());
 app.use(passportInit); // Must be placed after "express.json"
-app.use(routes); // Must be placed after all but before error handler
+app.use(routes); // Must be placed after req/res middlewares
 app.use(notFound); // Must be placed after routes
 app.use(errorHandler); // Must be the placed last
