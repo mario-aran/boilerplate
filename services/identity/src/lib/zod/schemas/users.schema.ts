a363d9148546b57fd02cc067d1486ca43356c248
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-14 15:00:42 -0300

    format

--- services/identity/src/lib/zod/schemas/users.schema.ts
+++ services/identity/src/lib/zod/schemas/users.schema.ts
@@ -1,5 +1,15 @@
 import { USERS_SORTABLE_COLUMNS } from '@/lib/drizzle/schemas';
-import { stringToPositiveInt, text, textId } from '@/lib/zod/utils/fields';
+import {
+  email,
+  firstName,
+  lastName,
+  limit,
+  page,
+  password,
+  roleId,
+  search,
+  uuid,
+} from '@/lib/zod/utils/fields';
 import { generateSortField } from '@/lib/zod/utils/generate-sort-field';
 import { z } from 'zod';
 
@@ -9,22 +19,28 @@ import { z } from 'zod';
 
 export type UserId = z.infer<typeof userIdSchema>;
 export type GetUsers = z.infer<typeof getUsersSchema>;
+export type UpdateUserMe = z.infer<typeof updateUserMeSchema>;
 export type UpdateUser = z.infer<typeof updateUserSchema>;
+
+// ---------------------------
+// FIELDS
+// ---------------------------
+
+const id = uuid;
+const sort = generateSortField(USERS_SORTABLE_COLUMNS);
 
 // ---------------------------
 // SCHEMAS
 // ---------------------------
 
-export const userIdSchema = z.strictObject({ id: textId });
+export const userIdSchema = z.strictObject({ id });
 
 export const getUsersSchema = z
-  .strictObject({
-    limit: stringToPositiveInt,
-    page: stringToPositiveInt,
-    sort: generateSortField(USERS_SORTABLE_COLUMNS),
-    roleId: textId,
-    search: text,
-  })
+  .strictObject({ limit, page, sort, roleId, search })
   .partial();
 
-export const updateUserSchema = z.strictObject({}).partial();
+export const updateUserMeSchema = z
+  .strictObject({ email, password, firstName, lastName })
+  .partial();
+
+export const updateUserSchema = updateUserMeSchema.extend({ roleId });
