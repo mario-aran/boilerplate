497cfe5569e5944392462c691dafba9d677e7829
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-14 11:57:02 -0300

    format

--- services/identity/src/scripts/workers/workers.script.ts
+++ services/identity/src/scripts/workers/workers.script.ts
@@ -1,13 +1,11 @@
 // DO NOT RENAME OR MOVE THIS FILE — used by "package.json"
 
 import { testDbConnection } from '@/lib/drizzle/db';
-import { bullMQConnection } from '@/lib/redis/bullmq-connection';
 import { verificationEmailWorker } from './verification-email.worker';
 
 (async () => {
   // Verify connections
   await testDbConnection();
-  await bullMQConnection.verify();
 
   // Start workers
   verificationEmailWorker();
