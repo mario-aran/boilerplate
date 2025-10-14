9aa0f7328530ea0c361361b79bba39c215ae7a93
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-14 15:01:42 -0300

    format

--- services/identity/src/lib/zod/utils/fields.ts
+++ services/identity/src/lib/zod/utils/fields.ts
@@ -1,16 +1,40 @@
 import { z } from 'zod';
 import { lowerAlphanumUnder, noSpaces } from './refines';
 
-export const positiveInt = z.number().int().positive();
+// ---------------------------
+// INTEGERS
+// ---------------------------
+
 export const stringToPositiveInt = z
   .string()
   .nonempty()
   .transform(Number)
-  .pipe(positiveInt);
+  .pipe(z.number().int().positive());
+
+export const limit = stringToPositiveInt;
+export const page = stringToPositiveInt;
+
+// ---------------------------
+// TEXTS
+// ---------------------------
 
 export const text = z.string().trim().min(1).max(60);
-export const textId = noSpaces(lowerAlphanumUnder(z.string().min(4).max(40)));
+export const search = text;
+export const firstName = text;
+export const lastName = text;
+
+// ---------------------------
+// IDS
+// ---------------------------
+
 export const uuid = z.uuid();
-export const token = noSpaces(z.string().min(1));
+export const textId = noSpaces(lowerAlphanumUnder(z.string().min(4).max(40)));
+export const roleId = textId;
+
+// ---------------------------
+// SPECIFIC
+// ---------------------------
+
 export const email = z.email().min(5).max(60);
 export const password = noSpaces(z.string().min(8).max(20));
+export const token = noSpaces(z.string().min(1));
