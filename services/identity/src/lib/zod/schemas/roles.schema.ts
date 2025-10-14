2bd024f16704212446c4a0ddf404a613641f54cf
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-14 14:57:50 -0300

    format

--- services/identity/src/lib/zod/schemas/roles.schema.ts
+++ services/identity/src/lib/zod/schemas/roles.schema.ts
@@ -1,6 +1,6 @@
 import { PERMISSION_VALUES } from '@/constants/permissions';
 import { ROLES_SORTABLE_COLUMNS } from '@/lib/drizzle/schemas';
-import { stringToPositiveInt, text, textId } from '@/lib/zod/utils/fields';
+import { limit, page, search, textId } from '@/lib/zod/utils/fields';
 import { generateSortField } from '@/lib/zod/utils/generate-sort-field';
 import { noDuplicateStrs } from '@/lib/zod/utils/refines';
 import { z } from 'zod';
@@ -18,6 +18,9 @@ export type UpdateRole = z.infer<typeof updateRoleSchema>;
 // FIELDS
 // ---------------------------
 
+const id = textId;
+const sort = generateSortField(ROLES_SORTABLE_COLUMNS);
+
 const permissionIds = noDuplicateStrs(
   z.enum(PERMISSION_VALUES).array().max(PERMISSION_VALUES.length),
 );
@@ -26,16 +29,11 @@ const permissionIds = noDuplicateStrs(
 // SCHEMAS
 // ---------------------------
 
-export const roleIdSchema = z.strictObject({ id: textId });
+export const roleIdSchema = z.strictObject({ id });
 
 export const getRolesSchema = z
-  .strictObject({
-    limit: stringToPositiveInt,
-    page: stringToPositiveInt,
-    sort: generateSortField(ROLES_SORTABLE_COLUMNS),
-    search: text,
-  })
+  .strictObject({ limit, page, sort, search })
   .partial();
 
-export const createRoleSchema = z.strictObject({ id: textId });
+export const createRoleSchema = z.strictObject({ id });
 export const updateRoleSchema = z.strictObject({ permissionIds }).partial();
