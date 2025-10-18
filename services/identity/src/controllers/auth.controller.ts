1f098b6e982c3b8e73413377343a6085971a5610
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-18 10:44:11 -0300

    format

--- services/identity/src/controllers/auth.controller.ts
+++ services/identity/src/controllers/auth.controller.ts
@@ -8,7 +8,7 @@ export const register = controllerCatchAsync(
   async (req: Request, res: Response) => {
     await authService.register(req.body);
     res.status(StatusCodes.CREATED).json({
-      message: `Registration successful. Verification will be sent shortly`,
+      message: 'Registration successful. Verification will be sent shortly',
     });
   },
 );
