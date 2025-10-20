50f267bf4f8691def7cfc59739610825dff2c3a3
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-20 20:14:57 -0300

    format

--- services/identity/src/features/auth/utils/jwt-handlers.ts
+++ services/identity/src/features/auth/utils/jwt-handlers.ts
@@ -1,11 +1,16 @@
 import {
   JWT_ACCESS_SECRET,
   JWT_REFRESH_SECRET,
+  JWT_RESET_PASSWORD_SECRET,
   JWT_VERIFICATION_EMAIL_SECRET,
 } from '@/config/env';
 import { InvalidTokenError } from '@/errors/http-errors';
 import { JwtPayload } from '@/features/auth/types';
 import jwt from 'jsonwebtoken';
+
+// ---------------------------
+// UTILS
+// ---------------------------
 
 const verifyToken = (token: string, secret: string) => {
   try {
@@ -15,11 +20,19 @@ const verifyToken = (token: string, secret: string) => {
   }
 };
 
+// ---------------------------
+// VERIFICATION EMAIL
+// ---------------------------
+
 export const signVerificationEmailToken = (payload: JwtPayload) =>
-  jwt.sign(payload, JWT_VERIFICATION_EMAIL_SECRET, { expiresIn: '1d' });
+  jwt.sign(payload, JWT_VERIFICATION_EMAIL_SECRET, { expiresIn: '15m' });
 
 export const verifyVerificationEmailToken = (token: string) =>
   verifyToken(token, JWT_VERIFICATION_EMAIL_SECRET);
+
+// ---------------------------
+// ACCESS + REFRESH
+// ---------------------------
 
 export const signAccessToken = (payload: JwtPayload) =>
   jwt.sign(payload, JWT_ACCESS_SECRET, { expiresIn: '15m' });
@@ -29,3 +42,13 @@ export const signRefreshToken = (payload: JwtPayload) =>
 
 export const verifyRefreshToken = (token: string) =>
   verifyToken(token, JWT_REFRESH_SECRET);
+
+// ---------------------------
+// RESET PASSWORD
+// ---------------------------
+
+export const signResetPasswordToken = (payload: JwtPayload) =>
+  jwt.sign(payload, JWT_RESET_PASSWORD_SECRET, { expiresIn: '15m' });
+
+export const verifyResetPasswordToken = (token: string) =>
+  verifyToken(token, JWT_RESET_PASSWORD_SECRET);
