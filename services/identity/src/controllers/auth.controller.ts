ace9e1f8a99c2b184d910e73aeb773d309287e59
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-21 18:34:15 -0300

    format

--- services/identity/src/controllers/auth.controller.ts
+++ services/identity/src/controllers/auth.controller.ts
@@ -13,9 +13,9 @@ export const register = controllerCatchAsync(
   },
 );
 
-export const resendVerificationEmail = controllerCatchAsync(
+export const resendEmailVerification = controllerCatchAsync(
   async (req: Request, res: Response) => {
-    const { email } = await authService.resendVerificationEmail(req.body);
+    const { email } = await authService.resendEmailVerification(req.body);
     res.json({
       message: `Verification email will be sent shortly to ${email}`,
     });
