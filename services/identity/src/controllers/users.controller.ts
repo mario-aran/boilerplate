506f0ac2133d49cfc6261378e2a73d2476c1e143
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-21 17:03:12 -0300

    format

--- services/identity/src/controllers/users.controller.ts
+++ services/identity/src/controllers/users.controller.ts
@@ -4,12 +4,9 @@ import { controllerCatchAsync } from '@/utils/controller-catch-async';
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-export const getUsers = controllerCatchAsync(
-  async (req: Request, res: Response) => {
-    const result = await usersService.getAll(req.query);
-    res.json(result);
-  },
-);
+// ---------------------------
+// CONTROLLERS: /users/me
+// ---------------------------
 
 export const getUserMe = controllerCatchAsync(
   async (req: Request, res: Response) => {
@@ -20,19 +17,48 @@ export const getUserMe = controllerCatchAsync(
   },
 );
 
-export const getUser = controllerCatchAsync(
-  async (req: Request, res: Response) => {
-    const result = await usersService.get(req.params.id);
-    res.json(result);
-  },
-);
-
 export const updateUserMe = controllerCatchAsync(
   async (req: Request, res: Response) => {
     const reqUser = requireUserFromReq(req);
 
     await usersService.update(reqUser.id, req.body);
     res.json({ message: 'User updated successfully' });
+  },
+);
+
+export const updateUserMeEmail = controllerCatchAsync(
+  async (req: Request, res: Response) => {
+    const reqUser = requireUserFromReq(req);
+
+    await usersService.updateEmail(reqUser.id, req.body);
+    res.json({ message: 'Verification email will be sent shortly' });
+  },
+);
+
+export const updateUserMePassword = controllerCatchAsync(
+  async (req: Request, res: Response) => {
+    const reqUser = requireUserFromReq(req);
+
+    await usersService.updatePassword(reqUser.id, req.body);
+    res.json({ message: 'Password updated successfully' });
+  },
+);
+
+// ---------------------------
+// CONTROLLERS: /users + /users/:id
+// ---------------------------
+
+export const getUsers = controllerCatchAsync(
+  async (req: Request, res: Response) => {
+    const result = await usersService.getAll(req.query);
+    res.json(result);
+  },
+);
+
+export const getUser = controllerCatchAsync(
+  async (req: Request, res: Response) => {
+    const result = await usersService.get(req.params.id);
+    res.json(result);
   },
 );
 
