19c96bf8bce79538ef96c2975a7cc1889fd761f8
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-22 14:01:08 -0300

    format

--- services/identity/src/features/auth/utils/require-req-user.ts
+++
@@ -1,8 +0,0 @@
-import { UnauthorizedError } from '@/errors/http-errors';
-import { Request } from 'express';
-
-export const requireReqUser = (req: Request) => {
-  if (!req.user) throw UnauthorizedError;
-
-  return req.user;
-};
