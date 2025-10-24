da5cbfb828cfa13598ecfd159628e6e1feb6a549
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-23 22:35:41 -0300

    eslint

--- services/identity/eslint.config.mjs
+++ services/identity/eslint.config.mjs
@@ -2,7 +2,6 @@ import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
 import eslintPluginCheckFile from 'eslint-plugin-check-file';
 import { defineConfig, globalIgnores } from 'eslint/config';
-import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
@@ -10,16 +9,17 @@ export default defineConfig([
   {
     files: ['**/*.ts'],
     languageOptions: {
-      ecmaVersion: 2022, // Should match target version in "tsconfig.json"
-      globals: globals.node,
+      parserOptions: {
+        projectService: true, // Enables "lint with type information"
+      },
     },
     extends: [
       // "eslint"
       eslint.configs.recommended,
 
       // "typescript-eslint"
-      tseslint.configs.strict,
-      tseslint.configs.stylistic,
+      tseslint.configs.strictTypeChecked, // "strict" with type information
+      tseslint.configs.stylisticTypeChecked, // "stylistic" with type information
     ],
     plugins: { 'check-file': eslintPluginCheckFile },
     rules: {
@@ -40,6 +40,9 @@ export default defineConfig([
         },
       ],
 
+      // "typescript-eslint"
+      '@typescript-eslint/no-floating-promises': 'error',
+
       // "eslint-plugin-check-file"
       'check-file/filename-naming-convention': [
         'error',
@@ -52,5 +55,5 @@ export default defineConfig([
       ],
     },
   },
-  eslintConfigPrettier, // "eslint-config-prettier": Must be placed last
+  eslintConfigPrettier, // "eslint-config-prettier": must be placed last
 ]);
