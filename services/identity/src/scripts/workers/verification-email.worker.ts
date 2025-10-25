239d5a62d58d7483ee2c86168b9a743500216b4f
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-25 13:56:32 -0300

    format

--- services/identity/src/scripts/workers/verification-email.worker.ts
+++ services/identity/src/scripts/workers/verification-email.worker.ts
@@ -1,9 +1,12 @@
 import { QUEUES } from '@/constants/queues';
 import { emailService } from '@/features/email/email.service';
+import { EmailPayload } from '@/features/email/types';
+import { Job } from 'bullmq';
 import { createWorker } from './utils';
 
 export const verificationEmailWorker = () =>
   createWorker({
     name: QUEUES.EMAIL_VERIFICATION_EMAIL,
-    processor: (job) => emailService.sendEmailVerification(job.data),
+    processor: (job: Job<EmailPayload>) =>
+      emailService.sendEmailVerification(job.data),
   });
