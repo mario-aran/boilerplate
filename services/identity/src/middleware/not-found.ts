677a5a8a8def8cd155de32af049af5af13f08d12
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-25 13:35:13 -0300

    format

--- services/identity/src/middleware/not-found.ts
+++ services/identity/src/middleware/not-found.ts
@@ -1,5 +1,6 @@
 import { NotFoundError } from '@/errors/http-errors';
 import { NextFunction, Request, Response } from 'express';
 
-export const notFound = (_: Request, _res: Response, next: NextFunction) =>
+export const notFound = (_: Request, _res: Response, next: NextFunction) => {
   next(NotFoundError);
+};
