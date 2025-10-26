faaddc32f4e56548d8e9339cec30a1e282716c0e
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-26 16:59:07 -0300

    format

--- services/identity/src/features/auth/passport/jwt.strategy.ts
+++ services/identity/src/features/auth/passport/jwt.strategy.ts
@@ -9,17 +9,18 @@ export const jwtStrategy = new Strategy(
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: JWT_ACCESS_SECRET,
   },
-  async (payload: JwtPayload, done) => {
-    try {
-      // Check if user exists
-      const user = await usersService.get(payload.userId);
-      if (!user.isActive) throw AccessDeniedError;
+  (payload: JwtPayload, done) =>
+    void usersService
+      .get(payload.userId)
+      .then((user) => {
+        // Check if user exists
+        if (!user.isActive) throw AccessDeniedError;
 
-      // Pass user to "passport.authenticate"
-      return done(null, user);
-    } catch (err) {
-      // Pass error it to "passport.authenticate"
-      return done(err, false);
-    }
-  },
+        // Pass user to "passport.authenticate"
+        done(null, user);
+      })
+      .catch((err: unknown) => {
+        // Pass error it to "passport.authenticate"
+        done(err, false);
+      }),
 );
