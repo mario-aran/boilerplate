655e0d3df9a15b90b9995e7b24f8981f7010d39a
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-26 11:42:33 -0300

    format

--- services/identity/src/controllers/permissions.controller.ts
+++ services/identity/src/controllers/permissions.controller.ts
@@ -1,9 +1,13 @@
 import { permissionsService } from '@/features/permissions/permissions.service';
+import { GetPermissions } from '@/lib/zod/schemas/permissions.schema';
 import { controllerCatchAsync } from '@/utils/controller-catch-async';
 import { Request, Response } from 'express';
 
 export const getPermissions = controllerCatchAsync(
-  async (req: Request, res: Response) => {
+  async (
+    req: Request<unknown, unknown, unknown, GetPermissions>,
+    res: Response,
+  ) => {
     const result = await permissionsService.getAll(req.query);
     res.json(result);
   },
