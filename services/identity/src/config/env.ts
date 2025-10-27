baadd6c8afc34c4b62cf4df607fb01aa01d124e0
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-27 16:22:59 -0300

    format

--- services/identity/src/config/env.ts
+++ services/identity/src/config/env.ts
@@ -29,7 +29,6 @@ if (!isProduction) {
 const getRequiredEnv = (key: string) => {
   const value = process.env[key];
   if (!value) throw new Error(`Environment variable ${key} is required`);
-
   return value;
 };
 
