49b7ebbf40f0bd007b8138bb9fc96f38e1aaba90
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-28 20:26:54 -0300

    format

---
+++ services/identity/src/lib/drizzle/utils/column-utils.ts
@@ -0,0 +1,39 @@
+import { getTableColumns } from 'drizzle-orm';
+import { AnyPgTable } from 'drizzle-orm/pg-core';
+
+export const getSwaggerColumnsObject = <
+  T extends AnyPgTable,
+  Excluded extends readonly (keyof T['$inferSelect'])[] = [], // '[]' enables proper typing of excluded keys
+>(
+  table: T,
+  excluded?: Excluded,
+) => {
+  const columns = getTableColumns(table);
+  const filteredEntries = Object.entries(columns).flatMap(([k, v]) =>
+    excluded?.includes(k) ? [] : [[k, v.dataType]],
+  );
+
+  return Object.fromEntries(filteredEntries) as Record<
+    Exclude<keyof T['$inferSelect'], Excluded[number]>,
+    string
+  >;
+};
+
+export const getSortableColumns = <
+  T extends AnyPgTable,
+  Col extends keyof T['$inferSelect'],
+  ExCol extends Col = never, // 'never' enables proper typing of excluded keys
+>(
+  table: T,
+  excluded?: readonly ExCol[],
+) => {
+  const columns = getTableColumns(table);
+  const result = Object.keys(columns).flatMap((col) =>
+    excluded?.includes(col as ExCol) ? [] : [col, `-${col}`],
+  );
+
+  return result as [
+    Exclude<Col, ExCol> | `-${string & Exclude<Col, ExCol>}`,
+    ...(Exclude<Col, ExCol> | `-${string & Exclude<Col, ExCol>}`)[],
+  ];
+};
