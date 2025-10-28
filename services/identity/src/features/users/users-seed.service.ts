ce25025adb04e3dd2ab9877bf603ea4083325ae2
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-28 20:25:35 -0300

    format

--- services/identity/src/features/users/users-seed.service.ts
+++ services/identity/src/features/users/users-seed.service.ts
@@ -1,5 +1,5 @@
 import { SYSTEM_ROLES } from '@/constants/system-roles';
-import { hashPassword } from '@/lib/bcrypt/utils/password';
+import { hashPassword } from '@/lib/bcrypt/password-utils';
 import { db } from '@/lib/drizzle/db';
 import { UserInsert, usersTable } from '@/lib/drizzle/schemas';
 import { faker } from '@faker-js/faker';
