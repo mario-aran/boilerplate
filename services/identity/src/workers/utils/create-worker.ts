b985682994afe7029363c106c787a5b1533c8194
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-10-27 22:25:31 -0300

    format

---
+++ services/identity/src/workers/utils/create-worker.ts
@@ -0,0 +1,24 @@
+import { logger } from '@/lib/logger/winston-logger';
+import { workerClient } from '@/lib/redis/bullmq/clients';
+import { Job, Worker } from 'bullmq';
+
+export const createWorker = <T extends object>({
+  name,
+  processor,
+}: {
+  name: string;
+  processor: (job: Job<T>) => Promise<unknown>;
+}) => {
+  const worker = new Worker(name, processor, {
+    connection: workerClient,
+  });
+  worker.on('completed', (job) =>
+    logger.info(`${name} job ${String(job.id)} has completed`),
+  );
+  worker.on('failed', (job, err) =>
+    logger.error(`${name} job ${String(job?.id)} has failed: ${err.message}`),
+  );
+
+  logger.info(`${name} worker started`);
+  return worker;
+};
